29466d0d17eabd1d3a8c948fb2a2c71849f8013f
Karel Hala <ch4rlien@gmail.com>
2018-09-04 15:35:35 +0200

    Update babel as JS file so we can load plugins and presets from local node_modules (#72)

---
+++ .babelrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  presets: ['babel-preset-env', 'babel-preset-react', 'babel-preset-es2017', 'babel-preset-es2015'].map(require.resolve),
+  plugins: [
+      [require.resolve('babel-plugin-transform-runtime'), {
+          polyfill: false,
+          regenerator: true
+      }],
+      require.resolve('babel-plugin-syntax-dynamic-import'),
+      require.resolve('babel-plugin-lodash'),
+      require.resolve('babel-plugin-transform-object-rest-spread')
+  ]
+}
---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  env: {
+    "es6": true,
+    "browser": true,
+    "node": true,
+    "jest": true
+  },
+  parser: "babel-eslint"
+}
