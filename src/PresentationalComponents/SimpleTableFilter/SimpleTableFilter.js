23ea2d95be081836f620ba2e9e7c08aba7126c0c
Karel Hala <ch4rlien@gmail.com>
2018-09-25 10:04:38 +0200

    Add dropdown for simple filter (#100)

--- src/PresentationalComponents/SimpleTableFilter/SimpleTableFilter.js
+++ src/PresentationalComponents/SimpleTableFilter/SimpleTableFilter.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Button, ButtonVariant } from '@patternfly/react-core';
+import { Button, ButtonVariant, Dropdown, DropdownToggle, DropdownItem } from '@patternfly/react-core';
 import { Input } from '../Input';
 
 class SimpleFilter extends Component {
@@ -8,18 +8,36 @@ class SimpleFilter extends Component {
         super(props);
         this.onInputChange = this.onInputChange.bind(this);
         this.onFilterSubmit = this.onFilterSubmit.bind(this);
+        this.onToggle = this.onToggle.bind(this);
+        this.onSelect = this.onSelect.bind(this);
         this.state = {
-            activeFilter: ''
+            activeFilter: '',
+            isOpen: false
         };
+    }
+
+    onToggle(isOpen) {
+        this.setState({
+            isOpen
+        });
     }
 
     onInputChange(event) {
         this.setState({ activeFilter: event.target.value });
-        this.props.hasOwnProperty('onFilterChange') && this.props.onFilterChange(event.target.value);
+        this.props.hasOwnProperty('onFilterChange') && this.props.onFilterChange(event.target.value, this.state.selected);
     }
 
     onFilterSubmit() {
-        this.props.hasOwnProperty('onButtonClick') && this.props.onButtonClick(this.state.activeFilter);
+        this.props.hasOwnProperty('onButtonClick') && this.props.onButtonClick(this.state.activeFilter, this.state.selected);
+    }
+
+    onSelect(event) {
+        const { options } = this.props;
+        this.setState({
+            isOpen: false,
+            selected: options.items.find(oneItem => oneItem.value === event.target.getAttribute('data-key'))
+        });
+        this.props.onOptionSelect && this.props.onOptionSelect(event);
     }
 
     render() {
@@ -28,13 +46,35 @@ class SimpleFilter extends Component {
             buttonTitle = 'Filter',
             className = '',
             onButtonClick,
+            onOptionSelect,
             onFilterChange,
+            options,
             ...props
         } = this.props;
+        const { isOpen, selected } = this.state;
         return (
             <div className={ `pf-c-input-group ${className}` } { ...props }>
+                {
+                    options &&
+          <Dropdown
+              onSelect={ this.onSelect }
+              isOpen={ isOpen }
+              toggle={
+                  <DropdownToggle onToggle={ this.onToggle }>
+                      { (selected && selected.title) || options.title || 'Dropdown' }
+                  </DropdownToggle>
+              }
+          >
+              { options.items.map(oneItem =>
+                  <DropdownItem key={ oneItem.value } data-key={ oneItem.value }>{ oneItem.title }</DropdownItem>
+              ) }
+          </Dropdown>
+                }
                 <Input placeholder={ placeholder } onChange={ this.onInputChange }/>
-                { buttonTitle && <Button variant={ ButtonVariant.secondary } onClick={ this.onFilterSubmit }>{ buttonTitle }</Button> }
+                {
+                    buttonTitle &&
+                    <Button variant={ ButtonVariant.secondary } onClick={ this.onFilterSubmit }>{ buttonTitle }</Button>
+                }
             </div>
         );
     }
@@ -44,8 +84,16 @@ SimpleFilter.propTypes = {
     buttonTitle: PropTypes.string,
     placeholder: PropTypes.string,
     className: PropTypes.string,
+    options: PropTypes.shape({
+        title: PropTypes.string,
+        items: PropTypes.arrayOf(PropTypes.shape({
+            value: PropTypes.string,
+            title: PropTypes.string
+        }))
+    }),
     onButtonClick: PropTypes.func,
-    onFilterChange: PropTypes.func
+    onFilterChange: PropTypes.func,
+    onOptionSelect: PropTypes.func
 };
 
 export default SimpleFilter;
