5eef2351e8b3ecd359449a02ce945f490c5835c9
Martin Maroši <marvusm.mmi@gmail.com>
2018-12-10 15:41:24 +0100

    Remove FormRenderer from repository. (#166)

--- src/SmartComponents/Forms/DataDrivenForm/validationMapper.js
+++
@@ -1,6 +0,0 @@
-import { required, minLength } from '../Validators/validators';
-
-export default validationType => ({
-    required,
-    minLength
-})[validationType];
--- src/SmartComponents/Forms/index.js
+++
@@ -1,1 +0,0 @@
-export { FormRenderer } from './DataDrivenForm';
