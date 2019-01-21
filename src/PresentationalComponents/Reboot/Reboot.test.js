98234b2763dc7d0acf1945b705c55f36f4de76d0
Karel Hala <ch4rlien@gmail.com>
2019-01-21 15:56:14 +0100

    Add Reboot tests <no> (#226)

---
+++ src/PresentationalComponents/Reboot/Reboot.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+import toJson from 'enzyme-to-json';
+import Reboot from './Reboot';
+
+describe('Reboot component', () => {
+    it('should render correctly', () => {
+        const wrapper = shallow(<Reboot />);
+        expect(toJson(wrapper)).toMatchSnapshot();
+    });
+
+    it('should render correctly with red', () => {
+        const wrapper = shallow(<Reboot red />);
+        expect(toJson(wrapper)).toMatchSnapshot();
+    });
+});
