c249969877630708e6dceda1fa2fd815ad3c43f1
Karel Hala <khala@redhat.com>
2019-01-21 18:06:05 +0100

    Add Wizard tests <no>

--- src/PresentationalComponents/Wizard/Wizard.js
+++ src/PresentationalComponents/Wizard/Wizard.js
@@ -7,31 +7,24 @@ import { Modal, Button } from '@patternfly/react-core';
 import './wizard.scss';
 
 class Wizard extends Component {
+    state = {
+        currentStep: 0
+    }
 
-    constructor () {
-        super();
-        this.state = {
-            currentStep: 0
-        };
-        this.handlePreviousModalStep = this.handlePreviousModalStep.bind(this);
-        this.handleNextModalStep = this.handleNextModalStep.bind(this);
-        this.handleOnClose = this.handleOnClose.bind(this);
-    };
-
-    handleNextModalStep() {
+    handleNextModalStep = () => {
         this.setState(({ currentStep }) => ({
             currentStep: currentStep + 1
         }));
     }
 
-    handlePreviousModalStep() {
+    handlePreviousModalStep = () => {
         this.setState(({ currentStep }) => ({
             currentStep: currentStep - 1
         }));
     }
 
     // On modal close, reset currentStep back to the initial step, the call modalToggle(PF)
-    handleOnClose(submit = false) {
+    handleOnClose = (submit) => {
         const result = this.props.onClose(submit);
 
         if (result === false) {
@@ -51,16 +44,16 @@ class Wizard extends Component {
         );
 
         const renderModalActions =  [
-            <Button key="cancel" variant="secondary" onClick={ () => this.handleOnClose(false) }>
+            <Button key="cancel" action="cancel" variant="secondary" onClick={ () => this.handleOnClose(false) }>
             Cancel
             </Button>,
             // Conditionally render 'previous' button if not on first page
             this.state.currentStep !== 0 &&
-                <Button key="back" variant="secondary" onClick={ this.handlePreviousModalStep }> Back </Button>,
+                <Button key="back" action="back" variant="secondary" onClick={ this.handlePreviousModalStep }> Back </Button>,
             // Conditionally render 'confirm' button if on last page
             this.state.currentStep < this.props.content.length - 1
-                ? <Button key="Next" variant="primary" onClick={ this.handleNextModalStep }> Next </Button>
-                : <Button key="confirm" variant="primary" onClick={ () => this.handleOnClose(true) }> Confirm </Button>
+                ? <Button key="Next" action="next" variant="primary" onClick={ this.handleNextModalStep }> Next </Button>
+                : <Button key="confirm" action="confirm" variant="primary" onClick={ () => this.handleOnClose(true) }> Confirm </Button>
         ];
 
         return (
---
+++ src/PresentationalComponents/Wizard/Wizard.test.js
@@ -0,0 +1,107 @@
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+import toJson from 'enzyme-to-json';
+import Wizard from './Wizard';
+
+const content = [
+    <div id="first">First</div>,
+    <div id="second">Second</div>,
+    <div id="third">Third</div>,
+    <div id="fourth">Fourth</div>
+]
+
+describe('Wizard component', () => {
+    describe('should render correctly', () => {
+        it('default', () => {
+            const wrapper = shallow(<Wizard content={content} title="Some title" isOpen />);
+            expect(toJson(wrapper)).toMatchSnapshot();
+        });
+
+        it('large', () => {
+            const wrapper = shallow(<Wizard content={content} isLarge title="Some title" isOpen />);
+            expect(toJson(wrapper)).toMatchSnapshot();
+        });
+
+        it('one page', () => {
+            const wrapper = shallow(<Wizard content={[<div>One</div>]} title="Some title" isOpen />);
+            expect(toJson(wrapper)).toMatchSnapshot();
+        });
+    });
+
+    describe('content change', () => {
+        it('should happen after clicking on next', () => {
+            const wrapper = mount(<Wizard content={content} title="Some title" isOpen />);
+            wrapper.find('[action="next"]').first().simulate('click');
+            wrapper.update();
+            expect(wrapper.find('#second').length).toBe(1);
+            expect(wrapper.find('#first').length).toBe(0);
+        });
+
+        it('should happen at last content', () => {
+            const wrapper = mount(<Wizard content={content} title="Some title" isOpen />);
+            wrapper.find('[action="next"]').first().simulate('click');
+            wrapper.update();
+            wrapper.find('[action="next"]').first().simulate('click');
+            wrapper.update();
+            wrapper.find('[action="next"]').first().simulate('click');
+            wrapper.update();
+            expect(wrapper.find('#fourth').length).toBe(1);
+            expect(wrapper.find('[action="next"]').length).toBe(0);
+            expect(wrapper.find('.pf-c-modal-box__footer > [action="confirm"]').length).toBe(1);
+        });
+
+        it('should happen at last content', () => {
+            const wrapper = mount(<Wizard content={content} title="Some title" isOpen />);
+            wrapper.find('[action="next"]').first().simulate('click');
+            wrapper.update();
+            wrapper.find('[action="next"]').first().simulate('click');
+            wrapper.update();
+            expect(wrapper.find('#third').length).toBe(1);
+            wrapper.find('[action="back"]').first().simulate('click');
+            expect(wrapper.find('#second').length).toBe(1);
+        });
+    });
+
+    describe('API', () => {
+        it('should not call onClose X button', () => {
+            const onClose = jest.fn();
+            const wrapper = mount(<Wizard content={content} title="Some title" isOpen />);
+            wrapper.find('.pf-c-modal-box__close button').first().simulate('click');
+            expect(onClose.mock.calls.length).toBe(0);
+        });
+
+        it('should not call onClose on close button', () => {
+            const onClose = jest.fn();
+            const wrapper = mount(<Wizard content={content} title="Some title" isOpen />);
+            wrapper.find('[action="cancel"]').first().simulate('click');
+            expect(onClose.mock.calls.length).toBe(0)
+        });
+
+        it('should call onClose X button', () => {
+            const onClose = jest.fn();
+            const wrapper = mount(<Wizard content={content} title="Some title" isOpen onClose={onClose}/>);
+            wrapper.find('.pf-c-modal-box__close button').first().simulate('click');
+            expect(onClose.mock.calls.length).toBe(1);
+        });
+
+        it('should call onClose X button', () => {
+            const onClose = jest.fn();
+            const wrapper = mount(<Wizard content={content} title="Some title" isOpen onClose={onClose} />);
+            wrapper.find('[action="cancel"]').first().simulate('click');
+            expect(onClose.mock.calls.length).toBe(1);
+        });
+
+        it('should call close on confirm', () => {
+            const onClose = jest.fn();
+            const wrapper = mount(<Wizard content={content} title="Some title" isOpen onClose={onClose} />);
+            wrapper.find('[action="next"]').first().simulate('click');
+            wrapper.update();
+            wrapper.find('[action="next"]').first().simulate('click');
+            wrapper.update();
+            wrapper.find('[action="next"]').first().simulate('click');
+            wrapper.update();
+            wrapper.find('[action="confirm"]').first().simulate('click');
+            expect(onClose.mock.calls.length).toBe(1);
+        });
+    });
+});
