7962ae3cc13a2b54af1576ad6404b48cf0a6aa28
Karel Hala <ch4rlien@gmail.com>
2019-01-21 16:06:14 +0100

    Add Cestion tests <no> (#228)

--- src/PresentationalComponents/Section/Section.js
+++ src/PresentationalComponents/Section/Section.js
@@ -4,14 +4,6 @@ import classNames from 'classnames';
 
 import './section.scss';
 
-/**
- * This is a dumb component that only recieves properties from a smart component.
- * Dumb components are usually functions and not classes.
- *
- * Section Modifiers:
- *  Types = 'content'
- */
-
 const Section = ({ type, children, className, ...props }) => {
 
     let sectionClasses = classNames(
---
+++ src/PresentationalComponents/Section/Section.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+import toJson from 'enzyme-to-json';
+import Section from './Section';
+
+describe('Section component', () => {
+    it('should render correctly', () => {
+        const wrapper = shallow(<Section type="page">Test</Section>);
+        expect(toJson(wrapper)).toMatchSnapshot();
+    });
+});
