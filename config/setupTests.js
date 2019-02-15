0a898099188f4616198722747bb0dc7141342393
Ryan Long <rlong@redhat.com>
2019-02-15 08:04:18 -0500

    updating basic tests for charts (#307)
    * updating basic tests for charts
    
    * fixing tests, adding IDs to prevent random generation

--- config/setupTests.js
+++ config/setupTests.js
@@ -3,3 +3,10 @@ import Adapter from 'enzyme-adapter-react-16';
 import 'babel-polyfill';
 
 configure({ adapter: new Adapter() });
+global.SVGPathElement = function () {};
+
+global.MutationObserver = class {
+    constructor(callback) {}
+    disconnect() {}
+    observe(element, initObject) {}
+};
