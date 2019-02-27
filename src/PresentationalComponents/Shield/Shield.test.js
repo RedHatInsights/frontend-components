baaadd407b2ada2d0f2e2e674d2b20a382b0f154
Jan Elznic <jan@elznic.com>
2019-02-27 23:09:32 +0100

    Shield Component - Unit test (#334)

---
+++ src/PresentationalComponents/Shield/Shield.test.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import toJson from 'enzyme-to-json';
+import Shield from './Shield';
+
+describe('Shield component', () => {
+    it('should render without props', () => {
+        const wrapper = shallow(<Shield />);
+        expect(toJson(wrapper)).toMatchSnapshot();
+    });
+
+    it('should render where impact value is undefined', () => {
+        const wrapper = shallow(<Shield impact={ undefined } />);
+        expect(toJson(wrapper)).toMatchSnapshot();
+    });
+
+    it('should render where impact value is empty string', () => {
+        const wrapper = shallow(<Shield impact={ '' } />);
+        expect(toJson(wrapper)).toMatchSnapshot();
+    });
+
+    it('should render where impact value is Low and tooltipPrefix is Severity: ', () => {
+        const wrapper = shallow(<Shield impact={ 'Low' } tooltipPrefix={ 'Severity: ' } />);
+        expect(toJson(wrapper)).toMatchSnapshot();
+    });
+
+    it('should render where impact value is Critical', () => {
+        const wrapper = shallow(<Shield impact={ 'Critical' } />);
+        expect(toJson(wrapper)).toMatchSnapshot();
+    });
+
+    it('should render where impact value is NonExist', () => {
+        const wrapper = shallow(<Shield impact={ 'NonExist' } />);
+        expect(toJson(wrapper)).toMatchSnapshot();
+    });
+
+    it('should render where impact value is Medium and tooltipPosition is bottom', () => {
+        const wrapper = shallow(<Shield impact={ 'Medium' } tooltipPosition={ 'bottom' } />);
+        expect(toJson(wrapper)).toMatchSnapshot();
+    });
+
+    it('should render where impact value is Medium and tooltipPosition is notValid', () => {
+        const wrapper = shallow(<Shield impact={ 'Medium' } tooltipPosition={ 'notValid' } />);
+        expect(toJson(wrapper)).toMatchSnapshot();
+    });
+
+    it('should render where impact value is Low and title is Hello world', () => {
+        const wrapper = shallow(<Shield impact={ 'Low' } title={ 'Hello world' } />);
+        expect(toJson(wrapper)).toMatchSnapshot();
+    });
+
+    it('should render where title is Hello world', () => {
+        const wrapper = shallow(<Shield title={ 'Hello world' } />);
+        expect(toJson(wrapper)).toMatchSnapshot();
+    });
+
+    it('should render where title is Hello world and hasTooltip is true', () => {
+        const wrapper = shallow(<Shield title={ 'Hello world' } hasTooltip={ true } />);
+        expect(toJson(wrapper)).toMatchSnapshot();
+    });
+});
