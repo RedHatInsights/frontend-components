58d4655d7d2c30a4db69cfb8bccfee13b3f2e974
Ryan Long <rlong@redhat.com>
2019-04-01 07:04:18 -0400

    updating battery to take up required space (#367)

--- src/PresentationalComponents/Battery/Battery.js
+++ src/PresentationalComponents/Battery/Battery.js
@@ -24,36 +24,15 @@ import './battery.scss';
 
 const Battery = ({ severity, label, labelHidden, className, ...props }) => {
 
-    let batteryWrapperClasses = classNames(
-        className,
-        'ins-battery'
-    );
-
     let batteryClasses = classNames(
+        className,
+        'ins-battery',
         { [`ins-battery-${severity}`]: severity !== undefined }
     );
 
     let ariaLabels = {};
     if (labelHidden) {
         ariaLabels = { ['aria-label']: severity + ' ' + label };
-    }
-
-    function generateBattery (severity, batteryClasses, ariaLabels) {
-        return (
-            <i className= { batteryClasses } { ...ariaLabels }>
-                <svg version="1.1"
-                    id="battery_svg"
-                    x="0px" y="0px"
-                    viewBox="0 0 448 512"
-                    style={ { enableBackground: 'new 0 0 448 512' } }
-                    shapeRendering= 'geometricpresision'>
-                    <path
-                        style={ { fill: 'none', fillOpacity: 1, stroke: '#969696', strokeWidth: 41.96378708, strokeLinejoin: 'round', strokeMiterlimit: 4, strokeDasharray: 'none', strokeDashoffset: 0, strokeOpacity: 1 } }
-                        d="m 144.16452,21.032222 h 159.67454 q 123.1748,0 123.1748,128.667868 v 212.64759 q 0,128.66788 -123.1748,128.66788 H 144.16452 q -123.174811,0 -123.174811,-128.66788 V 149.70009 q 0,-128.667868 123.174811,-128.667868 z" />
-                    { batteryLevels(severity) }
-                </svg>
-            </i>
-        );
     }
 
     function batteryLevels (severity) {
@@ -81,14 +60,22 @@ const Battery = ({ severity, label, labelHidden, className, ...props }) => {
     }
 
     return (
-        <span className={ batteryWrapperClasses } { ...props } widget-type='InsightsBattery' widget-id={ label }>
-            {
-                labelHidden ?
-                    generateBattery(severity, batteryClasses, ariaLabels) :
-                    generateBattery(severity, batteryClasses)
-            }
+        <React.Fragment>
+            <i className={ batteryClasses } { ...ariaLabels } { ...props } widget-type='InsightsBattery' widget-id={ label }>
+                <svg version="1.1"
+                    id="battery_svg"
+                    x="0px" y="0px"
+                    viewBox="0 0 448 512"
+                    style={ { enableBackground: 'new 0 0 448 512' } }
+                    shapeRendering= 'geometricpresision'>
+                    <path
+                        style={ { fill: 'none', fillOpacity: 1, stroke: '#969696', strokeWidth: 41.96378708, strokeLinejoin: 'round', strokeMiterlimit: 4, strokeDasharray: 'none', strokeDashoffset: 0, strokeOpacity: 1 } }
+                        d="m 144.16452,21.032222 h 159.67454 q 123.1748,0 123.1748,128.667868 v 212.64759 q 0,128.66788 -123.1748,128.66788 H 144.16452 q -123.174811,0 -123.174811,-128.66788 V 149.70009 q 0,-128.667868 123.174811,-128.667868 z" />
+                    { batteryLevels(severity) }
+                </svg>
+            </i>
             { !labelHidden && <span className='label'> { label } </span> }
-        </span>
+        </React.Fragment>
     );
 };
 
@@ -106,4 +93,3 @@ Battery.propTypes = {
 Battery.defaultProps = {
     severity: 'null'
 };
-
