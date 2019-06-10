09b8f1469c0bf9035756065fe6a9fb2f6b1f97b0
Jiri Dostal <jdostal@redhat.com>
2019-06-10 15:57:08 +0200

    Use default error interceptor

--- packages/inventory-vulnerabilities/src/api/interceptors.js
+++ packages/inventory-vulnerabilities/src/api/interceptors.js
@@ -1,19 +1,11 @@
-import { authInterceptor, interceptor401, responseDataInterceptor } from '@redhat-cloud-services/frontend-components-utilities/files/interceptors';
+import {
+    authInterceptor,
+    errorInterceptor,
+    interceptor401,
+    responseDataInterceptor
+} from '@redhat-cloud-services/frontend-components-utilities/files/interceptors';
 import axios from 'axios';
 const instance = axios.create();
-
-export function errorInterceptor(err) {
-    if (!axios.isCancel(err)) {
-        const errObject = { ...err };
-
-        if (errObject.response && errObject.response.data && errObject.response.data.errors) {
-            const data = errObject.response.data.errors[0];
-            throw data;
-        }
-
-        throw err;
-    }
-}
 
 instance.interceptors.request.use(authInterceptor);
 instance.interceptors.response.use(responseDataInterceptor);
