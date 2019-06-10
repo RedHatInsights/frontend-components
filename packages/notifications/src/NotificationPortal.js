0a9108f1aa9d01762f4c7c7abb923f23e62ffa7e
Karel Hala <khala@redhat.com>
2019-06-10 14:14:30 +0200

    Add paginated notifications if more than 5 notifications are present (#42)
    * Add paginated notifications if more than 5 notifications are present
    
    * Fix typo in notification test

--- packages/notifications/src/NotificationPortal.js
+++ packages/notifications/src/NotificationPortal.js
@@ -1,19 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { createPortal } from 'react-dom';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import { removeNotification } from './redux/actions/notifications';
+import { removeNotification, clearNotifications } from './redux/actions/notifications';
 import Notification from './Notification';
 import './notifications.scss';
+import NotificationPagination from './NotificationPagination';
 
-const Portal = ({ notifications, removeNotification, rootId }) =>
-    (!notifications || Array.isArray(notifications) && notifications.length === 0)
-        ? null
-        : createPortal((
-            <div className="notifications-portal">
-                {  notifications.map(props => <Notification onDismiss={ removeNotification } key={ props.id } { ...props } />) }
-            </div>
-        ), document.getElementById(rootId) || document.body);
+class Portal extends Component {
+    state = {
+        page: 1
+    }
+
+    onSetPage = (_event, page) => {
+        this.setState({ page });
+    }
+
+    render() {
+        const { page } = this.state;
+        const { notifications, removeNotification, rootId, onClearAll } = this.props;
+        const slicedNotifications = notifications && notifications.length <= 5 ?
+            notifications :
+            notifications && notifications.slice((page - 1) * 5, page * 5);
+        return (!notifications || Array.isArray(notifications) && notifications.length === 0)
+            ? null
+            : createPortal((
+                <div className="notifications-portal">
+                    {
+                        notifications &&
+                        notifications.length > 5 &&
+                        <NotificationPagination
+                            onSetPage={ this.onSetPage }
+                            count={ notifications.length }
+                            page={ page }
+                            onClearAll={ onClearAll }
+                        />
+                    }
+                    { slicedNotifications.map(props => <Notification onDismiss={ removeNotification } key={ props.id } { ...props } />) }
+                </div>
+            ), document.getElementById(rootId) || document.body);
+    }
+};
 
 Portal.propTypes = {
     notifications: PropTypes.arrayOf(PropTypes.shape({
@@ -23,7 +50,8 @@ Portal.propTypes = {
         description: PropTypes.node,
         dismissable: PropTypes.bool
     })),
-    removeNotification: PropTypes.func.isRequired
+    removeNotification: PropTypes.func.isRequired,
+    onClearAll: PropTypes.func
 };
 
 const mapStateToProps = ({ notifications }, initialProps) => ({
@@ -31,7 +59,8 @@ const mapStateToProps = ({ notifications }, initialProps) => ({
 });
 
 const mapDispatchToProps = (dispatch, initialProps) => ({
-    removeNotification: initialProps.removeNotification ? initialProps.removeNotification : id=> dispatch(removeNotification(id))
+    removeNotification: initialProps.removeNotification ? initialProps.removeNotification : id => dispatch(removeNotification(id)),
+    onClearAll: initialProps.clearNotifications ? initialProps.clearNotifications : () => dispatch(clearNotifications())
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Portal);
