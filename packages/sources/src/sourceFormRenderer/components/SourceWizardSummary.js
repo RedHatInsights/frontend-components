88b24abe0a6bc74f62075360c63e8d0e2f8f70f9
Richard Vsiansky <r.vsia@seznam.cz>
2019-07-25 21:38:38 +0200

    Update Summary screen according to design

--- packages/sources/src/sourceFormRenderer/components/SourceWizardSummary.js
+++ packages/sources/src/sourceFormRenderer/components/SourceWizardSummary.js
@@ -1,108 +1,48 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
-import { FormGroup, TextInput } from '@patternfly/react-core';
-import find from 'lodash/find';
-
-const openShiftSummary = ({ url, certificate_authority }) => (
-    <React.Fragment>
-        <FormGroup
-            label="URL"
-            fieldId='summary-url'
-        >
-            <TextInput
-                isDisabled
-                type="text"
-                id="summary-url"
-                value={ url }
-            />
-        </FormGroup>
-        <FormGroup
-            label = "SSL Certificate"
-            fieldId='summary-ssl-cert'
-        >
-            <TextInput
-                isDisabled
-                type="text"
-                id="summary-ssl-cert"
-                value={ certificate_authority }
-            />
-        </FormGroup>
-    </React.Fragment>
-);
-openShiftSummary.propTypes = {
-    url: PropTypes.string.isRequired,
-    certificate_authority: PropTypes.string.isRequired
-};
-
-const awsSummary = ({ username, password }) => (
-    <React.Fragment>
-        <FormGroup
-            label = "Access Key ID"
-        >
-            <TextInput
-                isDisabled
-                type="text"
-                id="summary-id"
-                value={ username }
-            />
-        </FormGroup>
-        <FormGroup
-            label = "Secret access key"
-            fieldId='summary-ssl-cert'
-        >
-            <TextInput
-                isDisabled
-                type="text"
-                id="summary-ssl-cert"
-                value={ password }
-            />
-        </FormGroup>
-    </React.Fragment>
-);
-awsSummary.propTypes = {
-    username: PropTypes.string.isRequired,
-    password: PropTypes.string.isRequired
-};
-
-const typeSpecificSummary = (type, props) => type === 'openshift' ?
-    openShiftSummary(props) : type === 'amazon' ?
-        awsSummary(props) : null;
-
-const niceTypeName = (name, types) => {
-    const sourceType = find(types, { name });
-    return sourceType && sourceType.product_name || name;
-};
+import { TextContent, TextListItem, TextListItemVariants, TextListVariants, TextList } from '@patternfly/react-core';
 
 const SourceWizardSummary = ({ sourceTypes, formOptions }) => {
-    const { source_name, source_type, ...rest } = formOptions.getState().values;
-    return (
-        <React.Fragment>
-            <FormGroup
-                label = "name"
-                fieldId="summary-name"
-            >
-                <TextInput
-                    isDisabled
-                    type="text"
-                    id="summary-name"
-                    value={ source_name }
-                />
-            </FormGroup>
-            <FormGroup
-                label = "Type"
-                fieldId="summary-type"
-            >
-                <TextInput
-                    isDisabled
-                    type="text"
-                    id="summary-type"
-                    value={ niceTypeName(source_type, sourceTypes) }
-                />
-            </FormGroup>
-            { typeSpecificSummary(source_type, rest) }
-        </React.Fragment>
-    );
+    const values = formOptions.getState().values;
+    const type = sourceTypes.find(type => type.name === values.source_type);
+    const fields = type.schema.fields;
+
+    const valuesList = Object.keys(values).map((key) => {
+        const formField = fields.find((field) => field.name === key);
+        let value = values[key];
+
+        // not in field, probably source_name or type, handled seperately
+        if (!formField) {
+            return undefined;
+        }
+
+        // do not show hidden fields
+        if (formField.type === 'hidden') {
+            return undefined;
+        }
+
+        // Boolean value convert to Yes / No
+        if (typeof values[key] === 'boolean') {
+            value = values[key] ? 'Yes' : 'No';
+        }
+
+        return (
+            <React.Fragment key={ key }>
+                <TextListItem component={ TextListItemVariants.dt }>{ formField.label }</TextListItem>
+                <TextListItem component={ TextListItemVariants.dd }>{ value }</TextListItem>
+            </React.Fragment>
+        );
+    });
+
+    return (<TextContent>
+        <TextList component={ TextListVariants.dl }>
+            <TextListItem component={ TextListItemVariants.dt }>{ 'Name' }</TextListItem>
+            <TextListItem component={ TextListItemVariants.dd }>{ values.source_name }</TextListItem>
+            <TextListItem component={ TextListItemVariants.dt }>{ 'Source Type' }</TextListItem>
+            <TextListItem component={ TextListItemVariants.dd }>{ type.product_name }</TextListItem>
+            { valuesList }
+        </TextList>
+    </TextContent>);
 };
 
 SourceWizardSummary.propTypes = {
