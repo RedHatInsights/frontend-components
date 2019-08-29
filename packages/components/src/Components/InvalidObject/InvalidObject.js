3a3c4bc2d8dba135c4b44932ea7be1867a9a2b05
Ryan Felton <rfelton@redhat.com>
2019-08-29 13:58:33 -0400

    added new invalid-component component

---
+++ packages/components/src/Components/InvalidObject/InvalidObject.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Icon404 from './icon-404';
+import {
+    Title,
+    Button
+} from '@patternfly/react-core';
+import { withRouter } from 'react-router-dom';
+
+const InvalidObject = (props) => {
+    return (
+        <section className="pf-l-page__main-section pf-c-page__main-section ins-c-component_invalid-componet">
+            <Title size='3xl'>This doesn&apos;t seem to exist.</Title>
+            <Icon404/>
+            <Title size='xl' className='ins-c-text__sorry'>Sorry, we couldn&apos;t find what you are looking for.
+                    This component does not exist.</Title>
+            <Button
+                variant="link"
+                onClick={ () => { props.history.goBack(); } }>
+                    Go Back
+            </Button>
+        </section>
+    );
+};
+
+InvalidObject.propTypes = {
+    history: PropTypes.shape({
+        goBack: PropTypes.func
+    })
+};
+
+InvalidObject.defaultProps = {
+    history: {
+        push: () => undefined,
+        goBack: () => undefined
+    }
+};
+
+export default withRouter(InvalidObject);
