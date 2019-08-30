885471a20758f02ba5fc0e80a8fdf685cd258593
Ryan Felton <rfelton@redhat.com>
2019-08-30 10:51:27 -0400

    test improvements

--- packages/components/src/Components/InvalidObject/InvalidObject.test.js
+++ packages/components/src/Components/InvalidObject/InvalidObject.test.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { shallow, mount } from 'enzyme';
 import toJson from 'enzyme-to-json';
 import InvalidObject from './InvalidObject';
 
 describe('InvalidObject component', () => {
     it('should render', () => {
-        const wrapper = shallow(<InvalidObject/>)
+        const wrapper = mount(<Router><InvalidObject/></Router>)
         expect(toJson(wrapper)).toMatchSnapshot();
     });
 })
