ea94408f9e8f54d20859f04193b294b17830e12f
Richard Vsiansky <r.vsia@seznam.cz>
2019-09-23 12:37:35 +0200

    Support to hide take me to sources button

--- packages/sources/src/addSourceWizard/steps/FinishedStep.js
+++ packages/sources/src/addSourceWizard/steps/FinishedStep.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 import { EmptyStateBody, EmptyState, EmptyStateIcon, Title, Button, EmptyStateSecondaryActions, EmptyStateVariant } from '@patternfly/react-core';
 import { CheckCircleIcon } from '@patternfly/react-icons';
 
-const FinishedStep = ({ onClose, successfulMessage }) => <div className="pf-l-bullseye">
+const FinishedStep = ({ onClose, successfulMessage, hideSourcesButton }) => <div className="pf-l-bullseye">
     <EmptyState variant={ EmptyStateVariant.full }>
         <EmptyStateIcon icon={ CheckCircleIcon } color="var(--pf-global--success-color--100)" />
         <Title headingLevel="h5" size="lg">
@@ -14,17 +14,20 @@ const FinishedStep = ({ onClose, successfulMessage }) => <div className="pf-l-bu
             { successfulMessage }
         </EmptyStateBody>
         <Button variant="primary" onClick={ onClose }>Back to my application</Button>
+        { !hideSourcesButton &&
         <EmptyStateSecondaryActions>
             <a href='/hybrid/settings/sources'>
                 <Button variant="link">Take me to sources</Button>
             </a>
         </EmptyStateSecondaryActions>
+        }
     </EmptyState>
 </div>;
 
 FinishedStep.propTypes = {
     onClose: PropTypes.func.isRequired,
-    successfulMessage: PropTypes.node.isRequired
+    successfulMessage: PropTypes.node.isRequired,
+    hideSourcesButton: PropTypes.bool.isRequired
 };
 
 export default FinishedStep;
