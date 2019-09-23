dfcb14b331959942e41d564cf075417e7f5b1578
Karel Hala <khala@redhat.com>
2019-09-23 13:38:51 +0200

    Use calculated time in order to keep updated date in compliance

--- packages/inventory-compliance/src/SystemPolicyCard.test.js
+++ packages/inventory-compliance/src/SystemPolicyCard.test.js
@@ -1,20 +1,22 @@
 import React from 'react';
-import { mount } from 'enzyme';
+import { render } from 'enzyme';
 import toJson from 'enzyme-to-json';
 import SystemPolicyCard from './SystemPolicyCard';
 import { IntlProvider } from 'react-intl';
 
 describe('SystemPolicyCard component', () => {
     it('should render', () => {
+        const currentTime = new Date();
+        currentTime.setMonth(currentTime.getMonth() - 6);
         const policy = {
             rulesPassed: 30,
             rulesFailed: 10,
-            lastScanned: '2019-03-06T06:20:13Z',
+            lastScanned: currentTime.toISOString(),
             refId: 'xccdf_org.ssgproject.content_profile_pci-dss',
             name: 'PCI-DSS v3 Control Baseline for Red Hat Enterprise Linux 7',
             compliant: false
         };
-        const wrapper = mount(
+        const wrapper = render(
             <IntlProvider locale={ navigator.language }>
                 <SystemPolicyCard policy={ policy } />
             </IntlProvider>
