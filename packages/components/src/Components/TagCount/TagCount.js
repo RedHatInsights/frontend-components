f73c0f34df7843008cab801c3f9ce78c700dda34
Ryan Felton <rfelton@redhat.com>
2019-09-26 10:17:08 -0400

    switched to PF Button and PF icon

--- packages/components/src/Components/TagCount/TagCount.js
+++ packages/components/src/Components/TagCount/TagCount.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './tagCount.scss';
+import { Button } from '@patternfly/react-core';
+import { TagIcon } from '@patternfly/react-icons'
 
 const TagCount = ({count, onTagClick, className, ...props}) => {
     return (
-        <button {...props} className={`ins-c-tag-count ${className}`} onClick={onTagClick}>
-            <i className="fas fa-tag"></i>
+        <Button  {...props} variant="plain" className={`ins-c-tag-count ${className}`} onClick={onTagClick}>
+            <TagIcon />
             <span>{count}</span>
-        </button>
+        </Button>
     )
 }
 
