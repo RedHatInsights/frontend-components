a919f7c6f3acbae6c9ad103d203d4f836c51fef6
Allen Wight <awight@redhat.com>
2019-10-09 10:54:09 -0400

    Intlhelper provides access to intl for a non-components (constants) 🤗

--- packages/translations/src/index.js
+++ packages/translations/src/index.js
@@ -1,3 +1,4 @@
 export { default as IntlProvider, updateLocaleData } from './Provider';
 export { default as defaultMessages } from './messages';
 export const LOCALSTORAGE_KEY = 'rhcs-language';
+export { default as intlHelper } from './intlHelper';
