e5d1a73bb1564a7e64ffa40e33a983c514e70233
Jiri Dostal <jdostal@redhat.com>
2019-10-15 16:25:23 +0200

    Add tooltips to impacts

--- packages/components/src/Components/Shield/Shield.js
+++ packages/components/src/Components/Shield/Shield.js
@@ -1,9 +1,10 @@
+import { Tooltip } from '@patternfly/react-core';
 import { QuestionIcon, SecurityIcon } from '@patternfly/react-icons';
 import propTypes from 'prop-types';
 import React from 'react';
 import { colorList, impactList } from './consts';
 
-const Shield = ({ impact, hasLabel, size }) => {
+const Shield = ({ impact, hasLabel, hasTooltip, size }) => {
     const unknownLabel = 'Unknown';
     const attributes = impactList[impact] || { title: unknownLabel, color: colorList.default };
     const badge = impactList[impact] ? (
@@ -11,10 +12,18 @@ const Shield = ({ impact, hasLabel, size }) => {
     ) : (
         <QuestionIcon aria-hidden="false" aria-label={unknownLabel} size={size} color={colorList.default} />
     );
-
-    return (
+    const body = (
         <span>
             {badge} {hasLabel && attributes.title}
+        </span>
+    );
+    return (
+        <span>
+            {(hasTooltip && (
+                <Tooltip content={<div>{attributes.message}</div>} position={'bottom'}>
+                    {body}
+                </Tooltip>
+            )) || { body }}
         </span>
     );
 };
@@ -22,14 +31,16 @@ const Shield = ({ impact, hasLabel, size }) => {
 Shield.defaultProps = {
     impact: 'N/A',
     hasLabel: false,
-    size: 'sm'
+    size: 'sm',
+    hasTooltip: true
 };
 
 Shield.propTypes = {
     impact: propTypes.oneOfType([ propTypes.string, propTypes.number ]),
     hasLabel: propTypes.bool,
     size: propTypes.string, // sm, md, lg and xl,
-    label: propTypes.bool
+    label: propTypes.bool,
+    hasTooltip: propTypes.bool
 };
 
 export default Shield;
