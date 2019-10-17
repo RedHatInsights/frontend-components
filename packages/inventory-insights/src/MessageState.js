8580b6e6b8b163cce56a5b2adb5c6246505a819a
Allen Wight <awight@redhat.com>
2019-10-17 16:16:30 -0400

    Updates table check marks (ansible/empty state) to align with ux spec
    fixes https://projects.engineering.redhat.com/browse/RHCLOUD-2759
    fixes https://projects.engineering.redhat.com/browse/RHCLOUD-2266

--- packages/inventory-insights/src/MessageState.js
+++ packages/inventory-insights/src/MessageState.js
@@ -3,23 +3,25 @@ import { EmptyState, EmptyStateBody, EmptyStateIcon, EmptyStateVariant, Title } 
 import PropTypes from 'prop-types';
 import { CubesIcon } from '@patternfly/react-icons';
 
-const MessageState = ({ children, icon, iconStyle, text, title, variant }) => (
-    <EmptyState variant={ variant }>
-        <EmptyStateIcon style={ iconStyle } icon={ icon } size="lg" />
-        <Title headingLevel="h5" size="lg">
-            { title }
+const MessageState = ({ children, icon, iconClass, iconStyle, size, text, title, variant }) => (
+    <EmptyState variant={variant}>
+        {icon !== 'none' && <EmptyStateIcon className={iconClass} style={iconStyle} icon={icon} size={size} />}
+        <Title headingLevel='h5' size='lg'>
+            {title}
         </Title>
-        <EmptyStateBody style={ { marginBottom: '16px' } }>
-            { text }
+        <EmptyStateBody style={{ marginBottom: '16px' }}>
+            {text}
         </EmptyStateBody>
-        { children }
+        {children}
     </EmptyState>
 );
 
 MessageState.propTypes = {
     children: PropTypes.any,
     icon: PropTypes.any,
+    iconClass: PropTypes.any,
     iconStyle: PropTypes.any,
+    size: PropTypes.string,
     text: PropTypes.any,
     title: PropTypes.string,
     variant: PropTypes.any
@@ -28,7 +30,8 @@ MessageState.propTypes = {
 MessageState.defaultProps = {
     icon: CubesIcon,
     title: '',
-    variant: EmptyStateVariant.full
+    variant: EmptyStateVariant.full,
+    size: ''
 };
 
 export default MessageState;
