4a178fcddb21f159e5cdb81ae728eb18ebcb8165
PanSpagetka <rknaur@redhat.com>
2019-10-29 09:19:43 +0100

    Add tooltip for DateFormat

--- packages/components/src/Components/DateFormat/DateFormat.js
+++ packages/components/src/Components/DateFormat/DateFormat.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { dateStringByType } from './helper';
+import { dateByType } from './helper';
 
 export default function DateFormat({ date, type = 'relative' }) {
     const dateObj = date instanceof Date ? date : new Date(date);
     const dateType = dateObj.toString() === 'Invalid Date' ? 'invalid' : type;
     return (
         <React.Fragment>
-            {dateStringByType(dateType)(dateObj)}
+            {dateByType(dateType)(dateObj)}
         </React.Fragment>
     );
 }
