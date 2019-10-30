196f55a051866c87a2992876f3a96cf5e873b51f
Allen Wight <awight@redhat.com>
2019-10-30 11:59:06 -0400

    Adds primary toolbar support to inv-insight (n other bugs)
    fixes https://projects.engineering.redhat.com/browse/RHCLOUD-2901

--- packages/inventory-insights/src/Constants.js
+++ packages/inventory-insights/src/Constants.js
@@ -1,24 +1,20 @@
+/* eslint-disable camelcase */
 export const BASE_FETCH_URL = '/api/insights/v1/';
 
-export const FILTER_CATEGORIES = [
-    {
-        type: 'checkbox', title: 'Ansible support', urlParam: 'has_playbook', values: [
-            { label: 'Has support', value: true },
-            { label: 'No support', value: false }
+export const FILTER_CATEGORIES = {
+    has_playbook: {
+        type: 'radio', title: 'Ansible support', urlParam: 'has_playbook', values: [
+            { label: 'Ansible remediation support', value: 'true' },
+            { label: 'No Ansible remediation support', value: 'false' },
+            { label: 'All rules', value: 'all' }
         ]
-    }, {
+    },
+    total_risk: {
         type: 'checkbox', title: 'Total risk', urlParam: 'total_risk', values: [
             { label: 'Critical', value: '4' },
             { label: 'Important', value: '3' },
             { label: 'Moderate', value: '2' },
             { label: 'Low', value: '1' }
         ]
-    }, {
-        type: 'checkbox', title: 'Risk of change', urlParam: 'risk', values: [
-            { label: 'Critical', value: '4' },
-            { label: 'Important', value: '3' },
-            { label: 'Moderate', value: '2' },
-            { label: 'Low', value: '1' }
-        ]
     }
-];
+};
