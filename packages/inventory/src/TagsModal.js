d21d80ea212e528dfb889c6cb553aa26f82efed8
Karel Hala <khala@redhat.com>
2019-11-18 15:02:50 +0100

    Use tags from API instead of mocked ones

--- packages/inventory/src/TagsModal.js
+++ packages/inventory/src/TagsModal.js
@@ -4,31 +4,23 @@ import PropTypes from 'prop-types';
 import { toggleTagModal } from './redux/actions';
 import { TagModal, Skeleton, SkeletonSize } from '@redhat-cloud-services/frontend-components';
 
-const TagsModal = ({ showTagDialog, tagsLoaded, tags, activeSystemTag, tagCount, onToggleTagModal }) => {
+const TagsModal = ({ showTagDialog, tags, activeSystemTag, tagCount, onToggleTagModal }) => {
     return (
         <TagModal
             width="auto"
             isOpen={ showTagDialog }
             toggleModal={() => onToggleTagModal()}
             systemName={ `${activeSystemTag.display_name} (${tagCount})` }
-            rows={
-                tagsLoaded ?
-                    tags.map(({ tagName, tagValue }) => ([ tagName, tagValue ])) :
-                    [ ...Array(tagCount) ].map(() => ([
-                        { title: <Skeleton size={ SkeletonSize.md }>&nbsp;</Skeleton> },
-                        { title: <Skeleton size={SkeletonSize.md}>&nbsp;</Skeleton> }
-                    ]))
-            }
+            rows={ tags.map(({ value, namespace }) => ([ value, namespace ])) }
         />
     );
 };
 
 TagsModal.propTypes = {
     showTagDialog: PropTypes.bool,
-    tagsLoaded: PropTypes.bool,
     tags: PropTypes.arrayOf(PropTypes.shape({
-        tagName: PropTypes.node,
-        tagValue: PropTypes.node
+        value: PropTypes.node,
+        namespace: PropTypes.node
     })),
     tagCount: PropTypes.number,
     activeSystemTag: PropTypes.shape({
@@ -48,13 +40,12 @@ TagsModal.defaultProps = {
 };
 
 export default connect(({ entities, entityDetails }) => {
-    const { showTagDialog, tagsLoaded, tags, activeSystemTag, tagCount } = entities || entityDetails || {};
+    const { showTagDialog, activeSystemTag = { tags: [] } } = entities || entityDetails || {};
     return ({
         showTagDialog,
-        tagsLoaded,
-        tags,
+        tags: activeSystemTag.tags,
         activeSystemTag,
-        tagCount
+        tagCount: activeSystemTag.tags.length
     });
 }, (dispatch) => ({
     onToggleTagModal: () => dispatch(toggleTagModal(false))
