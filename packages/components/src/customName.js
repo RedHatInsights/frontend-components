43834f405a4379aa071e5e65995c7919e1920c43
Karel Hala <khala@redhat.com>
2019-11-28 14:53:50 +0100

    Use sync reader

--- packages/components/src/customName.js
+++ packages/components/src/customName.js
@@ -1,10 +1,7 @@
-const util = require('util');
 const { join } = require('path');
-const fs = require('fs');
-const readFile = (fileName) => util.promisify(fs.readFile)(fileName, 'utf8');
-const readJson = (fileName) => readFile(fileName).then(JSON.parse);
+const { readFileSync } = require('fs');
 
-const mapper = readJson(join(__dirname, './fileMapper.json')) || {};
+const mapper = JSON.parse(readFileSync((join(__dirname, './fileMapper.json')))) || {};
 
 module.exports = (name) => {
     return `@redhat-cloud-services/frontend-components/components/${mapper[name] || name}`;
