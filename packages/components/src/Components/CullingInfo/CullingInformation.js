f83fa4d04916d9779daabbf906017b91f5ff6fe5
Karel Hala <khala@redhat.com>
2020-02-05 15:58:47 +0100

    Add culling component to be used accross other apps

---
+++ packages/components/src/Components/CullingInfo/CullingInformation.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import ExclamationTriangleIcon from '@patternfly/react-icons/dist/js/icons/exclamation-triangle-icon';
+import ExclamationCircleIcon from '@patternfly/react-icons/dist/js/icons/exclamation-circle-icon';
+import { Tooltip } from '@patternfly/react-core/dist/js/components/Tooltip/Tooltip';
+import './CullingInformation.scss';
+
+const seconds = 1000;
+const minutes = seconds * 60;
+const hours = minutes * 60;
+const days = hours * 24;
+
+const calculateTooltip = (culled, warning) => {
+    const culledDate = new Date(culled);
+    const warningDate = new Date(warning);
+    const currDate = new Date();
+    const diffTime = currDate - warningDate;
+    const removeIn = Math.ceil((culledDate - currDate) / days);
+    if (diffTime >= 0 && removeIn <= 1) {
+        return {
+            isError: true,
+            msg: `This system is scheduled for removal from inventory`
+        };
+    }
+
+    return {
+        isWarn: diffTime >= 0,
+        msg: `This system will be removed from the inventory in ${removeIn} days`
+    };
+};
+
+const CullingInformation = ({ culled, className, staleWarning, children, ...props }) => {
+    const { isWarn, isError, msg } = calculateTooltip(culled, staleWarning);
+    return <Tooltip
+        { ...props }
+        content={msg}
+        position="bottom"
+    >
+        <span className={
+            isWarn ? 'ins-c-inventory__culling-warning' :
+                isError ? 'ins-c-inventory__culling-danger' : ''
+        }>
+            { isError && <ExclamationCircleIcon className="ins-c-inventory__culling-danger"/> }
+            { isWarn && <ExclamationTriangleIcon className="ins-c-inventory__culling-warning"/> }
+            {children}
+        </span>
+    </Tooltip>;
+};
+
+CullingInformation.propTypes = {
+    culled: PropTypes.oneOfType([ PropTypes.string, PropTypes.number, PropTypes.instanceOf(Date) ]),
+    staleWarning: PropTypes.oneOfType([ PropTypes.string, PropTypes.number, PropTypes.instanceOf(Date) ])
+};
+CullingInformation.defaultProps = {
+    culled: new Date(0),
+    staleWarning: new Date(0)
+};
+export default CullingInformation;
