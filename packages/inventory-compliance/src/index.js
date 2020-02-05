abdca7ddf5b997177782408cf28a67661801e836
Sebastian Gräßl <mail@bastilian.me>
2020-02-05 12:16:57 +0100

    Export ComplianceRemediationButton and make minor changes to use in compliance frontend

--- packages/inventory-compliance/src/index.js
+++ packages/inventory-compliance/src/index.js
@@ -1,2 +1,3 @@
 export { default as default } from './Compliance';
 export { default as SystemRulesTable } from './SystemRulesTable';
+export { default as ComplianceRemediationButton } from './ComplianceRemediationButton';
