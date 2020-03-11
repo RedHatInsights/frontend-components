57c7e1db197a9846f4719877e966607a8523b30f
Serhii Zakharov <szakharo@redhat.com>
2020-03-11 16:12:43 +0100

    fixed icons

--- packages/rule-components/src/ReportDetails/RuleFeedback.js
+++ packages/rule-components/src/ReportDetails/RuleFeedback.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import ThumbsUpIcon from '@patternfly/react-icons/dist/js/icons/outlined-thumbs-up-icon';
+import ThumbsDownIcon from '@patternfly/react-icons/dist/js/icons/outlined-thumbs-down-icon';
 
 const RuleFeedback = ({ ruleId }) => (
     <div>
         <div>
             <span>Is this rule helpful?</span>
-            <i className="far fa-thumbs-up"/>
-            <i className="far fa-thumbs-down"/>
+            <ThumbsUpIcon/>
+            <ThumbsDownIcon/>
         </div>
     </div>
 );
