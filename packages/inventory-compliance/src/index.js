b5c67c804ed158ab33b19f1c49e70ac9702a0475
Sebastian Gräßl <mail@bastilian.me>
2020-03-28 23:21:53 +0100

    Export filter utilities

--- packages/inventory-compliance/src/index.js
+++ packages/inventory-compliance/src/index.js
@@ -1,3 +1,4 @@
 export { default as default } from './Compliance';
 export { default as SystemRulesTable } from './SystemRulesTable';
 export { default as ComplianceRemediationButton } from './ComplianceRemediationButton';
+export { ChipBuilder, FilterBuilder, FilterConfigBuilder } from './Utilities';
