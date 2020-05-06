62ed52823d8a71f287027b4a7e7049d2b0b5e875
Karel Hala <khala@redhat.com>
2020-05-06 14:11:53 +0200

    Add frontend-assets as endpoint for RH fonts

--- packages/pdf-generator/src/utils/fonts.js
+++ packages/pdf-generator/src/utils/fonts.js
@@ -1,5 +1,7 @@
 const fontBaseUrl = 'https://overpass-30e2.kxcdn.com/';
-const RHFontUrl = 'https://raw.githubusercontent.com/RedHatOfficial/RedHatFont/master/TTF/RedHatDisplay';
+const RHFontUrl = (window.location.hostname.includes('foo.redhat.com') || window.location.hostname.includes('cloud.redhat.com'))
+    ? `${window.location.origin}/apps/frontend-assets/fonts/RedHat/RedHatDisplay`
+    : 'https://raw.githubusercontent.com/RedHatOfficial/RedHatFont/master/TTF/RedHatDisplay';
 
 export const fontTypes = [ 'thin', 'extralight', 'light', 'semibold', 'bold', 'extrabold', 'heavy' ];
 
