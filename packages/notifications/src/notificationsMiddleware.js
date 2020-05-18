c16c39fe658c9b2632fea49f25d4c7577099da68
Karel Hala <khala@redhat.com>
2020-05-18 16:44:35 +0200

    Pass correctly request ID if present, do not or create sentryId if reuestId present

--- packages/notifications/src/notificationsMiddleware.js
+++ packages/notifications/src/notificationsMiddleware.js
@@ -24,7 +24,8 @@ const prepareErrorMessage = (payload, errorTitleKey, errorDescriptionKey) => {
     return {
         title: get(payload, titleKey) || 'Error',
         description: get(payload, descriptionKey),
-        sentryId: payload && payload.sentryId
+        sentryId: payload && payload.sentryId,
+        requestId: payload && payload.requestId
     };
 };
 
@@ -69,7 +70,8 @@ const createNotificationsMiddleware = (options = {}) => {
                 dispatch(addNotification({
                     ...defaultNotificationOptions,
                     ...notifications.rejected,
-                    sentryId: action.payload && action.payload.sentryId
+                    sentryId: action.payload && action.payload.sentryId,
+                    requestId: action.payload && action.payload.requestId
                 }));
             }
         }
