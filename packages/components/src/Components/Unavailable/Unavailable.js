9684d88121b7260f22218cfe0baf814463b03072
Ryan Long <rlong@redhat.com>
2020-05-18 14:06:15 -0400

    add 500 component

---
+++ packages/components/src/Components/Unavailable/Unavailable.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+import { Stack } from '@patternfly/react-core/dist/js/layouts/Stack/Stack';
+import { StackItem } from '@patternfly/react-core/dist/js/layouts/Stack/StackItem';
+
+import { Title } from '@patternfly/react-core/dist/js/components/Title/Title';
+
+import { EmptyState, EmptyStateVariant } from '@patternfly/react-core/dist/js/components/EmptyState/EmptyState';
+import { EmptyStateBody } from '@patternfly/react-core/dist/js/components/EmptyState/EmptyStateBody';
+import { EmptyStateIcon } from '@patternfly/react-core/dist/js/components/EmptyState/EmptyStateIcon';
+
+import ExclamationCircleIcon from '@patternfly/react-icons/dist/js/icons/exclamation-circle-icon';
+
+const Unavailable = () => {
+    return(
+        <EmptyState variant={EmptyStateVariant.large} className='ins-c-empty-state__unavailable'>
+            <EmptyStateIcon icon={ExclamationCircleIcon} />
+            <Title headingLevel="h5" size="lg">
+                Unable to connect
+            </Title>
+            <EmptyStateBody>
+                <Stack>
+                    <StackItem>
+                        This page is temporarily unavailable. Try refreshing the page.
+                    </StackItem>
+                    <StackItem>
+                        If the problem persists, conact your administrator.
+                    </StackItem>
+                    <StackItem>
+                        See <a href='https://cloud.redhat.com/status' target='_blank' rel='noopener noreferrer'>cloud.redhat.com/status</a> for more information.
+                    </StackItem>
+                </Stack>
+            </EmptyStateBody>
+        </EmptyState>
+    );
+};
+
+export default Unavailable;
