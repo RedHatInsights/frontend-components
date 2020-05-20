89f4ca0479568f18a090f3cffc3014763f987a46
Ryan Long <rlong@redhat.com>
2020-05-20 09:27:42 -0400

    use new wording and update tests

--- packages/components/src/Components/Unavailable/Unavailable.js
+++ packages/components/src/Components/Unavailable/Unavailable.js
@@ -1,8 +1,5 @@
 import React from 'react';
 
-import { Stack } from '@patternfly/react-core/dist/js/layouts/Stack/Stack';
-import { StackItem } from '@patternfly/react-core/dist/js/layouts/Stack/StackItem';
-
 import { Title } from '@patternfly/react-core/dist/js/components/Title/Title';
 
 import { EmptyState, EmptyStateVariant } from '@patternfly/react-core/dist/js/components/EmptyState/EmptyState';
@@ -16,20 +13,11 @@ const Unavailable = () => {
         <EmptyState variant={EmptyStateVariant.large} className='ins-c-empty-state__unavailable pf-m-redhat-font'>
             <EmptyStateIcon icon={ExclamationCircleIcon} />
             <Title headingLevel="h5" size="lg">
-                Unable to connect
+                This page is temporarily unavailable
             </Title>
             <EmptyStateBody>
-                <Stack>
-                    <StackItem>
-                        This page is temporarily unavailable. Try refreshing the page.
-                    </StackItem>
-                    <StackItem>
-                        If the problem persists, conact your administrator.
-                    </StackItem>
-                    <StackItem>
-                        See <a href='./status' target='_blank' rel='noopener noreferrer'>cloud.redhat.com/status</a> for more information.
-                    </StackItem>
-                </Stack>
+                Try refreshing the page. If the problem persists, contact your organization administrator or visit our
+                <a href='./status' target='_blank' rel='noopener noreferrer'> status page</a> for known outages.
             </EmptyStateBody>
         </EmptyState>
     );
