31b58b6b7efb2a39b44cb7bc1bd26a41e9b9f0fb
Daniel Lobato Garcia <me@daniellobato.me>
2020-06-02 10:22:36 +0200

    RHICOMPL-646 - Sync empty states in Reports/SCAP policies
    The empty state should also give some info to the user about existing
    policies, as the current message is a bit confusing. This shows how many
    policies exist (but are not reporting).

--- packages/inventory-compliance/src/ComplianceEmptyState.js
+++ packages/inventory-compliance/src/ComplianceEmptyState.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import propTypes from 'prop-types';
+import { ApolloClient, HttpLink, InMemoryCache } from 'apollo-boost';
 import {
     Title,
     TextContent,
@@ -11,9 +12,36 @@ import {
     EmptyStateIcon
 } from '@patternfly/react-core';
 import { CloudSecurityIcon } from '@patternfly/react-icons';
+import gql from 'graphql-tag';
+import { useQuery } from '@apollo/react-hooks';
+import { Spinner } from '@redhat-cloud-services/frontend-components';
+import { ErrorCard } from './PresentationalComponents';
+const COMPLIANCE_API_ROOT = '/api/compliance';
 
-const ComplianceEmptyState = ({ title, mainButton }) => (
-    <Bullseye>
+const QUERY = gql`
+{
+    profiles(search: "external = false") {
+        totalCount
+    }
+}
+`;
+
+const ComplianceEmptyState = ({ title, mainButton, client }) => {
+    let { data, error, loading } = useQuery(QUERY, { fetchPolicy: 'network-only', client: client });
+
+    if (loading) { return <Spinner/>; }
+
+    if (error) {
+        const errorMsg = `Oops! Error loading System data: ${error}`;
+        return <ErrorCard message={errorMsg} />;
+    }
+
+    const policiesCount = data.profiles.totalCount;
+
+    const policyWord = policiesCount > 1 ? 'policies' : 'policy';
+    const haveWord = policiesCount > 1 ? 'have' : 'has';
+
+    return <Bullseye>
         <EmptyState>
             <EmptyStateIcon style={{ fontWeight: '500', color: 'var(--pf-global--primary-color--100)' }}
                 size="xl" title="Compliance" icon={CloudSecurityIcon} />
@@ -21,6 +49,11 @@ const ComplianceEmptyState = ({ title, mainButton }) => (
             <Title size="lg">{ title }</Title>
             <br/>
             <EmptyStateBody>
+                { policiesCount > 0 ?
+                    <TextContent>
+                        <a href='insights/compliance/scappolicies'>{policiesCount} {policyWord}</a> {haveWord} been created but {haveWord} no reports.
+                    </TextContent> :
+                    '' }
                 <TextContent>
                     The Compliance service uses SCAP policies to track your organization&#39;s adherence to
                     compliance requirements.
@@ -39,12 +72,13 @@ const ComplianceEmptyState = ({ title, mainButton }) => (
                 </Button>
             </EmptyStateSecondaryActions>
         </EmptyState>
-    </Bullseye>
-);
+    </Bullseye>;
+};
 
 ComplianceEmptyState.propTypes = {
     title: propTypes.string,
-    mainButton: propTypes.object
+    mainButton: propTypes.object,
+    client: propTypes.object
 };
 
 ComplianceEmptyState.defaultProps = {
@@ -54,7 +88,14 @@ ComplianceEmptyState.defaultProps = {
         component="a"
         href="/insights/compliance/scappolicies">
         Create new policy
-    </Button>
+    </Button>,
+    client: new ApolloClient({
+        link: new HttpLink({
+            uri: COMPLIANCE_API_ROOT + '/graphql',
+            credentials: 'include'
+        }),
+        cache: new InMemoryCache()
+    })
 };
 
 export default ComplianceEmptyState;
--- packages/inventory-compliance/src/ComplianceEmptyState.test.js
+++ packages/inventory-compliance/src/ComplianceEmptyState.test.js
@@ -2,9 +2,37 @@ import React from 'react';
 import toJson from 'enzyme-to-json';
 import { shallow } from 'enzyme';
 import ComplianceEmptyState from './ComplianceEmptyState';
+import { useQuery } from '@apollo/react-hooks';
+jest.mock('@apollo/react-hooks');
+jest.mock('apollo-boost');
 
 describe('ComplianceEmptyState', () => {
-    it('expect to render without error', () => {
+    it('expect to render without error if no policies exist', () => {
+        useQuery.mockImplementation(() => ({
+            data: { profiles: { totalCount: 0 } }, error: false, loading: false
+        }));
+        const wrapper = shallow(
+            <ComplianceEmptyState />
+        );
+
+        expect(toJson(wrapper)).toMatchSnapshot();
+    });
+
+    it('expect to render different message if one policy exists', () => {
+        useQuery.mockImplementation(() => ({
+            data: { profiles: { totalCount: 1 } }, error: false, loading: false
+        }));
+        const wrapper = shallow(
+            <ComplianceEmptyState />
+        );
+
+        expect(toJson(wrapper)).toMatchSnapshot();
+    });
+
+    it('expect to render different message if many policies exist', () => {
+        useQuery.mockImplementation(() => ({
+            data: { profiles: { totalCount: 2 } }, error: false, loading: false
+        }));
         const wrapper = shallow(
             <ComplianceEmptyState />
         );
