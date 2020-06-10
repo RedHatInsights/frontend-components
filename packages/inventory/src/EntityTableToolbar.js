e164193bae6a8354625c8075b08ccd0aa78cbd8a
Martin Maroši <marvusm.mmi@gmail.com>
2020-06-10 15:55:49 +0200

    Update inventory component to PF4v4

--- packages/inventory/src/EntityTableToolbar.js
+++ packages/inventory/src/EntityTableToolbar.js
@@ -4,7 +4,7 @@ import React, { Component, Fragment } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { Skeleton, SkeletonSize, PrimaryToolbar } from '@redhat-cloud-services/frontend-components';
-import { Spinner } from '@patternfly/react-core/dist/esm/experimental';
+import { Spinner } from '@patternfly/react-core';
 import { fetchAllTags, clearFilters, entitiesLoading, toggleTagModal } from './redux/actions';
 import debounce from 'lodash/debounce';
 import { InventoryContext } from './Inventory';
