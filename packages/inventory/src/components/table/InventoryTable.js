52f2d2b1b015ccfd098e776c872ff246e597b614
Karel Hala <khala@redhat.com>
2020-07-02 12:21:01 +0200

    Use custom react and react-redux

--- packages/inventory/src/components/table/InventoryTable.js
+++ packages/inventory/src/components/table/InventoryTable.js
@@ -18,63 +18,61 @@ const InventoryTable = forwardRef(({
     sortBy: propsSortBy,
     ...props
 }, ref) => {
-    console.log(React, 'Inventory react');
-    return <div>aaaa</div>;
-    // const hasItems = Boolean(items);
-    // let pagination = {
-    //     page: useSelector(({ entities: { page: invPage } }) => (
-    //         (hasItems && page) || invPage || 1)
-    //     ),
-    //     perPage: useSelector(({ entities: { perPage: invPerPage } }) => (
-    //         (hasItems && perPage) || invPerPage || 50)
-    //     ),
-    //     total: useSelector(({ entities: { total: invTotal } }) => (
-    //         (hasItems && (total || items?.length)) || invTotal)
-    //     )
-    // };
-    // const sortBy = useSelector(({ entities: { sortBy: invSortBy } }) => (
-    //     (hasItems && propsSortBy) || invSortBy
-    // ));
+    const hasItems = Boolean(items);
+    let pagination = {
+        page: useSelector(({ entities: { page: invPage } }) => (
+            (hasItems && page) || invPage || 1)
+        ),
+        perPage: useSelector(({ entities: { perPage: invPerPage } }) => (
+            (hasItems && perPage) || invPerPage || 50)
+        ),
+        total: useSelector(({ entities: { total: invTotal } }) => (
+            (hasItems && (total || items?.length)) || invTotal)
+        )
+    };
+    const sortBy = useSelector(({ entities: { sortBy: invSortBy } }) => (
+        (hasItems && propsSortBy) || invSortBy
+    ));
 
-    // return (
-    //     <Fragment>
-    //         <EntityTableToolbar
-    //             { ...props }
-    //             items={ items }
-    //             onRefresh={onRefresh}
-    //             filters={ filters }
-    //             hasItems={ hasItems }
-    //             total={ pagination.total }
-    //             page={ pagination.page }
-    //             perPage={ pagination.perPage }
-    //             showTags={ showTags }
-    //         >
-    //             { children }
-    //         </EntityTableToolbar>
-    //         <InventoryList
-    //             { ...props }
-    //             ref={ref}
-    //             hasItems={ hasItems }
-    //             onRefresh={ onRefresh }
-    //             items={ items }
-    //             page={ pagination.page }
-    //             sortBy={ sortBy }
-    //             perPage={ pagination.perPage }
-    //             showTags={ showTags }
-    //         />
-    //         <TableToolbar isFooter className="ins-c-inventory__table--toolbar">
-    //             <Pagination
-    //                 isFull
-    //                 items={ items }
-    //                 total={ pagination.total }
-    //                 page={ pagination.page }
-    //                 perPage={ pagination.perPage }
-    //                 hasItems={ hasItems }
-    //                 onRefresh={ onRefresh }
-    //             />
-    //         </TableToolbar>
-    //     </Fragment>
-    // );
+    return (
+        <Fragment>
+            <EntityTableToolbar
+                { ...props }
+                items={ items }
+                onRefresh={onRefresh}
+                filters={ filters }
+                hasItems={ hasItems }
+                total={ pagination.total }
+                page={ pagination.page }
+                perPage={ pagination.perPage }
+                showTags={ showTags }
+            >
+                { children }
+            </EntityTableToolbar>
+            <InventoryList
+                { ...props }
+                ref={ref}
+                hasItems={ hasItems }
+                onRefresh={ onRefresh }
+                items={ items }
+                page={ pagination.page }
+                sortBy={ sortBy }
+                perPage={ pagination.perPage }
+                showTags={ showTags }
+            />
+            <TableToolbar isFooter className="ins-c-inventory__table--toolbar">
+                <Pagination
+                    isFull
+                    items={ items }
+                    total={ pagination.total }
+                    page={ pagination.page }
+                    perPage={ pagination.perPage }
+                    hasItems={ hasItems }
+                    onRefresh={ onRefresh }
+                />
+            </TableToolbar>
+        </Fragment>
+    );
 });
 
 export default InventoryTable;
--- packages/inventory/src/shared/Wrapper.js
+++ packages/inventory/src/shared/Wrapper.js
@@ -1,23 +1,18 @@
 import React, { PureComponent, createRef } from 'react';
 import PropTypes from 'prop-types';
-import { Provider } from 'react-redux';
+
 class RenderWrapper extends PureComponent {
     ref = createRef();
 
     render() {
-        console.log(React, 'Inventory react');
         const { cmp: Component, inventoryRef, store, ...props } = this.props;
-        return store ?
-            <Provider store={store}>
-                <div>aaa</div>
-            </Provider> :
-            <Component
-                {...props}
-                { ...inventoryRef && {
-                    ref: inventoryRef
-                }}
-                store={ store }
-            />;
+        return <Component
+            {...props}
+            { ...inventoryRef && {
+                ref: inventoryRef
+            }}
+            store={ store }
+        />;
     }
 }
 
