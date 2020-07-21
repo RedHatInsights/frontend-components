c2f291e64a6f83035275c6ae3b70a58bc93c9152
Ryan Long <rlong@redhat.com>
2020-07-21 15:28:54 -0400

    add maintenance component (#630)
    * add maintenance component
    
    * newline
    
    * use new icong
    
    * add period at the end
    
    Co-authored-by: Karel Hala <khala@redhat.com>

---
+++ packages/components/src/Components/Maintenance/Maintenance.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import toJson from 'enzyme-to-json';
+import Maintenance from './Maintenance';
+
+describe('Maintenance component', () => {
+    it('should render with default props', () => {
+        const wrapper = shallow(<Maintenance/>);
+        expect(toJson(wrapper)).toMatchSnapshot();
+    });
+
+    it('should render with times', () => {
+        const wrapper = shallow(<Maintenance startTime='12am' endTime='2am'/>);
+        expect(toJson(wrapper)).toMatchSnapshot();
+    });
+
+    it('should render with new description', () => {
+        const wrapper = shallow(<Maintenance description={<span> test </span>}/>);
+        expect(toJson(wrapper)).toMatchSnapshot();
+    });
+});
