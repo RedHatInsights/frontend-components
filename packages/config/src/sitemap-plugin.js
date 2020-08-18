aeac24e2a090e8a4fb35a3c615ace38bae10a373
PanSpagetka <rknaur@redhat.com>
2020-08-18 12:30:17 +0200

    Use appname for sitemap

--- packages/config/src/sitemap-plugin.js
+++ packages/config/src/sitemap-plugin.js
@@ -11,9 +11,10 @@ class SitemapPlugin {
         compiler.hooks.emit.tapAsync('SitemapPlugin', (compilation, callback) => {
             const packageJson = require(resolve(this.rootFolder, 'package.json'));
             const prefix = packageJson && packageJson.routes && packageJson.routes.prefix;
+            const appname = packageJson && packageJson.routes && packageJson.routes.appname;
 
             const paths = Object.entries(packageJson && packageJson.routes || {}).filter(([ key ]) => key !== 'prefix').map(value => value[1].path || value[1]);
-            const hostname = `https://cloud.redhat.com/${prefix}/`;
+            const hostname = `https://cloud.redhat.com/${prefix}/${appname}`;
 
             const sitemap = Sitemap.sitemapBuilder(hostname, paths);
             fs.writeFileSync(`${resolve(this.rootFolder, 'sitemap.xml')}`, sitemap.toString());
