a9d32dad5f77cc79865f79f98cfc578c803bb8ac
Karel Hala <khala@redhat.com>
2020-08-25 13:09:52 +0200

    Include all inventory and remediations dependencies

--- packages/utils/src/inventoryDependencies.js
+++ packages/utils/src/inventoryDependencies.js
@@ -1,49 +1,55 @@
-
 import {
+    ClipboardCopy,
     Dropdown,
     DropdownItem,
     DropdownPosition,
     KebabToggle,
     DropdownToggle,
-    DropdownToggleCheckbox
-} from '@patternfly/react-core/dist/esm/components/Dropdown';
-import { TextInput } from '@patternfly/react-core/dist/esm/components/TextInput/TextInput';
-import { Grid } from '@patternfly/react-core/dist/esm/layouts/Grid/Grid';
-import { GridItem } from '@patternfly/react-core/dist/esm/layouts/Grid/GridItem';
-import { Tab } from '@patternfly/react-core/dist/esm/components/Tabs/Tab';
-import { Tabs } from '@patternfly/react-core/dist/esm/components/Tabs/Tabs';
-import { Button } from '@patternfly/react-core/dist/esm/components/Button/Button';
-import { Checkbox } from '@patternfly/react-core/dist/esm/components/Checkbox/Checkbox';
-import { Switch } from '@patternfly/react-core/dist/esm/components/Switch/Switch';
-import { Form } from '@patternfly/react-core/dist/esm/components/Form/Form';
-import { Label } from '@patternfly/react-core/dist/esm/components/Label';
-import {
+    DropdownToggleCheckbox,
+    TextInput,
+    Grid,
+    GridItem,
+    Tab,
+    Tabs,
+    Button,
+    Checkbox,
+    Switch,
+    Form,
+    FormGroup,
+    FormSelect,
+    FormSelectOption,
+    Label,
     EmptyState,
     EmptyStateBody,
     EmptyStateIcon,
-    EmptyStateVariant
-} from '@patternfly/react-core/dist/esm/components/EmptyState';
-import { Select, SelectOption, SelectVariant, SelectGroup } from '@patternfly/react-core/dist/esm/components/Select';
-import { Radio } from '@patternfly/react-core/dist/esm/components/Radio/Radio';
-import { ChipGroup, Chip } from '@patternfly/react-core/dist/esm/components/ChipGroup';
-import { Title } from '@patternfly/react-core/dist/esm/components/Title/Title';
-import { Split } from '@patternfly/react-core/dist/esm/layouts/Split/Split';
-import { SplitItem } from '@patternfly/react-core/dist/esm/layouts/Split/SplitItem';
-import { Spinner } from '@patternfly/react-core/dist/esm/components/Spinner/Spinner';
-import { Flex } from '@patternfly/react-core/dist/esm/layouts/Flex/Flex';
-import { FlexItem } from '@patternfly/react-core/dist/esm/layouts/Flex/FlexItem';
-import { Bullseye } from '@patternfly/react-core/dist/esm/layouts/Bullseye/Bullseye';
-import { Pagination, PaginationVariant } from '@patternfly/react-core/dist/esm/components/Pagination';
-import { Badge } from '@patternfly/react-core/dist/esm/components/Badge/Badge';
-import { Tooltip } from '@patternfly/react-core/dist/esm/components/Tooltip/Tooltip';
-import { Modal } from '@patternfly/react-core/dist/esm/components/Modal/Modal';
-import { Stack } from '@patternfly/react-core/dist/esm/layouts/Stack/Stack';
-import { StackItem } from '@patternfly/react-core/dist/esm/layouts/Stack/StackItem';
-import { Level } from '@patternfly/react-core/dist/esm/layouts/Level/Level';
-import { LevelItem } from '@patternfly/react-core/dist/esm/layouts/Level/LevelItem';
-import { ClipboardCopy } from '@patternfly/react-core/dist/esm/components/ClipboardCopy/ClipboardCopy';
-import { Toolbar, ToolbarItem, ToolbarContent, ToolbarGroup } from '@patternfly/react-core/dist/esm/components/Toolbar';
-import {
+    EmptyStateVariant,
+    Select,
+    SelectOption,
+    SelectVariant,
+    SelectGroup,
+    Radio,
+    ChipGroup,
+    Chip,
+    Title,
+    Split,
+    SplitItem,
+    Spinner,
+    Flex,
+    FlexItem,
+    Bullseye,
+    Pagination,
+    PaginationVariant,
+    Badge,
+    Tooltip,
+    Modal,
+    Stack,
+    StackItem,
+    Level,
+    LevelItem,
+    Toolbar,
+    ToolbarItem,
+    ToolbarContent,
+    ToolbarGroup,
     Drawer,
     DrawerPanelContent,
     DrawerContent,
@@ -52,10 +58,12 @@ import {
     DrawerActions,
     DrawerHead,
     DrawerCloseButton
-} from '@patternfly/react-core/dist/esm/components/Drawer';
+} from '@patternfly/react-core';
 
-import CubesIcon from '@patternfly/react-icons/dist/esm/icons/cubes-icon';
-import ExclamationTriangleIcon from  '@patternfly/react-icons/dist/esm/icons/exclamation-triangle-icon';
+import {
+    CubesIcon,
+    ExclamationTriangleIcon
+} from '@patternfly/react-icons';
 
 export const reactIcons = {
     CubesIcon,
@@ -118,5 +126,8 @@ export const reactCore = {
     DrawerCloseButton,
     Switch,
     Form,
-    Label
+    Label,
+    FormGroup,
+    FormSelect,
+    FormSelectOption
 };
