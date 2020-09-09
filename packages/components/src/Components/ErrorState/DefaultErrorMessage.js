533445a5c516e2eaba7e130a177e3e3bb8d265e4
Alexander Rivera <hq@aleriver@redhat.com-mac>
2020-09-09 08:53:27 -0400

    cleaning defaultMessage

--- packages/components/src/Components/ErrorState/DefaultErrorMessage.js
+++ packages/components/src/Components/ErrorState/DefaultErrorMessage.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import propTypes from 'prop-types';
 
-const DefaultErrorMessage = ({redirectLink}) => {
+const DefaultErrorMessage = () => {
     const redirectLink = 'https://access.redhat.com/support';
+    const statusLink = 'status.redhat.com';
 
     return (
         <>
                 If the problem persists, contact <a href={redirectLink}>
-                Red Hat Supprt</a> or check out our <a href="status.redhat.com"> status page</a> for known outages.
+                Red Hat Supprt</a> or check out our <a href={statusLink}> status page</a> for known outages.
         </>
     )
 }
