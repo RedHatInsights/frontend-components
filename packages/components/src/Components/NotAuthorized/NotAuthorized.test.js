7625375875c5d82bc75df95c4afc6b5e3af57d1f
Karel Hala <khala@redhat.com>
2020-09-24 10:09:58 +0200

    Parametrize not authorized component, add isLoading flag to rbac hook (#743)
    * Parametrize not authorized component, add isLoading flag to rbac hook
    
    * Update inv no access snap

--- packages/components/src/Components/NotAuthorized/NotAuthorized.test.js
+++ packages/components/src/Components/NotAuthorized/NotAuthorized.test.js
@@ -1,11 +1,31 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import toJson from 'enzyme-to-json';
 import NotAuthorized from './NotAuthorized';
 
 describe('NotAuthorized component', () => {
     it('should render', () => {
         const wrapper = shallow(<NotAuthorized serviceName='Foo' />);
+        expect(toJson(wrapper)).toMatchSnapshot();
+    });
+
+    it('should apply custom styles', () => {
+        const wrapper = shallow(<NotAuthorized className="something" />);
+        expect(toJson(wrapper)).toMatchSnapshot();
+    });
+
+    it('should use custom icon', () => {
+        const wrapper = mount(<NotAuthorized icon={() => 'some Icon!'} />);
+        expect(toJson(wrapper, { mode: 'deep' })).toMatchSnapshot();
+    });
+
+    it('should not show buttons', () => {
+        const wrapper = shallow(<NotAuthorized showReturnButton={false} />);
+        expect(toJson(wrapper)).toMatchSnapshot();
+    });
+
+    it('should show custom description', () => {
+        const wrapper = shallow(<NotAuthorized description="Some text" />);
         expect(toJson(wrapper)).toMatchSnapshot();
     });
 });
