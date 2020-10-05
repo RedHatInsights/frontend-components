2d9a424685532765e725891ab2c734809369d681
Karel Hala <khala@redhat.com>
2020-10-05 15:34:17 +0200

    Fix infinit detail loading by using custom component for detail wrapper (#759)

---
+++ packages/inventory/src/components/detail/DetailRenderer.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { usePermissions } from '@redhat-cloud-services/frontend-components-utilities/files/RBACHook';
+import { Spinner } from '@patternfly/react-core/dist/js/components/Spinner';
+import DetailWrapper from './DetailWrapper';
+import AccessDenied from '../../shared/AccessDenied';
+
+const DetailRenderer = ({ showInventoryDrawer, ...props }) => {
+    const { hasAccess } = usePermissions('inventory', [ 'inventory:*:*', 'inventory:hosts:read' ]);
+    if (hasAccess === undefined) {
+        return <Spinner />;
+    } else if (hasAccess === false) {
+        return <AccessDenied />;
+    } else {
+        return showInventoryDrawer ? <DetailWrapper {...props} /> : <React.Fragment {...props} />;
+    }
+};
+
+DetailWrapper.propTypes = {
+    showInventoryDrawer: PropTypes.bool
+};
+
+DetailWrapper.defaultProps = {
+    showInventoryDrawer: false
+};
+
+export default DetailRenderer;
