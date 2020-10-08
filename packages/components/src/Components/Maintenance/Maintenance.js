89706614a8e2a177d0d64f1d7ad2664f4ca00c29
Alexander Rivera <56621323+arivepr@users.noreply.github.com>
2020-10-08 09:08:06 -0400

    Maintenance component utc update (#721)
    * Changes to maintenance
    
    * Cleaning up comments
    
    * pr changes requests
    
    * Addressing pr suggestions
    
    * pr tested pass
    
    * cleaning lint and updating snapshots
    
    * fixed test
    
    * taking this out
    
    * finally removing this
    
    * resetting package lock
    
    Co-authored-by: Alexander Rivera <hq@aleriver@redhat.com-mac>
    Co-authored-by: Ryan Long <rlong@redhat.com>
    Co-authored-by: Karel Hala <khala@redhat.com>
    Co-authored-by: Martin Maroši <marvusm.mmi@gmail.com>

--- packages/components/src/Components/Maintenance/Maintenance.js
+++ packages/components/src/Components/Maintenance/Maintenance.js
@@ -1,25 +1,32 @@
 import React from 'react';
 import propTypes from 'prop-types';
+import classNames from 'classnames';
 import {
     EmptyState, EmptyStateBody,
     Title,
     Stack, StackItem, EmptyStateIcon
 } from '@patternfly/react-core';
 import { HourglassHalfIcon } from '@patternfly/react-icons';
-
 import './maintenance.scss';
 
-const Maintenance = ({ startTime, endTime, timeZone, description }) => {
+const Maintenance = ({ utcStartTime, utcEndTime, startTime, endTime, timeZone, description, redirectLink, className, ...props }) => {
+
+    const emptyStateClassName = classNames(
+        className,
+        'ins-c-empty-state__maintenance'
+    );
+
     return (
-        <EmptyState className='ins-c-empty-state__maintenance'>
+        <EmptyState className={emptyStateClassName} {...props}>
             <EmptyStateIcon icon={HourglassHalfIcon}/>
             <Title headingLevel='h4' size='lg'>Maintenance in progress</Title>
             <EmptyStateBody>
                 { description
                     ? description
                     : <Stack>
-                        <StackItem>We are currently undergoing scheduled maintenance from {startTime}-{endTime} {timeZone}.</StackItem>
-                        <StackItem>We will be back shortly, thank you for your patience.</StackItem>
+                        <StackItem>We are currently undergoing scheduled maintenance and will be</StackItem>
+                        <StackItem>unavailable from from {utcStartTime}-{utcEndTime} UTC ({startTime}-{endTime} {timeZone}).</StackItem>
+                        <StackItem>For more information please visit <a href={redirectLink}>status.redhat.com</a></StackItem>
                     </Stack>
                 }
             </EmptyStateBody>
@@ -28,16 +35,22 @@ const Maintenance = ({ startTime, endTime, timeZone, description }) => {
 };
 
 Maintenance.propTypes = {
+    utcStartTime: propTypes.string,
+    utcEndTime: propTypes.string,
     startTime: propTypes.string,
     endTime: propTypes.string,
     timeZone: propTypes.string,
-    description: propTypes.node
+    description: propTypes.node,
+    redirectLink: propTypes.string
 };
 
 Maintenance.defaultProps = {
+    utcStartTime: '10am',
+    utcEndTime: '12am',
     startTime: '6am',
     endTime: '8am',
-    timeZone: 'EST'
+    timeZone: 'EST',
+    redirectLink: 'https://status.redhat.com/incidents'
 };
 
 export default Maintenance;
