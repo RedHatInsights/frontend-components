7a85da566c6227f8414de850a4a6a7c692100379
Katerina Patticha <apattich@redhat.com>
2020-10-19 13:17:13 +0200

    Fix indentation

--- packages/pdf-generator/src/DownloadButton.js
+++ packages/pdf-generator/src/DownloadButton.js
@@ -5,62 +5,68 @@ import { Button } from '@patternfly/react-core';
 import PDFDocument from './components/PDFDocument';
 
 class DownloadButton extends React.Component {
-    state = {
-        asyncPages: []
-    };
-    render() {
-        const {
-            fileName,
-            label,
-            isPreview,
-            asyncFunction,
-            buttonProps,
-            ...props
-        } = this.props;
+  state = {
+      asyncPages: []
+  };
+  render() {
+    console.log(new Date())
+    const {
+        fileName,
+        label,
+        isPreview,
+        asyncFunction,
+        buttonProps,
+        ...props
+    } = this.props;
 
-        return (
-            <React.Fragment>
-                { isPreview ? <PDFViewer>
-                    <PDFDocument { ...props } />
-                </PDFViewer> : asyncFunction ? <React.Fragment>
-                    <Button onClick={ () => {
-                        this.setState({
-                            asyncPages: []
-                        }, () => {
-                            Promise.resolve(asyncFunction()).then(asyncPages => this.setState({
-                                asyncPages
-                            }));
-                        });
-                    }} { ...buttonProps }>{label}</Button>
-                    {this.state.asyncPages.length > 0 && (
-                        <BlobProvider document={<PDFDocument { ...props } pages={this.state.asyncPages} />}>
-                            {({ blob }) => {
-                                if (blob) {
-                                    const link = document.createElement('a');
-                                    link.href = URL.createObjectURL(blob);
-                                    link.download = fileName;
-                                    document.body.append(link);
-                                    link.click();
-                                    link.remove();
-                                    this.setState({
-                                        asyncPages: []
-                                    });
-                                }
+    return (
+      <React.Fragment>
+        { isPreview 
+          ? <PDFViewer>
+              <PDFDocument { ...props } />
+            </PDFViewer> 
+          : asyncFunction 
+            ? <React.Fragment>
+                <Button onClick={ () => {
+                    this.setState({
+                        asyncPages: []
+                    }, () => {
+                        Promise.resolve(asyncFunction()).then(asyncPages => this.setState({
+                            asyncPages
+                        }));
+                    });
+                }} { ...buttonProps }>{label}</Button>
+                {this.state.asyncPages.length > 0 && (
+                    <BlobProvider document={<PDFDocument { ...props } pages={this.state.asyncPages} />}>
+                        {({ blob }) => {
+                            if (blob) {
+                                const link = document.createElement('a');
+                                link.href = URL.createObjectURL(blob);
+                                link.download = fileName;
+                                document.body.append(link);
+                                link.click();
+                                link.remove();
+                                this.setState({
+                                    asyncPages: []
+                                });
+                            }
 
-                                return <React.Fragment />;
-                            }}
-                        </BlobProvider>
-                    )}
-                </React.Fragment> : <PDFDownloadLink
-                    document={<PDFDocument { ...props } />}
-                    fileName={fileName}
-                    {...props}
-                >
-                    {label}
-                </PDFDownloadLink> }
-            </React.Fragment>
-        );
-    }
+                            return <React.Fragment />;
+                        }}
+                    </BlobProvider>
+                )}
+              </React.Fragment> 
+            : <PDFDownloadLink
+                document={<PDFDocument { ...props } />}
+                fileName={fileName}
+                {...props}
+              >
+                {label}
+              </PDFDownloadLink> 
+      }
+      </React.Fragment>
+    );
+  }
 }
 
 DownloadButton.propTypes = {
