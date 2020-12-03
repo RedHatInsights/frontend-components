d714d0f449a99cef7e5f0dbcd82211056a3d25e5
Richard Vsiansky <r.vsia@seznam.cz>
2020-12-03 13:29:09 +0100

    Update types selection step

--- packages/sources/src/utilities/filterApps.js
+++ packages/sources/src/utilities/filterApps.js
@@ -1,5 +1,17 @@
+import { CLOUD_VENDOR, getActiveVendor, REDHAT_VENDOR } from './stringConstants';
+
 export const TOPOLOGY_INV_NAME = '/insights/platform/topological-inventory';
 
 const filterApps = (type) => type.name !== TOPOLOGY_INV_NAME;
+
+export const filterVendorAppTypes = (sourceTypes) => {
+    const activeVendor = getActiveVendor();
+
+    return ({ supported_source_types }) => supported_source_types.find(type =>
+        activeVendor === CLOUD_VENDOR
+            ? sourceTypes.find(({ name }) => type === name)?.vendor !== REDHAT_VENDOR
+            : sourceTypes.find(({ name }) => type === name)?.vendor === REDHAT_VENDOR
+    );
+};
 
 export default filterApps;
