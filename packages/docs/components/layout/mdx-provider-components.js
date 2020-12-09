a368269bdb0f418d7052f6f2c25dc1837cfd36e1
Martin Marosi <marvusm.mmi@gmail.com>
2020-12-09 09:53:50 +0100

    Clean up page layout.

--- packages/docs/components/layout/mdx-provider-components.js
+++ packages/docs/components/layout/mdx-provider-components.js
@@ -1,5 +1,4 @@
 import { Title } from '@patternfly/react-core';
-import '@patternfly/react-styles/css/components/Table/table';
 
 const H1 = ({ className, ...props }) => <Title className="pf-u-mb-lg" headingLevel="h1" {...props} />;
 const H2 = ({ className, ...props }) => <Title className="pf-u-mb-lg" headingLevel="h2" {...props} />;
