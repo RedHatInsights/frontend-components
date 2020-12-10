0cf4a9e52ede6e53866734a4a46357b6f592d70d
Martin Marosi <marvusm.mmi@gmail.com>
2020-12-10 14:34:44 +0100

    Add copy to cliboard for example and some styling.

--- packages/docs/components/example-component/expandable-panel.js
+++ packages/docs/components/example-component/expandable-panel.js
@@ -1,10 +1,35 @@
-import React, { useState } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { createUseStyles } from 'react-jss';
 import classnames from 'classnames';
-import { Button, Card, CardBody, Toolbar, ToolbarContent, ToolbarItem } from '@patternfly/react-core';
-import { CodeIcon } from '@patternfly/react-icons';
+import { Alert, Button, Toolbar, ToolbarContent, ToolbarItem } from '@patternfly/react-core';
+import { CodeIcon, CopyIcon } from '@patternfly/react-icons';
 import CodeHighlight from './code-highlight';
+
+const useToastStyles = createUseStyles({
+    toast: {
+        position: 'fixed !important',
+        bottom: 'var(--pf-global--spacer--md)',
+        right: 'var(--pf-global--spacer--md)',
+        cursor: 'pointer'
+    }
+});
+
+const TimedToas = ({ handleClose, title }) => {
+    const classes = useToastStyles();
+    useEffect(() => {
+        const timeout = setTimeout(handleClose, 3000);
+        return () => {
+            clearTimeout(timeout);
+        };
+    }, []);
+    return (<Alert onClick={handleClose} className={classnames(classes.toast)} title={title} variant="info" />);
+};
+
+TimedToas.propTypes = {
+    handleClose: PropTypes.func.isRequired,
+    title: PropTypes.string.isRequired
+};
 
 const useStyles = createUseStyles({
     inconExpandExpanded: {
@@ -12,42 +37,66 @@ const useStyles = createUseStyles({
     },
     iconExpand: {
         transition: 'transform .3s'
+    },
+    toolbar: {
+        backgroundColor: 'transparent !important'
+    },
+    firstItem: {
+        marginLeft: 'auto'
     }
 });
 
 const ExpandablePanel = ({ sourceCode }) => {
     const [ isOpen, setIsOpen ] = useState(false);
+    const [ toast, setToast ] = useState(undefined);
+    const handleClose = () => setToast(undefined);
     const classes = useStyles();
 
+    const copyToClipboard = () => {
+        const textarea = document.createElement('textarea');
+        document.body.appendChild(textarea);
+        textarea.style.width = 0;
+        textarea.style.height = 0;
+        textarea.value = sourceCode;
+        textarea.select();
+        document.execCommand('copy');
+        document.body.removeChild(textarea);
+        if (typeof toast === 'undefined') {
+            setToast({ title: 'The code was copied to clipboard' });
+        }
+    };
+
     return (
-        <Card>
-            <CardBody className="pf-u-p-sm">
-                <Toolbar className={classnames('pf-u-p-0')}>
-                    <ToolbarContent>
-                        <ToolbarItem>
-                            <Button variant="plain" onClick={() => setIsOpen(prev => !prev)}>
-                                <CodeIcon className={classnames(classes.iconExpand, {
-                                    [classes.inconExpandExpanded]: isOpen
-                                })} />
-                            </Button>
-                        </ToolbarItem>
-                        <ToolbarItem>
-                            <Button>
-                                GH source link
-                            </Button>
-                        </ToolbarItem>
-                        <ToolbarItem>
-                            <Button>
-                                Code sandbox example link
-                            </Button>
-                        </ToolbarItem>
-                    </ToolbarContent>
-                </Toolbar>
-                {isOpen && (
-                    <CodeHighlight sourceCode={sourceCode} />
-                )}
-            </CardBody >
-        </Card>
+        <Fragment>
+            {toast && <TimedToas {...toast} handleClose={handleClose} />}
+            <Toolbar className={classnames('pf-u-p-0 pf-u-mb-md', classes.toolbar)}>
+                <ToolbarContent className="pf-u-p-0">
+                    <ToolbarItem className={classes.firstItem}>
+                        <Button variant="plain" onClick={() => setIsOpen(prev => !prev)}>
+                            <CodeIcon className={classnames(classes.iconExpand, {
+                                [classes.inconExpandExpanded]: isOpen
+                            })} />
+                        </Button>
+                        <Button variant="plain" onClick={copyToClipboard}>
+                            <CopyIcon />
+                        </Button>
+                    </ToolbarItem>
+                    <ToolbarItem>
+                        <Button>
+                            GH source link
+                        </Button>
+                    </ToolbarItem>
+                    <ToolbarItem>
+                        <Button>
+                            Code sandbox example link
+                        </Button>
+                    </ToolbarItem>
+                </ToolbarContent>
+            </Toolbar>
+            {isOpen && (
+                <CodeHighlight sourceCode={sourceCode} />
+            )}
+        </Fragment>
     );
 };
 
