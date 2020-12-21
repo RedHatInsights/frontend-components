6b40efaa8769860bfe5053810bcf1e6a9a483b3d
Martin Marosi <marvusm.mmi@gmail.com>
2020-12-21 11:08:30 +0100

    Add missing PF table styles to docs.

--- packages/docs/pages/_app.js
+++ packages/docs/pages/_app.js
@@ -1,6 +1,7 @@
 import Layout from '../components/layout';
 import { MDXProvider } from '@mdx-js/react';
 import components from '../components/layout/mdx-provider-components';
+import '@patternfly/react-styles/css/components/Table/table.css';
 
 import '../styles/globals.css';
 
