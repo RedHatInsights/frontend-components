135e9baf200c67e3a027b70bac0489c8ade0a5e4
Martin Marosi <marvusm.mmi@gmail.com>
2020-12-22 09:09:30 +0100

    Add missing FEC css.

--- packages/docs/examples/Battery/medium-severity.js
+++ packages/docs/examples/Battery/medium-severity.js
@@ -15,15 +15,15 @@ const useStyle = createUseStyles({
     }
 });
 
-const LowSeverity = () => {
+const MediumSeverity = () => {
     const classes = useStyle();
     return (
         <div className={classes.container}>
-            <Battery label="With prop: 1" severity={1} />
-            <Battery label="With prop: low" severity="low" />
-            <Battery label="With prop: info" severity="info" />
+            <Battery label="With prop: 2" severity={2} />
+            <Battery label="With prop: medium" severity="medium" />
+            <Battery label="With prop: warn" severity="warn" />
         </div>
     );
 };
 
-export default LowSeverity;
+export default MediumSeverity;
