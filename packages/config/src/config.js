b162c1f04e1f5134fa54ba65bb9a3d3407db83f3
Martin Marosi <marvusm.mmi@gmail.com>
2021-01-07 16:18:56 +0100

    Remove  contentBasePublicPath: publicPath from shared webpack config.
    As of webpack verion > 5.10.0, this configuration is breaking the static
    assets path in webpack dev server. Removing the options fixes the
    issues.

--- packages/config/src/config.js
+++ packages/config/src/config.js
@@ -120,7 +120,6 @@ module.exports = ({
             inline: true,
             disableHostCheck: true,
             historyApiFallback: true,
-            contentBasePublicPath: publicPath,
             writeToDisk: true
         }
     };
