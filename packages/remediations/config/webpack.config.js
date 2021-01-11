5d9d15a4b64176276219322b193c5e5908735904
Martin Marosi <marvusm.mmi@gmail.com>
2021-01-11 13:03:35 +0100

    Add Remediations button to UMD output.

--- packages/remediations/config/webpack.config.js
+++ packages/remediations/config/webpack.config.js
@@ -14,6 +14,7 @@ module.exports = (env) => ({
     },
     entry: {
         index: [ './src/index.js', './src/RemediationButton.js' ],
+        RemediationButton: './src/RemediationButton.js',
         remediationsApi: './src/api/index.js'
     },
     output: {
