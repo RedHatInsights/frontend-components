1cbdbd17e85882e99b118e2c19c74bb45f2f7add
Martin Marosi <marvusm.mmi@gmail.com>
2021-01-11 10:29:07 +0100

    Remove stringify from render loader.

--- packages/config/src/chrome-render-loader.js
+++ packages/config/src/chrome-render-loader.js
@@ -1,5 +1,5 @@
 function chromeRenderLoader(source) {
-    return `var isChrome2 = window.insights && window.insights.chrome && window.insights.chrome.isChrome2 || false; if(!isChrome2){${JSON.stringify(source)}}`;
+    return `var isChrome2 = window.insights && window.insights.chrome && window.insights.chrome.isChrome2 || false; if(!isChrome2){${source}}`;
 }
 
 module.exports = chromeRenderLoader;
