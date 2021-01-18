ad4f47414daca711faba06f55e1f0298fb7dbc20
Filip Hlavac <filip.hlavac01@gmail.com>
2021-01-18 18:26:23 +0100

    Add fetch error state

--- packages/remediations/src/NewRemediationWizard.js
+++ packages/remediations/src/NewRemediationWizard.js
@@ -1,28 +1,37 @@
-import React, { Component, Fragment, createContext } from 'react';
+import React, { Component, Fragment } from 'react';
 import keyBy from 'lodash/keyBy';
 import transform from 'lodash/transform';
 import FormRenderer from '@data-driven-forms/react-form-renderer/dist/esm/form-renderer';
 import Pf4FormTemplate from '@data-driven-forms/pf4-component-mapper/dist/esm/form-template';
 import schemaBuilder from './schema';
+import { remediationUrl } from './utils';
+import * as api from './api';
 import Deferred from '@redhat-cloud-services/frontend-components-utilities/files/Deffered';
 import Wizard from '@data-driven-forms/pf4-component-mapper/dist/esm/wizard';
 import componentTypes from '@data-driven-forms/react-form-renderer/dist/esm/component-types';
 import SelectPlaybook from './newSteps/selectPlaybook';
 import ReviewActions from './newSteps/reviewActions';
 import IssueResolution from './newSteps/issueResolution';
-import * as api from './api';
+import FetchError from './newSteps/fetchError';
+import Review from './newSteps/review';
 
-const RemediationWizardContext = createContext({
-    success: false,
-    submitting: false,
-    error: undefined
-});
+function createNotification (id, name, isNewSwitch) {
+    const verb = isNewSwitch ? 'created' : 'updated';
+
+    return {
+        variant: 'success',
+        title: `Playbook ${verb}`,
+        description: <span>You have successfully { isNewSwitch ? 'created' : 'updated' } <a href={ remediationUrl(id) } >{ name }</a>.</span>,
+        dismissable: true
+    };
+}
 
 class RemediationWizard extends Component {
 
     state = {
         open: false,
-        schema: {}
+        schema: {},
+        errors: []
     }
 
     constructor() {
@@ -36,9 +45,9 @@ class RemediationWizard extends Component {
 
     getFormTemplate = (props) => <Pf4FormTemplate {...props} showFormControls={false} />;
 
-    getIssuesMultiple = (data, issuesById) => (
+    getIssues = (data, issuesById, getResolution) =>
         data.issues.map(issue => {
-            const resolutions = this.getResolution(issue.id);
+            const resolutions = getResolution(issue.id);
             const { description, needs_reboot: needsReboot  } = resolutions?.[0] || {};
             return {
                 action: issuesById[issue.id].description,
@@ -49,37 +58,31 @@ class RemediationWizard extends Component {
                 shortId: issue?.id?.split('|')?.slice(-1)?.[0] || issue.id,
                 alternate: resolutions.length - 1
             };
-        }).filter(record => record.alternate > 1));
+        });
 
-    openWizard = (data) => {
+    openWizard = (data, basePath) => {
         const deferred = new Deferred();
         const issuesById = keyBy(data.issues, issue => issue.id);
 
         this.loadResolutions(data.issues).then(
             (values) => {
                 this.setState(values);
-                const issuesMultiple = this.getIssuesMultiple(data, issuesById);
+                const issuesMultiple = this.getIssues(data, issuesById, this.getResolution).filter(record => record.alternate > 1);
                 this.setState({
                     open: true,
-
+                    data,
+                    deferred,
+                    basePath,
+                    onRemediationCreated: data.onRemediationCreated,
                     schema: schemaBuilder(this.container.current, issuesMultiple),
 
-                    wizardContextValue: {
-                        success: false,
-                        submitting: false,
-                        error: undefined,
-                        hideForm: false,
-                        issues: data.issues,
-                        systems: data.systems
-                    },
                     mapperExtension: {
                         'select-playbook': {
-                            component: SelectPlaybook,
+                            component: this.state.errors.length > 0 ? FetchError : SelectPlaybook,
                             SelectPlaybookProps: {
                                 issues: data.issues,
                                 systems: data.systems
-                            },
-                            loadResolutions: this.loadResolutions
+                            }
                         },
                         'review-actions': {
                             component: ReviewActions,
@@ -89,8 +92,14 @@ class RemediationWizard extends Component {
                         'issue-resolution': {
                             component: IssueResolution,
                             systems: data.systems,
-                            issuesById: issuesById,
                             getResolution: this.getResolution
+                        },
+                        review: {
+                            component: Review,
+                            data: data,
+                            issuesById,
+                            getIssues: this.getIssues,
+                            resolutions: this.state.resolutions
                         }
                     }
                 });
@@ -99,12 +108,6 @@ class RemediationWizard extends Component {
 
         return deferred.promise;
     }
-
-    setWizardError = (error) => this.setState({ wizardContextValue: { ...this.state.wizardContextValue, error } });
-
-    setWizardSuccess = (success) => this.setState({ wizardContextValue: { ...this.state.wizardContextValue, success } });
-
-    setHideForm = (hideForm) => this.setState({ wizardContextValue: { ...this.state.wizardContextValue, hideForm } });
 
     closeWizard = () => {
         this.setOpen(false);
@@ -137,33 +140,54 @@ class RemediationWizard extends Component {
 
     getResolution = issueId => this.state.resolutions.find(r => r.id === issueId)?.resolutions || [];
 
+    resolver = () => (id, name, isNewSwitch) => (
+        this.state.onRemediationCreated({
+            remediation: { id, name },
+            getNotification: () => createNotification(id, name, isNewSwitch)
+        })
+    );
+
     render () {
 
         return (
             this.state.open ?
-                <RemediationWizardContext.Provider
-                    value={{ ...this.state.wizardContextValue,
-                        setWizardError: this.setWizardError,
-                        setWizardSuccess: this.setWizardSuccess,
-                        setHideForm: this.setHideForm }}>
-                    <FormRenderer
-                        schema={this.state.schema}
-                        container={this.container}
-                        subscription={{ values: true }}
-                        FormTemplate={this.getFormTemplate}
-                        initialValues={{
-                            multiple: !!this.state.resolutions?.find(r => r.resolutions.length > 1),
-                            'manual-resolution': true,
-                            'selected-resolutions': {}
-                        }}
-                        componentMapper={{
-                            [componentTypes.WIZARD]: Wizard,
-                            ...this.state.mapperExtension
-                        }}
-                        onSubmit={() => undefined}
-                        onCancel={this.closeWizard}
-                    />
-                </RemediationWizardContext.Provider>
+                <FormRenderer
+                    schema={this.state.schema}
+                    container={this.container}
+                    subscription={{ values: true }}
+                    FormTemplate={this.getFormTemplate}
+                    initialValues={{
+                        multiple: !!this.state.resolutions?.find(r => r.resolutions.length > 1),
+                        'manual-resolution': true,
+                        'selected-resolutions': {}
+                    }}
+                    componentMapper={{
+                        [componentTypes.WIZARD]: Wizard,
+                        ...this.state.mapperExtension
+                    }}
+                    onSubmit={(_, formOptions) => {
+                        const values = formOptions.getState().values;
+                        const issues = this.state.data.issues.map(({ id }) => ({
+                            id,
+                            resolution: values['manual-resolution'] ? this.getResolution(id)?.[0]?.id : undefined,
+                            systems: this.state.data.systems
+                        }));
+                        const add = { issues, systems: this.state.data.systems };
+                        if (values['existing-playbook-selected']) {
+                            const { id, name } = values['existing-playbook'];
+                            // eslint-disable-next-line camelcase
+                            api.patchRemediation(id, { add, auto_reboot: values['auto-reboot'] }, this.state.basePath)
+                            .then(() => this.resolver(this.state.deferred)(id, name, false));
+                        } else {
+                            // eslint-disable-next-line camelcase
+                            api.createRemediation({ name: values['select-playbook'], add, auto_reboot: values['auto-reboot'] }, this.state.basePath)
+                            .then(({ id }) => this.resolver(this.state.deferred)(id, values['select-playbook'], true));
+                        }
+
+                        this.setOpen(false);
+                    }}
+                    onCancel={this.closeWizard}
+                />
                 : <Fragment/>
         );
     }
