da1a66ee50b3466f65ea8546ffbf015c0a619774
Karel Hala <khala@redhat.com>
2021-01-22 14:59:17 +0100

    Add post install script to utils package

---
+++ packages/utils/postinstall.js
@@ -0,0 +1,16 @@
+const { readdirSync } = require('fs');
+const { copySync } = require('fs-extra');
+
+const getDirectories = source => readdirSync(source, { withFileTypes: true })
+.filter(dirent => dirent.isDirectory())
+.map(dirent => dirent.name);
+
+copySync('./styles', './Utilities');
+
+const files = getDirectories('./').filter(item => ![ 'esm', 'files', 'doc', 'src', 'node_modules' ].includes(item));
+
+files.map(file => copySync(`./${file}`, `./files/${file}`));
+files.map(file => copySync(`./${file}`, `./files/cjs/${file}`));
+copySync('./esm', './files/esm');
+copySync('./index.js', './files/index.js');
+copySync('./index.js', './files/cjs/index.js');
