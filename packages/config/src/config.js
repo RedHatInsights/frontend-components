75ca61c2e61dbea571a3e811cada2ff3a7b7debf
Martin Marosi <marvusm.mmi@gmail.com>
2021-02-18 09:26:33 +0100

    fix(config): ignore webpack proxy with deployment

--- packages/config/src/config.js
+++ packages/config/src/config.js
@@ -13,7 +13,8 @@ module.exports = ({
     appName,
     useFileHash = true,
     betaEnv = 'ci',
-    sassPrefix
+    sassPrefix,
+    deployment
 } = {}) => {
     const filenameMask = `js/[name]${useFileHash ? '.[chunkhash]' : ''}.js`;
     return {
@@ -146,14 +147,14 @@ module.exports = ({
             disableHostCheck: true,
             historyApiFallback: true,
             writeToDisk: true,
-            proxy: betaEnv ? {
+            proxy: !deployment && betaEnv ? {
                 [`https://${betaEnv}.foo.redhat.com:1337/beta`]: {
                     target: `http${https ? 's' : ''}://localhost:${port || 8002}`,
                     pathRewrite: function(path) {
                         const pathRewrite = path.replace(/^\/beta\//, '/');
                         if (rewriteLineCounter === 0) {
                             // eslint-disable-next-line max-len
-                            console.warn('\x1b[33m%s\x1b[0m', `[${rewriteLineCounter}]Warning, automatic beta rewrites are deprecated.`, 'Please use deployment configuration to use beta env: https://github.com/RedHatInsights/frontend-starter-app/pull/411');
+                            console.warn('\x1b[33m%s\x1b[0m', `[${rewriteLineCounter}]Warning, automatic beta rewrites are deprecated.`, 'Please use deployment configuration to use beta env: https://github.com/RedHatInsights/frontend-starter-app/pull/421/files');
                             rewriteLineCounter += 1;
                         }
 
