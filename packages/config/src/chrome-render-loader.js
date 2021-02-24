5df16eaee24f670931e786df29eeaf77a342eb55
Karel Hala <khala@redhat.com>
2021-02-24 12:12:09 +0100

    Add option to skip chrome 2 wrapper in order to render non module apps

--- packages/config/src/chrome-render-loader.js
+++ packages/config/src/chrome-render-loader.js
@@ -2,7 +2,7 @@
 function chromeRenderLoader(source) {
     const loaderUtils = require('loader-utils');
     const options = loaderUtils.getOptions(this);
-    return `document.getElementById('root').classList.add('${options.appName}');var isChrome2 = window.insights && window.insights.chrome && window.insights.chrome.isChrome2 || false; if(!isChrome2){${source}}`;
+    return `document.getElementById('root').classList.add('${options.appName}');var isChrome2 = (!${options.skipChrome2} && window.insights && window.insights.chrome && window.insights.chrome.isChrome2) || false; if(!isChrome2){${source}}`;
 }
 
 module.exports = chromeRenderLoader;
