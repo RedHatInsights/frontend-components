542628d8b924e5ca6dbd3540d5bd3ad94970d745
Martin Marosi <marvusm.mmi@gmail.com>
2021-03-01 14:57:51 +0100

    Merge babel config files.

--- packages/utils/babel.config.js
+++ packages/utils/babel.config.js
@@ -1,24 +1,3 @@
 module.exports = {
-    extends: '../../babel.config.js',
-    env: {
-        cjs: {
-            presets: [ [ '@babel/preset-env', { modules: 'commonjs' }] ],
-            plugins: [
-                [
-                    '../../plugins/transform-scss-plugin'
-                ]
-            ]
-        },
-        esm: {
-            presets: [ [ '@babel/preset-env', { modules: false }] ],
-            plugins: [
-                [
-                    '../../plugins/transform-scss-plugin',
-                    {
-                        esm: true
-                    }
-                ]
-            ]
-        }
-    }
+    extends: '../../babel.config.js'
 };
