6490061fffe44ec57bf5b624c81e2396a0b3804e
Martin Marosi <marvusm.mmi@gmail.com>
2021-03-01 09:44:30 +0100

    Update FEC dependencies to v3

--- packages/pdf-generator/src/components/PDFDocument.js
+++ packages/pdf-generator/src/components/PDFDocument.js
@@ -7,7 +7,7 @@ import {
     Document
 } from '@react-pdf/renderer';
 import RHLogo from './Logo';
-import { DateFormat } from '@redhat-cloud-services/frontend-components';
+import { DateFormat } from '@redhat-cloud-services/frontend-components/DateFormat';
 import { customTitle } from '../utils/text';
 import styles from '../utils/styles';
 
