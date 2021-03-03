1386f40746f679a1d24d019c69627ec3617017cf
Martin Marosi <marvusm.mmi@gmail.com>
2021-03-03 15:59:51 +0100

    fix(docs): remove single intem nav groups

--- packages/docs/components/navigation/index.js
+++ packages/docs/components/navigation/index.js
@@ -11,8 +11,73 @@ const useStyles = createUseStyles({
         '& > button': {
             textTransform: 'capitalize'
         }
+    },
+    groupLink: {
+        'font-size': 14
     }
 });
+
+const NavigationGroup = ({ group, items, packageName }) => {
+    const classes = useStyles();
+    const { pathname } = useRouter();
+    if (items.length === 1) {
+        const  item = items[0];
+        const title = typeof item === 'object' ? item.title : item;
+        const name = typeof item === 'object' ? item.name : item;
+        return (
+            <NavItem
+                id={`/${packageName}/${name}`}
+                to={`/${packageName}/${name}`}
+                ouiaId={`/${packageName}/${name}`}
+                key={name}
+                component={({ children, ...props }) => (
+                    <Link {...props}>
+                        <a className={classnames('pf-c-nav__link', {
+                            'pf-m-current': props.href === pathname
+                        })}>
+                            {children}
+                        </a>
+                    </Link>
+                )}
+            >
+                {title}
+            </NavItem>
+        );
+    }
+
+    return (
+        <NavGroup
+            key={group}
+            title={group}
+            className={classes.capitalize}
+        >
+            {items.map(item => {
+                const title = typeof item === 'object' ? item.title : item;
+                const name = typeof item === 'object' ? item.name : item;
+                return (
+                    <NavItem
+                        id={`/${packageName}/${name}`}
+                        to={`/${packageName}/${name}`}
+                        ouiaId={`/${packageName}/${name}`}
+                        key={name}
+                        component={({ children, ...props }) => (
+                            <Link {...props}>
+                                <a className={classnames('pf-c-nav__link', classes.groupLink, {
+                                // eslint-disable-next-line react/prop-types
+                                    'pf-m-current': props.href === pathname
+                                })}>
+                                    {children}
+                                </a>
+                            </Link>
+                        )}
+                    >
+                        {title}
+                    </NavItem>
+                );
+            })}
+        </NavGroup>
+    );
+};
 
 const Navigation = () => {
     const { pathname } = useRouter();
@@ -30,35 +95,7 @@ const Navigation = () => {
                         isExpanded={pathname.includes(`/${packageName}`)}
                     >
                         {groups.map(({ group, items }) => (
-                            <NavGroup
-                                key={group}
-                                title={group}
-                                className={classes.capitalize}
-                            >
-                                {items.map(item => {
-                                    const title = typeof item === 'object' ? item.title : item;
-                                    const name = typeof item === 'object' ? item.name : item;
-                                    return (
-                                        <NavItem
-                                            id={`/${packageName}/${name}`}
-                                            to={`/${packageName}/${name}`}
-                                            ouiaId={`/${packageName}/${name}`}
-                                            key={name}
-                                            component={({ children, ...props }) => (
-                                                <Link {...props}>
-                                                    <a className={classnames('pf-c-nav__link', {
-                                                    // eslint-disable-next-line react/prop-types
-                                                        'pf-m-current': props.href === pathname
-                                                    })}>
-                                                        {children}
-                                                    </a>
-                                                </Link>
-                                            )}
-                                        >
-                                            {title}
-                                        </NavItem>
-                                    );})}
-                            </NavGroup>
+                            <NavigationGroup key={group} group={group} items={items} packageName={packageName} />
                         ))}
                     </NavExpandable>
                 ))}
