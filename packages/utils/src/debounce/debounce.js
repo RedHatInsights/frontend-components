15c8debcfdc0e6aa551236d6beeb8746e82d75cf
Martin Marosi <marvusm.mmi@gmail.com>
2021-03-04 15:14:04 +0100

    chore(docs): update debounce function arguments docs

--- packages/utils/src/debounce/debounce.js
+++ packages/utils/src/debounce/debounce.js
@@ -3,8 +3,8 @@ import awesomeDebouncePromise from 'awesome-debounce-promise';
 /**
  * Async debounce factory functions.
  * @param {Function} asyncFunction async function to be debounced
- * @param {number} time delay in ms
- * @param {Object} config additional config. See <a target="_blank" href="https://github.com/slorber/awesome-debounce-promise#options">for more details</a>.
+ * @param {number} [time = 800] delay in ms
+ * @param {Object} [config = {onlyResolvesLast: true}] <a target="_blank" href="https://github.com/slorber/awesome-debounce-promise#options">more details</a>
  */
 export const debounceFunction = (asyncFunction, time = 800, config = { onlyResolvesLast: true }) => awesomeDebouncePromise(
     asyncFunction,
