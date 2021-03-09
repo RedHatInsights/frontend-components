ada5721055f9aeb6bf1a217ff7574092554ed973
Martin Marosi <marvusm.mmi@gmail.com>
2021-03-09 11:31:36 +0100

    fix(components): add missing ts module exports

--- packages/components/src/Dark/index.d.ts
+++ packages/components/src/Dark/index.d.ts
@@ -1,2 +1,3 @@
-export { default } from './Dark';
-export * from './Dark';
+export { default } from './DarkContext';
+export { default as Dark } from './DarkContext';
+export { default as DarkContext } from './configContext';
