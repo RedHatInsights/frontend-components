b923420205cfc9eeb675eeda7c5d8779c46416bc
Allen Wight <awight@redhat.com>
2021-03-29 09:59:34 -0400

    Swap order of kebab n remediations buttons (#1047)
    fixes https://issues.redhat.com/browse/ADVISOR-1

--- packages/inventory-insights/src/Constants/index.js
+++ packages/inventory-insights/src/Constants/index.js
@@ -1,2 +1,2 @@
-export { default } from './Constants';
+export { BASE_FETCH_URL, FILTER_CATEGORIES, RISK_TO_STRING, IMPACT_LABEL, LIKELIHOOD_LABEL } from './Constants';
 export * from './Constants';
