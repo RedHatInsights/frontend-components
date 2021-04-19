bb44b66a5803361346bf9fbb73a73bf231aea94a
Filip Hlavac <filip.hlavac01@gmail.com>
2021-04-19 15:27:08 +0200

    Add resolutions to Redux

---
+++ packages/remediations/src/redux/actions/resolution-actions.js
@@ -0,0 +1,26 @@
+import * as api from '../../api';
+import { FETCH_RESOLUTIONS } from '../action-types';
+import transform from 'lodash/transform';
+
+export const fetchResolutions = (issues = []) => ({
+    type: FETCH_RESOLUTIONS,
+    payload: async () => {
+        try {
+            const result = await api.getResolutionsBatch(issues.map(i => i.id));
+
+            const [ resolutions, warnings ] = transform(result, ([ resolutions, errors ], value, key) => {
+                if (!value) {
+                    errors.push(`Issue ${key} does not have Ansible support`);
+                } else {
+                    resolutions.push(value);
+                }
+
+                return [ resolutions, errors ];
+            }, [ [], [] ]);
+
+            return { resolutions, warnings };
+        } catch (e) {
+            return { errors: [ 'Error obtaining resolution information. Please try again later.' ] };
+        }
+    }
+});
