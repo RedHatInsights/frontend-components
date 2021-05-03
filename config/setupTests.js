1bcee2a45804790a6ad6e4680455ef2c723755b4
Martin Marosi <marvusm.mmi@gmail.com>
2021-05-03 12:19:01 +0200

    Use npm workspaces to hoist dependencies.

--- config/setupTests.js
+++ config/setupTests.js
@@ -2,7 +2,6 @@
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import 'babel-polyfill';
-import 'whatwg-fetch';
 
 configure({ adapter: new Adapter() });
 global.SVGPathElement = function () {};
