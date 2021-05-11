46c5351326c3b1e7f2d1b008c743511585e3d7a1
rvsia <r.vsia@seznam.cz>
2021-05-11 11:45:53 +0200

    Remove label from OS column

--- packages/inventory/src/shared/OperatingSystemFormatter.js
+++ packages/inventory/src/shared/OperatingSystemFormatter.js
@@ -1,24 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Label } from '@patternfly/react-core';
 
 const OperatingSystemFormatter = ({ systemProfile }) => {
-    if (!systemProfile?.operating_system || !systemProfile?.operating_system?.name) {
-        return <span>Not available</span>;
-    }
-
-    if (systemProfile.operating_system.name === 'RHEL') {
+    if (systemProfile?.operating_system?.name === 'RHEL') {
         const version = (systemProfile.operating_system.major && systemProfile.operating_system.minor)
         && `${systemProfile.operating_system.major}.${systemProfile.operating_system?.minor}` || null;
 
-        return <Label color="red">
+        return <span>
             RHEL {version}
-        </Label>;
+        </span>;
     }
 
-    return <Label>
-        {systemProfile.operating_system.name}
-    </Label>;
+    return <span>
+        {systemProfile?.operating_system?.name || 'Not available'}
+    </span>;
 };
 
 OperatingSystemFormatter.propTypes = {
