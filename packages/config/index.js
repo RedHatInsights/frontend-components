41c0e4727ed6fb1e4a10fe2a55bd4ce433b7af65
Martin Marosi <marvusm.mmi@gmail.com>
2021-05-26 10:33:41 +0200

    fix(config): add missing isProd variable

--- packages/config/index.js
+++ packages/config/index.js
@@ -30,6 +30,7 @@ const getAppEntry = (rootFolder, isProd) => {
 
 module.exports = (configurations) => {
     configurations.isProd = configurations.isProd || process.env.NODE_ENV === 'production';
+    const isProd = configurations.isProd;
     const { insights } = require(`${configurations.rootFolder}/package.json`);
     const gitBranch = process.env.TRAVIS_BRANCH || process.env.BRANCH || gitRevisionPlugin.branch();
     const appDeployment = configurations.deployment || ((isProd && betaBranches.includes(gitBranch)) ?
