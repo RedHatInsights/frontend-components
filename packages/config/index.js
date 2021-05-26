818cd997f4b9685c408182bd3aed51725898fb79
Martin Marosi <marvusm.mmi@gmail.com>
2021-05-26 10:35:56 +0200

    fix config linter errors

--- packages/config/index.js
+++ packages/config/index.js
@@ -2,22 +2,23 @@ const config = require('./src/config');
 const plugins = require('./src/plugins');
 const { sync } = require('glob');
 
-const gitRevisionPlugin = new (require('git-revision-webpack-plugin'))({
+const gitRevisionPlugin = new(require('git-revision-webpack-plugin'))({
     branch: true
 });
-const betaBranches = ['master', 'qa-beta', 'ci-beta', 'prod-beta', 'main', 'devel'];
-const akamaiBranches = ['prod-beta', 'prod-stable'];
+const betaBranches = [ 'master', 'qa-beta', 'ci-beta', 'prod-beta', 'main', 'devel' ];
+const akamaiBranches = [ 'prod-beta', 'prod-stable' ];
 
 const getAppEntry = (rootFolder, isProd) => {
     // Use entry-dev if it exists
     if (!isProd) {
-      const entries = sync('src/entry-dev.{js,jsx,ts,tsx}', { cwd: rootFolder });
-      if (entries.length > 1) {
-          console.warn('Found multiple entry-dev files. Using', entries[0]);
-      }
-      if (entries.length > 0) {
-          return `rootFolder/${entries[0]}`;
-      }
+        const entries = sync('src/entry-dev.{js,jsx,ts,tsx}', { cwd: rootFolder });
+        if (entries.length > 1) {
+            console.warn('Found multiple entry-dev files. Using', entries[0]);
+        }
+
+        if (entries.length > 0) {
+            return `rootFolder/${entries[0]}`;
+        }
     }
 
     const entries = sync('src/entry.{js,jsx,ts,tsx}', { cwd: rootFolder });
@@ -46,12 +47,13 @@ module.exports = (configurations) => {
         console.log('~~~Using variables~~~');
         console.log(`Root folder: ${configurations.rootFolder}`);
         console.log(`Current branch: ${gitBranch}`);
-        !generateSourceMaps && console.log(`Source map generation for "${gitBranch}" deployment has been disabled.`)
+        !generateSourceMaps && console.log(`Source map generation for "${gitBranch}" deployment has been disabled.`);
         console.log(`Beta branches: ${betaBranches}`);
         console.log(`Using deployments: ${appDeployment}`);
         console.log(`Public path: ${publicPath}`);
         console.log(`App entry: ${appEntry}`);
         console.log(`Use proxy: ${configurations.useProxy ? 'true' : 'false'}`);
+        // eslint-disable-next-line max-len, no-irregular-whitespace
         !configurations.useProxy && console.log('You can use webpack proxy (instead of using insights-proxy) by setting "useProxy". Check config documentation to see more details.');
         console.log('~~~~~~~~~~~~~~~~~~~~~');
     }
