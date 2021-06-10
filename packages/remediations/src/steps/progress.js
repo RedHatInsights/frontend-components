bf696511eaea7004e51fd02ec4b41adc2b2a6d95
Filip Hlavac <filip.hlavac01@gmail.com>
2021-06-10 13:55:22 +0200

    Create progressStep variants

--- packages/remediations/src/steps/progress.js
+++ packages/remediations/src/steps/progress.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState }/*, { useContext }*/ from 'react';
+import React from 'react';
 import {
     Title,
     Button,
@@ -6,82 +6,151 @@ import {
     EmptyStateVariant,
     EmptyStateIcon,
     EmptyStateBody,
-    EmptyStateSecondaryActions,
-    ListItem,
-    List
+    EmptyStateSecondaryActions
 } from '@patternfly/react-core';
 import ProgressBar from '../common/ProgressBar';
-//import { AddRoleWizardContext } from './add-role-wizard';
 import PropTypes from 'prop-types';
-import { InProgressIcon } from '@patternfly/react-icons';
+import { CheckCircleIcon, ExclamationCircleIcon, ExternalLinkSquareAltIcon, InProgressIcon } from '@patternfly/react-icons';
+import { dedupeArray, getEnvUrl, pluralize, remediationUrl, SELECT_PLAYBOOK, SYSTEMS } from '../utils';
 import './progress.scss';
 
-const EmtpyStateWithErrors = ({ errors }) => (
-    (errors && Array.isArray(errors) && errors.length > 0) ? (
-        <EmptyStateBody className='wizard-failed-errors'>
-            <List>
-                {
-                    errors.map((error) => (
-                        <ListItem key={ error }>{ error }</ListItem>
-                    ))
-                }
-            </List>
-        </EmptyStateBody>
-    ) : null
-);
-
-EmtpyStateWithErrors.propTypes = {
-    errors: PropTypes.array
-};
-
-const Progress = ({ onClose, title, percent, failed }) => {
-    const [ message, setMessage ] = useState('This may take tens of seconds.');
-    const [ errors, setErrors ] = useState(null);
-
-    //const { setHideForm } = useContext(AddRoleWizardContext);
+const Progress = ({
+    onClose,
+    setOpen,
+    submitRemediation,
+    setState,
+    state
+}) => {
+    const { percent, failed, systems, issues, playbook } = {
+        percent: state.percent,
+        failed: state.failed,
+        systems: dedupeArray(Object.values(state.formValues[SYSTEMS]).flat()),
+        issues: Object.keys(state.formValues[SYSTEMS]),
+        playbook: {
+            name: state.formValues[SELECT_PLAYBOOK],
+            id: state.id
+        }
+    };
 
     return (
-
         <EmptyState variant={EmptyStateVariant.large}>
-            <EmptyStateIcon className="pf-u-mb-lg" icon={InProgressIcon} />
+            <EmptyStateIcon
+                className="pf-u-mb-lg pf-u-mt-sm"
+                color={failed ? 'var(--pf-global--danger-color--100)' :
+                    (percent === 100 ? 'var(--pf-global--success-color--100)' : undefined)}
+                icon={failed ?
+                    ExclamationCircleIcon :
+                    (percent === 100 ? CheckCircleIcon : InProgressIcon)} />
             <Title headingLevel="h1" size='lg'>
-                {title}
+                {failed ? 'Error: Unable to add items to playbook' :
+                    (percent === 100 ? 'Items added to playbook' : 'Adding items to the playbook')}
             </Title>
-            <EmptyStateBody>
+            <EmptyStateBody className="pf-c-progress-bar pf-u-mt-md">
                 <ProgressBar
                     percent={percent}
                     failed={failed}
                 />
             </EmptyStateBody>
-            <EmptyStateBody className={failed && 'wizard-failed-message'}>
-                { message }
+            {(failed || percent === 100) && <EmptyStateBody className="pf-c-progress-message">
+                { failed ?
+                    'Please try again.' :
+                    <div>{`${issues.length} ${pluralize(issues.length, 'action')} affecting 
+                        ${systems.length} ${pluralize(systems.length, 'system')} 
+                        ${issues.length > 1 ? 'were' : 'was'} added to the playbook `}
+                    <a href={remediationUrl(playbook.id)}>{playbook.name}</a>.
+                    </div>
+                }
             </EmptyStateBody>
-            <EmtpyStateWithErrors error={ errors } />
+            }
+            {percent === 100 && <EmptyStateBody className="pf-c-progress-message-description pf-u-mb-md">
+            You can remediate these systems directly from Insights by enabling
+                <Button
+                    variant="link"
+                    className="pf-u-py-0 pf-u-px-xs"
+                    icon={<ExternalLinkSquareAltIcon />}
+                    iconPosition="right"
+                    isSmall
+                    onClick={
+                        () => window.location.href = `${window.location.origin}/${getEnvUrl()}settings/connector`
+                    }
+                >
+                Cloud Connector
+                </Button>
+            on eligible systems.
+            </EmptyStateBody>
+            }
+            {
+                failed &&
+                    <Button
+                        className="pf-u-mt-md pf-u-mb-sm"
+                        variant="primary"
+                        ouiaId="TryAgainButton"
+                        onClick={() => {
+                            setState({ failed: false, percent: 0 });
+                            submitRemediation(false);
+                        }}>
+                        Try again
+                    </Button>
+            }
+            {
+                percent === 100 &&
+                    <Button
+                        className="pf-u-mt-md pf-u-mb-sm"
+                        variant="primary"
+                        ouiaId="ReturnToAppButton"
+                        onClick={() => {
+                            onClose();
+                            setOpen(false);
+                        }}>
+                        Return to application
+                    </Button>
+            }
             <EmptyStateSecondaryActions>
                 {
-                    (percent === 100 || failed) &&
-                            <Button
-                                variant={'primary'}
-                                ouiaId="ReturnToAppButton"
-                                onClick={() => null}>
-                                { failed ? 'Back' : 'Return to application' }
-                            </Button>
+                    failed &&
+                        <Button
+                            variant="secondary"
+                            ouiaId="BackToWizardButton"
+                            onClick={() => {
+                                onClose();
+                            }}>
+                            Go back to the wizard
+                        </Button>
+                }
+                {
+                    percent === 100 &&
+                        <Button
+                            variant="link"
+                            ouiaId="OpenPlaybookButton"
+                            onClick={() => {
+                                onClose();
+                                setOpen(false);
+                                window.location.href = remediationUrl(playbook.id);
+                            }}>
+                            Open Playbook {playbook.name}
+                        </Button>
                 }
             </EmptyStateSecondaryActions>
         </EmptyState>
-
     );
 };
 
-Progress.defaultProps = {
-    failed: false
-};
-
 Progress.propTypes = {
-    onClose: PropTypes.func.isRequired,
-    title: PropTypes.string.isRequired,
-    percent: PropTypes.number.isRequired,
-    failed: PropTypes.bool
+    onClose: PropTypes.func,
+    setOpen: PropTypes.func,
+    submitRemediation: PropTypes.func,
+    setState: PropTypes.func,
+    state: PropTypes.shape({
+        id: PropTypes.number,
+        percent: PropTypes.number.isRequired,
+        failed: PropTypes.bool.isRequired,
+        formValues: PropTypes.shape({
+            [SELECT_PLAYBOOK]: PropTypes.string,
+            [SYSTEMS]: PropTypes.objectOf(
+                PropTypes.arrayOf(PropTypes.string)
+            )
+        })
+    })
 };
 
 export default Progress;
