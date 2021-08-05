68f5ab62b72d1eb326fac0077121efc437403f97
rvsia <r.vsia@seznam.cz>
2021-08-05 11:17:34 +0200

    Change path in esm scss exports

--- plugins/transform-scss-plugin.js
+++ plugins/transform-scss-plugin.js
@@ -34,6 +34,50 @@ module.exports = function (babel) {
                 }
 
                 path.replaceWith(t.importDeclaration(path.node.specifiers, t.stringLiteral(cssSourcePath)));
+            },
+            /**
+                 * In RuleTable component there is the style file reexported
+                 * for esm, we need to change the source path
+                 */
+            ExportDeclaration: function(path, state) {
+                /**
+                 * skip default export
+                 */
+                if (!path.node.source) {
+                    return;
+                }
+
+                /**
+                 * skip files with no sass exports
+                 */
+                if (!sassExt.test(path.node.source.value)) {
+                    return;
+                }
+
+                /**
+                 * replace sass extenstion with css extension
+                 */
+                const sassFileName = path.node.source.value.replace(sassExt, '.css');
+                /**
+                 * replace sass export with css import
+                 */
+                let cssSourcePath;
+                if (state.opts.esm === true) {
+                    /**
+                     * Get root folder for ESM reference
+                     */
+                    let relative = nodePath.relative(this.filename.replace('/src/', '/esm/'), this.filename.replace('/src', '')).replace(/^..\//, '').split('/');
+                    relative.pop();
+                    relative = relative.join('/');
+                    cssSourcePath =  `${relative}/${sassFileName}`;
+                } else {
+                    cssSourcePath = `${sassFileName}`;
+                }
+
+                /**
+                     * Change the export to new path
+                     */
+                path.replaceWith(t.exportNamedDeclaration(path.node.declarations, path.node.specifiers, t.stringLiteral(cssSourcePath)));
             }
         }
     };
