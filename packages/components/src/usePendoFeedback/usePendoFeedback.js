9caa4171d869d30e1c64046e7ea98f2070aab893
Martin Maroši <marvusm.mmi@gmail.com>
2021-08-06 14:21:44 +0200

    Update packages/components/src/usePendoFeedback/usePendoFeedback.js
    Co-authored-by: Karel Hala <ch4rlien@gmail.com>

--- packages/components/src/usePendoFeedback/usePendoFeedback.js
+++ packages/components/src/usePendoFeedback/usePendoFeedback.js
@@ -6,7 +6,7 @@ const usePendoFeedback = (...args) => {
      * Return no-op in case the chrome changes are not avaiable in current env.
      */
     if (typeof usePendoFeedbackInternal !== 'function') {
-        console.warn('The "usePendoFeedback" hook is not available in this enviroment. Default feedback form will be used. Wait from chrome updates.');
+        console.warn('The "usePendoFeedback" hook is not available in this enviroment. Default feedback form will be used. Wait for chrome updates.');
         return;
     }
 
