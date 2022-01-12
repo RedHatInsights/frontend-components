d89c94f90a72bdc829bacc8258d258ade33d04f4
Martin Maroši <marvusm.mmi@gmail.com>
2022-01-12 12:20:05 +0100

    Convert insights label to TS.

--- packages/components/src/InsightsLabel/CriticalIcon.tsx
+++ packages/components/src/InsightsLabel/CriticalIcon.tsx
@@ -2,7 +2,7 @@ import './critical-icon.scss';
 
 import React from 'react';
 
-const CriticalIcon = () => <svg className='ins-c-critical-icon' viewbox='0 0 10 10'>
+const CriticalIcon = () => <svg className='ins-c-critical-icon' viewBox='0 0 10 10'>
     <polygon points='10 10, 10 3, 5 0, 0 3, 0 10, 5 8'/>
 </svg>;
 
--- packages/components/src/InsightsLabel/InsightsLabel.tsx
+++ packages/components/src/InsightsLabel/InsightsLabel.tsx
@@ -4,38 +4,43 @@ import { AngleDoubleDownIcon } from '@patternfly/react-icons';
 import { AngleDoubleUpIcon } from '@patternfly/react-icons';
 import CriticalIcon from './CriticalIcon';
 import { EqualsIcon } from '@patternfly/react-icons';
-import { Label } from '@patternfly/react-core';
-import PropTypes from 'prop-types';
+import { Label, LabelProps } from '@patternfly/react-core';
 import React  from 'react';
 import classNames from 'classnames';
 
-const VALUE_TO_STATE = {
+type InsightsLabelValueMapping ={
+    [key: number]: {
+        icon: React.ReactNode,
+        text: string
+    }
+}
+const VALUE_TO_STATE: InsightsLabelValueMapping = {
     1: { icon: <AngleDoubleDownIcon/>, text: 'Low' },
     2: { icon: <EqualsIcon/>, text: 'Moderate' },
     3: { icon: <AngleDoubleUpIcon/>, text: 'Important' },
     4: { icon: <CriticalIcon/>, text: 'Critical' }
 };
 
-const InsightsLabel = ({ value, text, hideIcon, className, rest }) => {
+export interface InsightsLabelProps extends LabelProps {
+    value?: number,
+    text?: React.ReactNode,
+    hideIcon?: boolean,
+    className?: string,
+    /**
+     * @deprecated Please do not use the rest object to pass default PF label props. Assing props directly to insights label element
+     */
+    rest: {[key: string]: any}
+}
+
+const InsightsLabel: React.FunctionComponent<InsightsLabelProps> = ({ value = 1, text, hideIcon, className, rest, ...props }) => {
     return <Label
         { ...rest }
+        {...props}
         className={ classNames(className, `ins-c-label-${value}`) }
         icon={ !hideIcon && VALUE_TO_STATE[value].icon }
     >
         { text || VALUE_TO_STATE[value].text }
     </Label>;
-};
-
-InsightsLabel.propTypes = {
-    value: PropTypes.number,
-    text: PropTypes.node,
-    hideIcon: PropTypes.bool,
-    className: PropTypes.string,
-    rest: PropTypes.object
-};
-
-InsightsLabel.defaultProps = {
-    value: 1
 };
 
 export default InsightsLabel;
