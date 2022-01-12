906b21f1fbb6d3d73fc4851f8508eedc3cba94fb
Martin Maroši <marvusm.mmi@gmail.com>
2022-01-12 13:15:09 +0100

    Convert Main component to TS.

--- packages/components/src/Main/Main.ts
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-
-export type MainProps = Omit<React.HTMLAttributes<HTMLElement>, 'className'> & {
-    className?: string;
-    children: React.ReactNode;
-};
-
-export declare const Main: React.FunctionComponent<MainProps>;
--- packages/components/src/Main/index.js
+++
@@ -1,2 +0,0 @@
-export { default } from './Main';
-export { default as Main } from './Main';
