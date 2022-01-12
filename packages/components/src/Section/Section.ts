d51120683e939d263cfc752983ad3357e0e60042
Martin Maroši <marvusm.mmi@gmail.com>
2022-01-12 13:21:25 +0100

    Convert Section component to TS.

--- packages/components/src/Section/Section.ts
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-
-export type SectionProps = Omit<React.HTMLAttributes<HTMLElement>, 'className' | 'type'> & {
-    className?: string;
-    children: React.ReactNode;
-    type?: string;
-};
-
-export declare const Section: React.FunctionComponent<SectionProps>;
--- packages/components/src/Section/index.js
+++
@@ -1,2 +0,0 @@
-export { default } from './Section';
-export { default as Section } from './Section';
