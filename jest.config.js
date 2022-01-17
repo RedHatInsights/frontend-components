2849414530ceecfc651fde56a0245cd52c217172
Adithya Krishna <adikrish@redhat.com>
2022-01-18 02:46:02 +0530

    Updates to Seperate Jest Config File - Related to https://github.com/RedHatInsights/frontend-components/issues/1323
    Signed-off-by: Adithya Krishna <adikrish@redhat.com>

---
+++ jest.config.js
@@ -0,0 +1,47 @@
+// Sync object
+/** @type {import('@jest/types').Config.InitialOptions} */
+
+const config = {
+    verbose: true,
+    coverageDirectory: "./coverage/",
+    collectCoverage: true,
+    transformIgnorePatterns: [
+        "node_modules/(?!@patternfly|@data-driven-forms)",
+    ],
+    collectCoverageFrom: [
+        "<rootDir>/packages/**/src/**/*.js",
+        "!<rootDir>/packages/**/stories/*",
+        "!<rootDir>/packages/**/index.js",
+        "!<rootDir>/packages/**/*{c|C}ontext*.js",
+        "!<rootDir>/packages/components/src/Components/Table/*"
+    ],
+    setupFiles: [
+        "<rootDir>/config/setupTests.js",
+        "jest-canvas-mock"
+    ],
+    testEnvironment: "jsdom",
+    roots: [
+        "<rootDir>/packages/"
+    ],
+    moduleNameMapper: {
+        "\\.(css|scss)$": "identity-obj-proxy",
+        "^lodash-es$": "lodash",
+        "customReact": "react",
+        "reactRedux": "react-redux",
+        "PFReactCore": "@patternfly/react-core",
+        "PFReactTable": "@patternfly/react-table"
+    },
+    testURL: "http://localhost:5000/",
+    globalSetup: "<rootDir>/config/globalSetup.js",
+    transform: {
+        "^.+\\.jsx?$": "babel-jest",
+        "^.+\\.tsx?$": "ts-jest"
+    },
+    globals: {
+        "ts-jest": {
+            "tsconfig": "./tsconfig.json"
+        },
+    }
+};
+
+module.exports = config;
