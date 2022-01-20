8fe93aac903feacb5c6c3fb4abaede626f8e39c2
Adithya Krishna <adikrish@redhat.com>
2022-01-20 19:17:50 +0530

    Lint Fix  on Jest Config File
    Signed-off-by: Adithya Krishna <adikrish@redhat.com>

--- jest.config.js
+++ jest.config.js
@@ -2,49 +2,44 @@
 /** @type {import('@jest/types').Config.InitialOptions} */
 
 const config = {
-    verbose: true,
-    coverageDirectory: "./coverage/",
-    collectCoverage: true,
-    transformIgnorePatterns: [
-        "node_modules/(?!@patternfly|@data-driven-forms)",
+  verbose: true,
+  coverageDirectory: './coverage/',
+  collectCoverage: true,
+  transformIgnorePatterns: [
+    'node_modules/(?!@patternfly|@data-driven-forms)',
 
-        // Uncomment the below line if you face any errors with jest
-        // "/node_modules/(?!@redhat-cloud-services)"
-    ],
-    collectCoverageFrom: [
-        "<rootDir>/packages/**/src/**/*.js",
-        "!<rootDir>/packages/**/stories/*",
-        "!<rootDir>/packages/**/index.js",
-        "!<rootDir>/packages/**/*{c|C}ontext*.js",
-        "!<rootDir>/packages/components/src/Components/Table/*"
-    ],
-    setupFiles: [
-        "<rootDir>/config/setupTests.js",
-        "jest-canvas-mock"
-    ],
-    testEnvironment: "jsdom",
-    roots: [
-        "<rootDir>/packages/"
-    ],
-    moduleNameMapper: {
-        "\\.(css|scss)$": "identity-obj-proxy",
-        "^lodash-es$": "lodash",
-        "customReact": "react",
-        "reactRedux": "react-redux",
-        "PFReactCore": "@patternfly/react-core",
-        "PFReactTable": "@patternfly/react-table"
+    // Uncomment the below line if you face any errors with jest
+    // "/node_modules/(?!@redhat-cloud-services)"
+  ],
+  collectCoverageFrom: [
+    '<rootDir>/packages/**/src/**/*.js',
+    '!<rootDir>/packages/**/stories/*',
+    '!<rootDir>/packages/**/index.js',
+    '!<rootDir>/packages/**/*{c|C}ontext*.js',
+    '!<rootDir>/packages/components/src/Components/Table/*',
+  ],
+  setupFiles: ['<rootDir>/config/setupTests.js', 'jest-canvas-mock'],
+  testEnvironment: 'jsdom',
+  roots: ['<rootDir>/packages/'],
+  moduleNameMapper: {
+    '\\.(css|scss)$': 'identity-obj-proxy',
+    '^lodash-es$': 'lodash',
+    customReact: 'react',
+    reactRedux: 'react-redux',
+    PFReactCore: '@patternfly/react-core',
+    PFReactTable: '@patternfly/react-table',
+  },
+  testURL: 'http://localhost:5000/',
+  globalSetup: '<rootDir>/config/globalSetup.js',
+  transform: {
+    '^.+\\.jsx?$': 'babel-jest',
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  globals: {
+    'ts-jest': {
+      tsconfig: './tsconfig.json',
     },
-    testURL: "http://localhost:5000/",
-    globalSetup: "<rootDir>/config/globalSetup.js",
-    transform: {
-        "^.+\\.jsx?$": "babel-jest",
-        "^.+\\.tsx?$": "ts-jest"
-    },
-    globals: {
-        "ts-jest": {
-            "tsconfig": "./tsconfig.json"
-        },
-    }
+  },
 };
 
 module.exports = config;
