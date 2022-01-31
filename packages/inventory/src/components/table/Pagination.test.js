2cd962dc3c9a9f68e001087774d04d6f1d39d362
Martin Maroši <marvusm.mmi@gmail.com>
2022-01-31 18:14:29 +0100

    Fix tests after PF update.

--- packages/inventory/src/components/table/Pagination.test.js
+++ packages/inventory/src/components/table/Pagination.test.js
@@ -58,7 +58,7 @@ describe('Pagination', () => {
       const store = mockStore(initialState);
       const wrapper = render(<WrappedPagination store={store} />);
       expect(toJson(wrapper)).toMatchSnapshot();
-      expect(wrapper.find('button[disabled=""]').length).toBe(5);
+      expect(wrapper.find('button[disabled=""]').length).toBe(4);
     });
 
     it('should render correctly - with no access', () => {
