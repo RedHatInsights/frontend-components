eecc64570bee0f3963c254c45486498231e1739b
Karel Hala <khala@redhat.com>
2022-02-01 15:11:49 +0100

    Wrap inventory components with inventory clasName

--- packages/components/src/Inventory/TagWithDialog.js
+++ packages/components/src/Inventory/TagWithDialog.js
@@ -5,6 +5,7 @@ import { useHistory } from 'react-router-dom';
 import { useStore } from 'react-redux';
 import { Bullseye, Spinner } from '@patternfly/react-core';
 import InventoryLoadError from './InventoryLoadError';
+import classNames from 'classnames';
 
 /**
  * Inventory sub component.
@@ -14,25 +15,30 @@ import InventoryLoadError from './InventoryLoadError';
 const BaseTagWithDialog = (props) => {
   const history = useHistory();
   const store = useStore();
+  const Cmp = props.component;
   return (
-    <Suspense fallback={props.fallback}>
-      <ScalprumComponent
-        history={history}
-        store={store}
-        appName="inventory"
-        module="./TagWithDialog"
-        scope="inventory"
-        ErrorComponent={<InventoryLoadError component="InventoryDetailHead" history={history} store={store} {...props} />}
-        ref={props.innerRef}
-        {...props}
-      />
-    </Suspense>
+    <Cmp className={classNames(props.className, 'inventory')}>
+      <Suspense fallback={props.fallback}>
+        <ScalprumComponent
+          history={history}
+          store={store}
+          appName="inventory"
+          module="./TagWithDialog"
+          scope="inventory"
+          ErrorComponent={<InventoryLoadError component="InventoryDetailHead" history={history} store={store} {...props} />}
+          ref={props.innerRef}
+          {...props}
+        />
+      </Suspense>
+    </Cmp>
   );
 };
 
 BaseTagWithDialog.propTypes = {
   fallback: PropTypes.node,
   innerRef: PropTypes.object,
+  component: PropTypes.string,
+  className: PropTypes.string,
 };
 
 /**
@@ -45,6 +51,10 @@ const TagWithDialog = React.forwardRef((props, ref) => <BaseTagWithDialog innerR
 TagWithDialog.propTypes = {
   /** React Suspense fallback component. <a href="https://reactjs.org/docs/code-splitting.html#reactlazy" target="_blank">Learn more</a>. */
   fallback: PropTypes.node,
+  /** Optional wrapper component */
+  component: PropTypes.string,
+  /** Optional classname applied to wrapper component */
+  className: PropTypes.string,
 };
 
 TagWithDialog.defaultProps = {
@@ -53,6 +63,7 @@ TagWithDialog.defaultProps = {
       <Spinner size="xl" />
     </Bullseye>
   ),
+  component: 'section',
 };
 
 export default TagWithDialog;
