757c70f321a5e28a29a1f67b30686c55a80d23fe
Karel Hala <khala@redhat.com>
2022-02-02 15:57:18 +0100

    Migrate async component to TS

---
+++ packages/components/src/AsyncComponent/index.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import { ScalprumComponent } from '@scalprum/react-core';
+import { Bullseye, Spinner } from '@patternfly/react-core';
+import classNames from 'classnames';
+
+export interface AsyncComponentProps {
+  /** Name of the app from which module will be loaded. */
+  appName: string;
+  /** Loaded module, it has to start with `./`. */
+  module: string;
+  /** Optional scope, if not passed appName is used. */
+  scope?: string;
+  /** React Suspense fallback component. <a href="https://reactjs.org/docs/code-splitting.html#reactlazy" target="_blank">Learn more</a>. */
+  fallback: React.ReactNode;
+  /** Optional classname applied to wrapper component */
+  className: string;
+  /** Optional wrapper component */
+  component: keyof JSX.IntrinsicElements;
+}
+
+interface BaseAsyncComponentProps extends AsyncComponentProps {
+  innerRef: React.MutableRefObject<HTMLElement | null> | ((instance: HTMLElement | null) => void) | null;
+}
+
+const BaseAsyncComponent: React.FunctionComponent<BaseAsyncComponentProps> = ({
+  appName,
+  scope,
+  module,
+  fallback = (
+    <Bullseye>
+      <Spinner size="xl" />
+    </Bullseye>
+  ),
+  innerRef,
+  className,
+  component: Cmp = 'section',
+  ...props
+}) => {
+  return (
+    <Cmp className={classNames(className, appName)}>
+      <ScalprumComponent
+        className={className}
+        appName={appName}
+        module={module}
+        scope={scope ?? appName}
+        ErrorComponent={fallback}
+        ref={innerRef}
+        fallback={fallback}
+        {...props}
+      />
+    </Cmp>
+  );
+};
+
+/**
+ * Async component that wraps ScalprumComponent for easier manipulation.
+ *
+ * This component uses fallback as ErrorComponent, if you want to show different
+ * component for error pass it as ErrorComponent prop.
+ */
+const AsynComponent = React.forwardRef<HTMLElement, AsyncComponentProps>((props, ref) => <BaseAsyncComponent innerRef={ref} {...props} />);
+
+export default AsynComponent;
