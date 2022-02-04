9d583906f3cb311d08aab7b2d28b1171537cf439
Karel Hala <khala@redhat.com>
2022-02-04 12:12:31 +0100

    Convert Breadcrumbs tests to RTL

--- packages/components/src/Breadcrumbs/Breadcrumbs.test.js
+++ packages/components/src/Breadcrumbs/Breadcrumbs.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { shallow } from 'enzyme';
-import toJson from 'enzyme-to-json';
+import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import Breadcrumbs from './Breadcrumbs';
 
 describe('Breadcrumbs component', () => {
@@ -12,25 +12,25 @@ describe('Breadcrumbs component', () => {
   const current = 'Something';
   describe('should render correctly', () => {
     it('no items', () => {
-      const wrapper = shallow(<Breadcrumbs items={undefined} current={current} onNavigate={onNavigate} />);
-      expect(toJson(wrapper)).toMatchSnapshot();
+      const { container } = render(<Breadcrumbs items={undefined} current={current} onNavigate={onNavigate} />);
+      expect(container).toMatchSnapshot();
     });
 
     it('no current', () => {
-      const wrapper = shallow(<Breadcrumbs items={items} current={undefined} onNavigate={onNavigate} />);
-      expect(toJson(wrapper)).toMatchSnapshot();
+      const { container } = render(<Breadcrumbs items={items} current={undefined} onNavigate={onNavigate} />);
+      expect(container).toMatchSnapshot();
     });
 
     it('with items and current item', () => {
-      const wrapper = shallow(<Breadcrumbs items={items} current={current} onNavigate={onNavigate} />);
-      expect(toJson(wrapper)).toMatchSnapshot();
+      const { container } = render(<Breadcrumbs items={items} current={current} onNavigate={onNavigate} />);
+      expect(container).toMatchSnapshot();
     });
   });
 
   describe('API', () => {
     it('should call correctly', () => {
-      const wrapper = shallow(<Breadcrumbs items={items} current={current} onNavigate={onNavigate} />);
-      wrapper.find('a').first().simulate('click');
+      const { container } = render(<Breadcrumbs items={items} current={current} onNavigate={onNavigate} />);
+      userEvent.click(container.querySelector('a'));
       expect(onNavigate.mock.calls.length).toBe(1);
       expect(onNavigate.mock.calls[0][1]).toBe(items[0].navigate);
       expect(onNavigate.mock.calls[0][2]).toBe(0);
