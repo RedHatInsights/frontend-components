7a25607f8938d724c0ec0b4e9d4dcc724a98873b
Martin Maroši <marvusm.mmi@gmail.com>
2022-02-11 09:48:57 +0100

    Autopick DOCs navigation based on current bucket.

--- packages/docs/components/navigation/chrome-navigation.js
+++
@@ -1,6 +0,0 @@
-import data from './chrome-navigation.json';
-import Navigation from './common-navigation';
-
-const ChromeNavigation = () => <Navigation {...data} />;
-
-export default ChromeNavigation;
