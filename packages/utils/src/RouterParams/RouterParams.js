c79a8becd8964717dfa83e7828364554b77ff4d3
Martin Maroši <marvusm.mmi@gmail.com>
2022-03-03 16:06:23 +0100

    Enable TS build in utils package.

--- packages/utils/src/RouterParams/RouterParams.js
+++ packages/utils/src/RouterParams/RouterParams.js
@@ -4,14 +4,30 @@ import { withRouter, matchPath } from 'react-router-dom';
 import { connect } from 'react-redux';
 import isEqual from 'lodash/isEqual';
 
-export const routerParams = (Component) => {
-  class RouterParams extends React.Component {
-    componentDidMount() {
-      const {
-        match: { params, path, url },
-        onPathChange,
-        location,
-      } = this.props;
+class RouterParams extends React.Component {
+  componentDidMount() {
+    const {
+      match: { params, path, url },
+      onPathChange,
+      location,
+    } = this.props;
+    if (matchPath(location.pathname, { path: url, exact: true })) {
+      onPathChange &&
+        onPathChange({
+          params,
+          path,
+        });
+    }
+  }
+
+  componentDidUpdate() {
+    const {
+      match: { params, path, url },
+      onPathChange,
+      location,
+      routerData,
+    } = this.props;
+    if (routerData && (!isEqual(params, routerData.params) || path !== routerData.path)) {
       if (matchPath(location.pathname, { path: url, exact: true })) {
         onPathChange &&
           onPathChange({
@@ -20,42 +36,28 @@ export const routerParams = (Component) => {
           });
       }
     }
-
-    componentDidUpdate() {
-      const {
-        match: { params, path, url },
-        onPathChange,
-        location,
-        routerData,
-      } = this.props;
-      if (routerData && (!isEqual(params, routerData.params) || path !== routerData.path)) {
-        if (matchPath(location.pathname, { path: url, exact: true })) {
-          onPathChange &&
-            onPathChange({
-              params,
-              path,
-            });
-        }
-      }
-    }
-
-    render() {
-      const { onPathChange, routerData, staticContext, ...props } = this.props;
-      return <Component {...props} />;
-    }
   }
 
-  RouterParams.propTypes = {
-    match: PropTypes.shape({ params: PropTypes.object, path: PropTypes.string, url: PropTypes.string }).isRequired,
-    onPathChange: PropTypes.func,
-    location: PropTypes.shape({ pathname: PropTypes.string.isRequired }).isRequired,
-    routerData: PropTypes.shape({
-      params: PropTypes.object,
-      path: PropTypes.string,
-    }).isRequired,
-    staticContext: PropTypes.any,
-  };
-  return withRouter(
+  render() {
+    const { onPathChange, routerData, staticContext, Component, ...props } = this.props;
+    return <Component {...props} />;
+  }
+}
+
+RouterParams.propTypes = {
+  match: PropTypes.shape({ params: PropTypes.object, path: PropTypes.string, url: PropTypes.string }).isRequired,
+  onPathChange: PropTypes.func,
+  location: PropTypes.shape({ pathname: PropTypes.string.isRequired }).isRequired,
+  routerData: PropTypes.shape({
+    params: PropTypes.object,
+    path: PropTypes.string,
+  }).isRequired,
+  staticContext: PropTypes.any,
+  Component: PropTypes.any,
+};
+
+const routerParams = (Component) =>
+  withRouter(
     connect(
       ({ routerData }) => ({ routerData }),
       (dispatch) => ({
@@ -65,8 +67,7 @@ export const routerParams = (Component) => {
             payload: data,
           }),
       })
-    )(RouterParams)
+    )((props) => <RouterParams {...props} Component={Component} />)
   );
-};
 
 export default routerParams;
