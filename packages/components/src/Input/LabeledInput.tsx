5d686c72ebc4356ebd522144298aafd3756fb15f
Martin Maroši <marvusm.mmi@gmail.com>
2022-03-07 13:38:38 +0100

    Migrate Labeledinput component to TS.

--- packages/components/src/Input/LabeledInput.tsx
+++ packages/components/src/Input/LabeledInput.tsx
@@ -1,9 +1,8 @@
-import PropTypes from 'prop-types';
 import React from 'react';
 import classnames from 'classnames';
-import Input from './Input';
+import Input, { InputProps } from './Input';
 
-const LabeledInput = ({ type = 'text', className, children, name, ...props }) => {
+const LabeledInput: React.FC<InputProps> = ({ type = 'text', className, children, name, ...props }) => {
   // Might generate ID which is not unique enough
   name = name || new Date().getTime() + Math.random().toString(36);
   return (
@@ -11,13 +10,6 @@ const LabeledInput = ({ type = 'text', className, children, name, ...props }) =>
       <Input {...props} type={type} name={name} id={name} /> {children}
     </label>
   );
-};
-
-LabeledInput.propTypes = {
-  type: PropTypes.string,
-  className: PropTypes.string,
-  children: PropTypes.node,
-  name: PropTypes.string,
 };
 
 export default LabeledInput;
