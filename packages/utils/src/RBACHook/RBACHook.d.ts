682b74979f87f23b12c0911b4cd7afed8e828292
Martin Maroši <marvusm.mmi@gmail.com>
2022-03-07 10:02:00 +0100

    Migrate RBACHook to TS

--- packages/utils/src/RBACHook/RBACHook.d.ts
+++
@@ -1,7 +0,0 @@
-import { RBAC } from '../RBAC';
-
-export interface UsePermissionsState extends RBAC {
-  hasAccess: boolean;
-}
-
-export function usePermissions(appName: string, permissionsList: string[], disableCache?: boolean): UsePermissionsState;
--- packages/utils/src/RBACHook/index.js
+++
@@ -1,2 +0,0 @@
-export { default } from './RBACHook';
-export * from './RBACHook';
