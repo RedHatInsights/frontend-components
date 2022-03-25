c2b5aa53fdb17195f6063bb34483b7a703dba35d
Sebastian Gräßl <mail@bastilian.me>
2022-03-25 14:20:35 +0100

    refactor: Add default args for helpers

--- packages/utils/src/useBulkSelect/helpers.js
+++ packages/utils/src/useBulkSelect/helpers.js
@@ -20,9 +20,9 @@ export const checkboxState = (selectedItemsTotal, itemsTotal) => {
 
 export const selectOrUnselect = (selected) => (selected ? 'Unselect' : 'Select');
 
-const allItemsIncluded = (items, selection = []) => items.filter((item) => selection.includes(item)).length === items.length;
+const allItemsIncluded = (items = [], selection = []) => items?.filter((item) => selection.includes(item)).length === items.length;
 
-export const checkCurrentPageSelected = (items, selectedItems) => {
+export const checkCurrentPageSelected = (items = [], selectedItems) => {
   if (selectedItems.length === 0) {
     return false;
   } else {
