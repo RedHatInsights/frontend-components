4e15743d2999029128b88a8fa60b73ce0957cb88
Georgii Karataev <gkaratae@redhat.com>
2022-04-19 12:08:12 +0200

    Rewrite RuleDescription return value

--- packages/advisor-components/src/RuleDescription/RuleDescription.tsx
+++ packages/advisor-components/src/RuleDescription/RuleDescription.tsx
@@ -13,23 +13,16 @@ interface RuleDescriptionProps {
   isDetailsPage: boolean;
 }
 
-const RuleDescription: React.FC<RuleDescriptionProps> = ({ product, rule, isDetailsPage }) => {
-  const ruleDescription = (data: string, isGeneric = false) => (
-    <React.Fragment>
-      {data && (
-        <span className={isGeneric ? 'ins-c-generic__override' : ''}>
-          <Markdown rehypePlugins={[rehypeRaw, rehypeSanitize]}>{data}</Markdown>
-        </span>
-      )}
-    </React.Fragment>
-  );
-
-  if (product === AdvisorProduct.ocp) {
-    return ruleDescription(rule.generic, true);
-  } else {
-    // RHEL rule
-    return isDetailsPage ? ruleDescription(rule.generic, true) : ruleDescription(rule.summary);
-  }
-};
+const RuleDescription: React.FC<RuleDescriptionProps> = ({ product, rule, isDetailsPage }) => (
+  <React.Fragment>
+    {(rule.generic || rule.summary) && (
+      <span className={product === AdvisorProduct.ocp || isDetailsPage ? 'ins-c-generic__override' : ''}>
+        <Markdown rehypePlugins={[rehypeRaw, rehypeSanitize]}>
+          {product === AdvisorProduct.ocp || isDetailsPage ? rule.generic : rule.summary}
+        </Markdown>
+      </span>
+    )}
+  </React.Fragment>
+);
 
 export default RuleDescription;
