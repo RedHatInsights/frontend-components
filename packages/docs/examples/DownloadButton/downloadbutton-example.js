5dbfa32138b04b85a6ef8b87b3e561fd2c760c4d
Zein Sleiman <zsleiman@redhat.com>
2022-05-06 16:10:50 -0500

    fix linting

--- packages/docs/examples/DownloadButton/downloadbutton-example.js
+++ packages/docs/examples/DownloadButton/downloadbutton-example.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { DownloadButton } from '@redhat-cloud-services/frontend-components/DownloadButton';
 import { DropdownItem } from '@patternfly/react-core';
 
-const extraItems = [<DropdownItem key="extra-1" component="button"></DropdownItem>]
+const extraItems = [<DropdownItem key="extra-1" component="button"></DropdownItem>];
 
 const Example = () => {
-    return (
-        <div>
-            <DownloadButton extraItems={extraItems} />
-        </div>
-    );
-}
+  return (
+    <div>
+        <DownloadButton extraItems={extraItems} />
+    </div>
+   );
+};
 
 export default Example;
