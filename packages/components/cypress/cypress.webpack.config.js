77edcbade60b448d4f6e30436458b453d2f29b06
Raoul Snyman <raoul@snyman.info>
2022-06-10 16:59:50 -0700

    Add an initial stab at component testing
    - Create a Cypress configuration for the "components" folder
    - Create two simple tests for the DownloadButton
    - Fix up a component's import that was conflicting with Webpack

---
+++ packages/components/cypress/cypress.webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+
+module.exports = {
+  entry: '../src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.s?[ac]ss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg|woff2?)$/i, 
+        use: 'file-loader',
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  stats: {
+    errorDetails: true
+  }
+};
