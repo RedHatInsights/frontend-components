9fee0a9cbdd15ee5555726306fd41bdc522a7aa3
Aneela Chagarlamudi <achagarl@redhat.com>
2022-07-20 08:57:33 -0400

    Add external plugins to prod config

--- packages/config/src/scripts/prod.webpack.config.js
+++ packages/config/src/scripts/prod.webpack.config.js
@@ -1,12 +1,15 @@
 const config = require('@redhat-cloud-services/frontend-components-config');
 const commonPlugins = require('./webpack.plugins');
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+const fecConfig = require(process.env.FEC_CONFIG_PATH);
 
 const { config: webpackConfig, plugins } = config({
   rootFolder: process.env.FEC_ROOT_DIR || process.cwd(),
   ...(process.env.BETA === 'true' && { deployment: 'beta/apps' }),
 });
-plugins.push(...commonPlugins);
+
+const { plugins: externalPlugins } = fecConfig;
+plugins.push(...commonPlugins, ...externalPlugins);
 
 module.exports = (env) => {
   if (env && env.analyze === 'true') {
