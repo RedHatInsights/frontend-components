d6d69d948ce505b1c1e906182c950136a07d0d90
Sebastian Gräßl <mail@bastilian.me>
2022-07-29 13:07:16 +0200

    fix(RBACProvider): Don't block state update via "loading"

--- packages/components/src/RBACProvider/RBACProvider.tsx
+++ packages/components/src/RBACProvider/RBACProvider.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Access } from '@redhat-cloud-services/rbac-client';
 import { Bullseye, Spinner } from '@patternfly/react-core';
 
@@ -21,11 +21,9 @@ export interface RBACProviderProps {
 }
 
 export const RBACProvider: React.FunctionComponent<RBACProviderProps> = ({ appName, children }) => {
-  const loading = useRef(false);
   const [permissionState, setPermissionState] = useState(initialPermissions);
 
   const fetchPermissions = async () => {
-    loading.current = true;
     const { isOrgAdmin, permissions: userPermissions } = await getRBAC(appName, true);
 
     setPermissionState((currentPerms: any) => ({
@@ -34,11 +32,10 @@ export const RBACProvider: React.FunctionComponent<RBACProviderProps> = ({ appNa
       isOrgAdmin,
       permissions: userPermissions,
     }));
-    loading.current = false;
   };
 
   useEffect(() => {
-    if (!loading.current && permissionState.permissions.length == 0) {
+    if (appName) {
       fetchPermissions();
     }
   }, [appName]);
