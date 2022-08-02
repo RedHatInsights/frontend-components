f169e26f5b38dbc988c4427ae04e63d43ca21992
Sebastian Gräßl <mail@bastilian.me>
2022-08-02 10:59:37 +0200

    fix(NoResultsTable): Fix body passing in

--- packages/components/src/TableToolsTable/NoResultsTable.js
+++ packages/components/src/TableToolsTable/NoResultsTable.js
@@ -3,23 +3,23 @@ import propTypes from 'prop-types';
 import { Bullseye, EmptyState, EmptyStateBody, EmptyStateVariant, Title } from '@patternfly/react-core';
 import EmptyTable from '../EmptyTable';
 
-export const NoResultsTable = ({
-  title = 'No matching results found',
-  body = (
-    <>
-      This filter criteria matches no {kind}.<br />
-      Try changing your filter settings.
-    </>
-  ),
-  kind = 'results',
-}) => (
+export const NoResultsTable = ({ title = 'No matching results found', body, kind = 'results' }) => (
   <EmptyTable>
     <Bullseye>
       <EmptyState variant={EmptyStateVariant.full}>
         <Title headingLevel="h5" size="lg">
           {title}
         </Title>
-        <EmptyStateBody>{body}</EmptyStateBody>
+        <EmptyStateBody>
+          {body ? (
+            body
+          ) : (
+            <>
+              This filter criteria matches no {kind}.<br />
+              Try changing your filter settings.
+            </>
+          )}
+        </EmptyStateBody>
       </EmptyState>
     </Bullseye>
   </EmptyTable>
