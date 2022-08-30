ff4ef98c529656a604e53d3834f2faf5c25f1c92
Petr Bartos <mrpetrbartos@gmail.com>
2022-08-30 11:54:43 +0200

    fix linting issues

--- packages/components/src/Breadcrumbs/Breadcrumbs.spec.ct.js
+++ packages/components/src/Breadcrumbs/Breadcrumbs.spec.ct.js
@@ -3,26 +3,26 @@ import { mount } from '@cypress/react';
 
 import { Breadcrumbs } from '..';
 
-describe("Breadcrumbs component", () => {
-    const items = [
-        { title: 'first', navigate: 'first' },
-        { title: 'second', navigate: 'second' },
-      ];
+describe('Breadcrumbs component', () => {
+  const items = [
+    { title: 'first', navigate: 'first' },
+    { title: 'second', navigate: 'second' },
+  ];
 
-    it('renders without any items', () => {
-        mount(<Breadcrumbs items={undefined} current={"first"} onNavigate={undefined} />)
-        cy.get('.ins-c-breadcrumbs');
-      });
+  it('renders without any items', () => {
+    mount(<Breadcrumbs items={undefined} current={'first'} onNavigate={undefined} />);
+    cy.get('.ins-c-breadcrumbs');
+  });
 
-    it('renders multiple items correctly', () => {
-        mount(<Breadcrumbs items={items} current={"third"} onNavigate={undefined} />);
-        cy.get('.pf-c-breadcrumb__list').children().should('have.length', 3);
-    });
+  it('renders multiple items correctly', () => {
+    mount(<Breadcrumbs items={items} current={'third'} onNavigate={undefined} />);
+    cy.get('.pf-c-breadcrumb__list').children().should('have.length', 3);
+  });
 
-    it('fires callback when an item is clicked', () => {
-        const onNavigateSpy = cy.spy().as('onNavigateSpy');
-        mount(<Breadcrumbs items={items} current={"third"} onNavigate={onNavigateSpy} />);
-        cy.get('.pf-c-breadcrumb__list > li[data-key="1"] > a').click();
-        cy.get('@onNavigateSpy').should('have.been.calledWithMatch', Cypress.sinon.match.object, "second", 1);
-    })
+  it('fires callback when an item is clicked', () => {
+    const onNavigateSpy = cy.spy().as('onNavigateSpy');
+    mount(<Breadcrumbs items={items} current={'third'} onNavigate={onNavigateSpy} />);
+    cy.get('.pf-c-breadcrumb__list > li[data-key="1"] > a').click();
+    cy.get('@onNavigateSpy').should('have.been.calledWithMatch', Cypress.sinon.match.object, 'second', 1);
+  });
 });
