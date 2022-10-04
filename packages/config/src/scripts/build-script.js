86abd9a2d364ee8865407274963b3a2ca5c71d4b
Aneela Chagarlamudi <achagarl@redhat.com>
2022-10-04 10:07:02 -0400

    Capture Error status of spawn
    [Jira Ref RHCLOUD-21143]

--- packages/config/src/scripts/build-script.js
+++ packages/config/src/scripts/build-script.js
@@ -3,26 +3,25 @@ const { resolve } = require('path');
 const { spawn } = require('child_process');
 
 function buildScript(argv, cwd) {
-  try {
-    validateFECConfig(cwd);
-    const processArgs = [];
-    let configPath;
-    if (typeof argv.webpackConfig !== 'undefined') {
-      configPath = getWebpackConfigPath(argv.webpackConfig, cwd);
-    } else {
-      configPath = resolve(__dirname, './prod.webpack.config.js');
-    }
-    processArgs.push(`node_modules/.bin/webpack -c ${configPath}`);
-    process.env.NODE_ENV = 'production';
-    spawn('node', processArgs, {
-      stdio: [process.stdout, process.stdout, process.stdout],
-      cwd,
-      shell: true,
-    });
-  } catch (error) {
-    logError(error);
-    process.exit(1);
+  validateFECConfig(cwd);
+  const processArgs = [];
+  let configPath;
+  if (typeof argv.webpackConfig !== 'undefined') {
+    configPath = getWebpackConfigPath(argv.webpackConfig, cwd);
+  } else {
+    configPath = resolve(__dirname, './prod.webpack.config.js');
   }
+  processArgs.push(`node_modules/.bin/webpack -c ${configPath}`);
+  process.env.NODE_ENV = 'production';
+  const subprocess = spawn('node', processArgs, {
+    stdio: [process.stdout, process.stdout, process.stdout],
+    cwd,
+    shell: true,
+  });
+  subprocess.on('error', function (err) {
+    logError(err);
+    process.exit(1);
+  });
 }
 
 module.exports = buildScript;
