ec88b910878c015897a85b76a5942cd60dbbc7fb
Martin Maroši <marvusm.mmi@gmail.com>
2022-10-24 13:30:23 +0200

    Use ForkTsCheckerWebpackPlugin only if tsconfig exists.

--- packages/config/src/plugins.test.js
+++ packages/config/src/plugins.test.js
@@ -1,23 +1,31 @@
 import plugins from './plugins';
+import path from 'path';
 
 const HTML_WEBPACK = 2;
 const REPLACE = 3;
 const DEFINE_PLUGIN = 4;
 
 describe('plugins generations, no option', () => {
-  const enabledPlugins = plugins({ useChromeTemplate: false });
+  const enabledPlugins = plugins({ useChromeTemplate: false, rootFolder: '/foo/bar' });
 
   it('should generate plugins', () => {
-    expect(enabledPlugins.length).toBe(8);
+    expect(enabledPlugins.length).toBe(7);
   });
 
   it('should generate plugins with sourceMaps', () => {
-    const enabledPlugins = plugins({ generateSourceMaps: true, useChromeTemplate: false });
-    expect(enabledPlugins.length).toBe(9);
+    const enabledPlugins = plugins({ generateSourceMaps: true, useChromeTemplate: false, rootFolder: '/foo/bar' });
+    expect(enabledPlugins.length).toBe(8);
   });
 
   it('should generate correct template path for HtmlWebpackPlugin', () => {
-    expect(enabledPlugins[HTML_WEBPACK].userOptions.template).toBe('/src/index.html');
+    expect(enabledPlugins[HTML_WEBPACK].userOptions.template).toBe('/foo/bar/src/index.html');
+  });
+});
+
+describe('TS for plugins', () => {
+  const pluginsWithTs = plugins({ generateSourceMaps: true, useChromeTemplate: false, rootFolder: path.resolve(__dirname, '../../../') });
+  it('should have TSForkPlugin and 9 plugins in todal', () => {
+    expect(pluginsWithTs).toHaveLength(9);
   });
 });
 
@@ -30,7 +38,7 @@ describe('rootFolder', () => {
 });
 
 describe('appDeployment', () => {
-  const enabledPlugins = plugins({ appDeployment: '/test/folder', useChromeTemplate: false });
+  const enabledPlugins = plugins({ appDeployment: '/test/folder', useChromeTemplate: false, rootFolder: '/foo/bar' });
 
   it('should replace correct string', () => {
     enabledPlugins[REPLACE].replace({ html: 'string @@env' }, (_, { html }) => expect(html).toBe('string /test/folder'));
@@ -38,13 +46,14 @@ describe('appDeployment', () => {
 });
 
 it('htmlPlugin should update', () => {
-  const enabledPlugins = plugins({ htmlPlugin: { title: 'myTitle' }, useChromeTemplate: false });
+  const enabledPlugins = plugins({ htmlPlugin: { title: 'myTitle' }, useChromeTemplate: false, rootFolder: '/foo/bar' });
   expect(enabledPlugins[HTML_WEBPACK].userOptions.title).toBe('myTitle');
 });
 
 it('replacePlugin should update', () => {
   const enabledPlugins = plugins({
     useChromeTemplate: false,
+    rootFolder: '/foo/bar',
     replacePlugin: [
       {
         pattern: '@@another',
@@ -58,6 +67,7 @@ it('replacePlugin should update', () => {
 it('definePlugin should have default replace of CRC_APP_NAME', () => {
   const enabledPlugins = plugins({
     useChromeTemplate: false,
+    rootFolder: '/foo/bar',
     insights: { appname: 'test_app' },
   });
   expect(enabledPlugins[DEFINE_PLUGIN].definitions.CRC_APP_NAME).toBe('"test_app"');
@@ -65,6 +75,7 @@ it('definePlugin should have default replace of CRC_APP_NAME', () => {
 
 it('definePlugin should update', () => {
   const enabledPlugins = plugins({
+    rootFolder: '/foo/bar',
     useChromeTemplate: false,
     definePlugin: {
       SOME_VAR: JSON.stringify('test_val'),
