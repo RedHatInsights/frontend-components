b9e7d656f0d3da4770ffc8e6de7f79ab26544cf1
Tomas Psota <to.psota@gmail.com>
2022-11-10 11:09:38 +0100

    chore: Setup visual tests with cypress

--- packages/advisor-components/cypress/support/index.js
+++ packages/advisor-components/cypress/support/index.js
@@ -16,6 +16,7 @@
 // Import commands.js using ES2015 syntax:
 import './commands';
 import '@patternfly/patternfly/patternfly.scss';
+import '@frsource/cypress-plugin-visual-regression-diff';
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
