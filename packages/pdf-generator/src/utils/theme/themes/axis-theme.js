dc1a65c3f22c05e2413a0ddbd180584d1d614d74
Karel Hala <khala@redhat.com>
2023-01-13 14:04:57 +0100

    Copy PF's chart theme utilities since they no longer support it (#1701)
    Co-authored-by: Filip Hlavac <50696716+fhlavac@users.noreply.github.com>

---
+++ packages/pdf-generator/src/utils/theme/themes/axis-theme.js
@@ -0,0 +1,17 @@
+/* eslint-disable camelcase */
+import chart_axis_grid_stroke_Color from '@patternfly/react-tokens/dist/js/chart_axis_grid_stroke_Color';
+import chart_axis_tick_stroke_Color from '@patternfly/react-tokens/dist/js/chart_axis_tick_stroke_Color';
+
+// Axis theme
+export const AxisTheme = {
+  axis: {
+    style: {
+      grid: {
+        stroke: chart_axis_grid_stroke_Color.value,
+      },
+      ticks: {
+        stroke: chart_axis_tick_stroke_Color.value,
+      },
+    },
+  },
+};
