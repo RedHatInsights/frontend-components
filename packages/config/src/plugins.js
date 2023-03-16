3364a5fcac7dfa5bfc1f9bf8c1d477ab736ed5b4
Martin Maroši <marvusm.mmi@gmail.com>
2023-03-16 14:29:26 +0100

    Integrate core webpack config with plugin SDK.

--- packages/config/src/plugins.js
+++ packages/config/src/plugins.js
@@ -2,23 +2,12 @@ const { SourceMapDevToolPlugin } = require('webpack');
 const { ProvidePlugin, DefinePlugin } = require('webpack');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const ChunkMapperPlugin = require('@redhat-cloud-services/frontend-components-config-utilities/chunk-mapper');
-const jsVarName = require('@redhat-cloud-services/frontend-components-config-utilities/jsVarName');
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 const { glob } = require('glob');
 const path = require('path');
 
-module.exports = ({
-  rootFolder,
-  insights,
-  modules,
-  generateSourceMaps,
-  plugins,
-  definePlugin = {},
-  _unstableHotReload = false,
-  useFileHash = true,
-} = {}) => {
+module.exports = ({ rootFolder, insights, generateSourceMaps, plugins, definePlugin = {}, _unstableHotReload = false, useFileHash = true } = {}) => {
   const hasTsConfig = glob.sync(path.resolve(rootFolder, './{tsconfig.json,!(node_modules)/**/tsconfig.json}')).length > 0;
   const fileHash = !_unstableHotReload && useFileHash;
   return [
@@ -49,7 +38,6 @@ module.exports = ({
       process: 'process/browser.js',
       Buffer: ['buffer', 'Buffer'],
     }),
-    new ChunkMapperPlugin({ _unstableHotReload, modules: [...(insights ? [jsVarName(insights.appname)] : []), ...(modules || [])] }),
     ...(hasTsConfig ? [new ForkTsCheckerWebpackPlugin()] : []),
     ...(plugins || []),
     ...(_unstableHotReload ? [new ReactRefreshWebpackPlugin()] : []),
--- packages/config/src/plugins.test.js
+++ packages/config/src/plugins.test.js
@@ -7,19 +7,19 @@ describe('plugins generations, no option', () => {
   const enabledPlugins = plugins({ rootFolder: '/foo/bar' });
 
   it('should generate plugins', () => {
-    expect(enabledPlugins.length).toBe(5);
+    expect(enabledPlugins.length).toBe(4);
   });
 
   it('should generate plugins with sourceMaps', () => {
     const enabledPlugins = plugins({ generateSourceMaps: true, rootFolder: '/foo/bar' });
-    expect(enabledPlugins.length).toBe(6);
+    expect(enabledPlugins.length).toBe(5);
   });
 });
 
 describe('TS for plugins', () => {
   const pluginsWithTs = plugins({ generateSourceMaps: true, rootFolder: path.resolve(__dirname, '../../../') });
-  it('should have TSForkPlugin and 7 plugins in todal', () => {
-    expect(pluginsWithTs).toHaveLength(7);
+  it('should have TSForkPlugin and 6 plugins in todal', () => {
+    expect(pluginsWithTs).toHaveLength(6);
   });
 });
 
