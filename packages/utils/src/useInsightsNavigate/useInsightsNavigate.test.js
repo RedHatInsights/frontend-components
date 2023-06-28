8f59faefae5e44bb3ee26b8c19effcf7c251abf0
Sebastian Gräßl <bastilian@users.noreply.github.com>
2023-06-28 16:21:52 +0200

    chore(InsightsLink): Add documentation & minor fixes and improvements (#1772)

---
+++ packages/utils/src/useInsightsNavigate/useInsightsNavigate.test.js
@@ -0,0 +1,34 @@
+import { renderHook } from '@testing-library/react-hooks';
+import useInsightsNavigate from './useInsightsNavigate';
+import { useNavigate } from 'react-router-dom';
+import useChrome from '@redhat-cloud-services/frontend-components/useChrome';
+jest.mock('@redhat-cloud-services/frontend-components/useChrome');
+
+jest.mock('react-router-dom', () => ({
+  useNavigate: jest.fn(),
+}));
+
+useChrome.mockImplementation(() => ({
+  isBeta: () => false,
+  getApp: () => 'compliance',
+  getBundle: () => 'insights',
+}));
+
+describe('useInsightsNavigate', () => {
+  it('should return a function', () => {
+    useNavigate.mockImplementation(() => {});
+    const { result } = renderHook(() => useInsightsNavigate());
+
+    expect(typeof result.current).toEqual('function');
+  });
+
+  it('should work', () => {
+    const navigateMock = jest.fn();
+    useNavigate.mockImplementation(() => navigateMock);
+    const { result } = renderHook(() => useInsightsNavigate());
+
+    result.current('/path');
+
+    expect(navigateMock).toHaveBeenCalled();
+  });
+});
--- packages/utils/src/useInsightsNavigate/useInsightsNavigate.ts
+++ packages/utils/src/useInsightsNavigate/useInsightsNavigate.ts
@@ -1,14 +1,14 @@
 // @ts-ignore
-import { useNavigate } from 'react-router-dom';
+import { LinkProps, useNavigate } from 'react-router-dom';
 // @ts-ignore
 import useChrome from '@redhat-cloud-services/frontend-components/useChrome';
 import { buildInsightsPath } from '../helpers/urlPathHelpers';
 
-const useInsightsNavigate = (app: string) => {
+const useInsightsNavigate = (app: string, forcePreview?: boolean) => {
   const navigate = useNavigate();
   const chrome = useChrome();
 
-  return (to: any) => navigate(buildInsightsPath(chrome, app, to));
+  return (to: LinkProps['to'], preview?: boolean) => navigate(buildInsightsPath(chrome, app, to, preview || forcePreview));
 };
 
 export default useInsightsNavigate;
