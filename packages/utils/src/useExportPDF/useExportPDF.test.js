d53c98a088168c8e68a8803ea6d4f483763d4c8c
Muslimjon Kholjuraev <mkholjur@redhat.com>
2023-06-30 16:22:22 +0200

    update export and build the package

--- packages/utils/src/useExportPDF/useExportPDF.test.js
+++ packages/utils/src/useExportPDF/useExportPDF.test.js
@@ -1,5 +1,5 @@
 import useExportPDF from './useExportPDF';
-
+import { addNotification } from '@redhat-cloud-services/frontend-components-notifications/redux';
 jest.mock('@redhat-cloud-services/frontend-components-notifications/redux', () => ({
   ...jest.requireActual('@redhat-cloud-services/frontend-components-notifications/redux'),
   addNotification: jest.fn(() => ({})),
@@ -21,5 +21,6 @@ describe('useExportPDF', () => {
       headers: { 'Content-Type': 'application/json' },
       method: 'POST',
     });
+    expect(addNotification).toHaveBeenCalledWith();
   });
 });
