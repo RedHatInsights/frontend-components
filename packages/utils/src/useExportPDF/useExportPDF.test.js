61f6a3b38c103661784eb8e5ea9413f2039ad64e
Muslimjon Kholjuraev <mkholjur@redhat.com>
2023-07-03 11:57:30 +0200

    chore: add coverage tests

--- packages/utils/src/useExportPDF/useExportPDF.test.js
+++ packages/utils/src/useExportPDF/useExportPDF.test.js
@@ -6,22 +6,38 @@ jest.mock('@redhat-cloud-services/frontend-components-notifications/redux', () =
   addNotification: jest.fn(() => ({})),
 }));
 
-global.fetch = jest.fn(() =>
-  Promise.resolve({
-    json: () => Promise.resolve({ test: 100 }),
-  })
-);
+window.URL.createObjectURL = jest.fn();
+global.fetch = jest.fn();
 
 describe('useExportPDF', () => {
   it('Should download PDF', async () => {
+    global.fetch.mockReturnValueOnce(Promise.resolve({ blob: jest.fn() }));
     const exportPDF = useExportPDF('vulnerability');
     await exportPDF('executiveReport', 'vulnerability-test-export', { someRequestPayload: 'some value' });
-
     expect(fetch).toHaveBeenCalledWith(pdfGeneratorURL, {
       body: '{"service":"vulnerability","template":"executiveReport","params":{"someRequestPayload":"some value"}}',
       headers: { 'Content-Type': 'application/json' },
       method: 'POST',
     });
-    expect(addNotification).toHaveBeenCalledWith();
+    expect(addNotification).toHaveBeenCalledWith({
+      description: 'Once complete, your download will start automatically.',
+      title: 'Preparing export',
+      variant: 'info',
+    });
+    expect(addNotification).toHaveBeenCalledWith({
+      variant: 'success',
+      title: 'Downloading export',
+    });
+  });
+  it('Should fail to download PDF with notification', async () => {
+    global.fetch.mockReturnValueOnce(Promise.reject('error'));
+
+    const exportPDF = useExportPDF('vulnerability');
+    await exportPDF('executiveReport', 'vulnerability-test-export', { someRequestPayload: 'some value' });
+    expect(addNotification).toHaveBeenCalledWith({
+      description: 'Reinitiate this export to try again.',
+      title: 'Couldn’t download export',
+      variant: 'danger',
+    });
   });
 });
