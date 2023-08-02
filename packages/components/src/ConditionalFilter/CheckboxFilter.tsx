5e5d89c4054c84a0d4e5efd26fbc880a5380c41f
Martin Maroši <marvusm.mmi@gmail.com>
2023-08-02 12:29:49 +0200

    Remove text filter as a fallback from checkbox filter.

--- packages/components/src/ConditionalFilter/CheckboxFilter.tsx
+++ packages/components/src/ConditionalFilter/CheckboxFilter.tsx
@@ -1,18 +1,20 @@
-import React, { Fragment, useState } from 'react';
+import React, { useState } from 'react';
 import { Badge, MenuToggle, Select, SelectList, SelectOption, SelectProps } from '@patternfly/react-core';
 
 import omit from 'lodash/omit';
-import TextFilter, { FilterItem, FilterValue, isFilterValue } from './TextFilter';
+import { FilterItem, FilterValue, isFilterValue } from './TextFilter';
+
+export type CheckboxFilterValue = undefined | string | number | boolean | FilterValue;
 
 export interface CheckboxFilterProps {
   /** Optional onChange event callback. */
-  onChange?: (
+  onChange: (
     e: React.MouseEvent | React.ChangeEvent | React.FormEvent<HTMLInputElement> | undefined,
-    newSelection: string | FilterValue | (string | FilterValue)[],
-    selection?: string | FilterValue
+    newSelection: CheckboxFilterValue[],
+    selection?: CheckboxFilterValue
   ) => void;
   /** Optional list of selected values. */
-  value?: string | FilterValue | (string | FilterValue)[] | Record<string, unknown>;
+  value?: CheckboxFilterValue[];
   /** Optional select value placeholder. */
   placeholder?: string;
   /** Optional list of available options. */
@@ -23,36 +25,31 @@ export interface CheckboxFilterProps {
   className?: string;
 }
 
+function calculateSelected(value: CheckboxFilterProps['value'], selected: CheckboxFilterValue[]): CheckboxFilterValue[] {
+  return Array.from(
+    new Set([
+      ...(value && (value as CheckboxFilterValue[]).length > 0 && value.constructor === Array
+        ? value.map((item) => {
+            return isFilterValue(item) ? item.value : item;
+          })
+        : []),
+      ...selected,
+    ])
+  );
+}
+
 /**
  * Component that works as a checkbox filter for ConditionalFilter component.
  *
  * It was not designed to be used as a standalone component, but rather within conditionalFilter.
  */
-const CheckboxFilter: React.FunctionComponent<CheckboxFilterProps> = ({
-  items = [],
-  value = [],
-  onChange = () => undefined,
-  isDisabled = false,
-  ...props
-}) => {
+const CheckboxFilter: React.FunctionComponent<CheckboxFilterProps> = ({ items = [], value = [], onChange, isDisabled = false, ...props }) => {
   const { placeholder, className } = props;
   const [isExpanded, setExpanded] = useState(false);
-  const [selected, setSelected] = useState<(string | FilterValue)[]>([]);
+  const [selected, setSelected] = useState<CheckboxFilterValue[]>(calculateSelected(value, []));
 
-  const calculateSelected = () =>
-    Array.from(
-      new Set([
-        ...(value && (value as (string | FilterValue)[]).length > 0 && value.constructor === Array
-          ? value.map((item) => {
-              return isFilterValue(item) ? item.value : item;
-            })
-          : []),
-        ...selected,
-      ])
-    );
-
-  const onSelect = (event: React.MouseEvent<Element, MouseEvent> | React.ChangeEvent<Element> | undefined, selection: string | FilterValue) => {
-    let newSelection = calculateSelected();
+  const onSelect = (event: React.MouseEvent<Element, MouseEvent> | React.ChangeEvent<Element> | undefined, selection: CheckboxFilterValue) => {
+    let newSelection = calculateSelected(value, selected);
     newSelection = newSelection.includes(selection) ? newSelection.filter((item) => item !== selection) : [...newSelection, selection];
 
     onChange?.(event, newSelection, selection);
@@ -71,37 +68,31 @@ const CheckboxFilter: React.FunctionComponent<CheckboxFilterProps> = ({
   );
 
   return (
-    <Fragment>
-      {!items || (items && items.length <= 0) ? (
-        <TextFilter {...props} onChange={onChange} isDisabled={isDisabled} value={`${calculateSelected()}`} />
-      ) : (
-        <Select
-          toggle={toggle}
-          className={className}
-          aria-label="Select Input"
-          onOpenChange={(isExpanded) => setExpanded(isExpanded)}
-          onSelect={(event, value) => onSelect(event, value as string | FilterValue)}
-          isOpen={isExpanded}
-          placeholder={placeholder}
-          ouiaId={placeholder}
-        >
-          <SelectList>
-            {items.map(({ value, onClick, label, id, ...item }, key) => (
-              <SelectOption
-                hasCheckbox
-                isSelected={selected.includes(value)}
-                {...omit(item, 'onChange')}
-                key={id || key}
-                value={String(value || id || key)}
-                onClick={(e) => onClick?.(e, { value, label, id, ...item }, key)}
-              >
-                {label}
-              </SelectOption>
-            ))}
-          </SelectList>
-        </Select>
-      )}
-    </Fragment>
+    <Select
+      toggle={toggle}
+      className={className}
+      aria-label="Select Input"
+      onOpenChange={(isExpanded) => setExpanded(isExpanded)}
+      onSelect={(event, value) => onSelect(event, value)}
+      isOpen={isExpanded}
+      placeholder={placeholder}
+      ouiaId={placeholder}
+    >
+      <SelectList>
+        {items.map(({ value, onClick, label, id, ...item }, key) => (
+          <SelectOption
+            hasCheckbox
+            isSelected={selected.includes(value)}
+            {...omit(item, 'onChange')}
+            key={id || key}
+            value={String(value || id || key)}
+            onClick={(e) => onClick?.(e, { value, label, id, ...item }, key)}
+          >
+            {label}
+          </SelectOption>
+        ))}
+      </SelectList>
+    </Select>
   );
 };
 
