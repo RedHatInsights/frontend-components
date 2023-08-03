fe3b505c7054e271ca92eb6be01d8738a8383deb
Martin Maroši <marvusm.mmi@gmail.com>
2023-08-03 10:20:37 +0200

    Remove custom input component.

--- packages/components/src/Input/index.ts
+++
@@ -1,5 +0,0 @@
-export { default } from './Input';
-export { default as Input } from './Input';
-export * from './Input';
-export { default as LabeledInput } from './LabeledInput';
-export * from './LabeledInput';
