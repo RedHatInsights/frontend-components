dcaee14659810afe65df3b94d0a33caa4881960e
nicolethoen <nthoen@redhat.com>
2023-08-15 10:14:09 -0400

    fix: simple table filter

--- packages/components/src/SimpleTableFilter/SimpleTableFilter.tsx
+++ packages/components/src/SimpleTableFilter/SimpleTableFilter.tsx
@@ -91,12 +91,8 @@ const SimpleFilter: React.FC<SimpleFilterProps> = ({
         aria-label="simple-table-filter"
         placeholder={placeholder}
         onChange={onInputChange}
+        {...(!buttonTitle && searchIcon && { customIcon: <SearchIcon className="ins-c-search-icon" /> })}
       />
-      {!buttonTitle && searchIcon && (
-        <Icon size="sm">
-          <SearchIcon className="ins-c-search-icon" />
-        </Icon>
-      )}
       {buttonTitle && (
         <Button variant={ButtonVariant.secondary} action="filter" onClick={onFilterSubmit}>
           {buttonTitle}
