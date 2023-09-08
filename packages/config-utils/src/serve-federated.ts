8b5c7778c364e070c9e9936382b581cc971d0c65
Martin Maroši <marvusm.mmi@gmail.com>
2023-09-08 09:07:06 +0200

    Fix serve federated prod config path.

--- packages/config-utils/src/serve-federated.ts
+++ packages/config-utils/src/serve-federated.ts
@@ -3,7 +3,7 @@ import fs from 'fs';
 import concurrently from 'concurrently';
 
 function federate(argv: Record<string, any>, cwd: string) {
-  let configPath: string = argv.config || './node_modules/@redhat-cloud-services/frontend-components-config/src/scripts/prod.webpack.config.js';
+  let configPath: string = argv.config || './node_modules/@redhat-cloud-services/frontend-components-config/bin/prod.webpack.config.js';
   const WEBPACK_PATH = path.resolve(cwd, './node_modules/.bin/webpack');
   const HTTP_SERVER_PATH = path.resolve(cwd, './node_modules/.bin/http-server');
   if (typeof configPath !== 'string') {
