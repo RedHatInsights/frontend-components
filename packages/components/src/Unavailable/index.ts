7ecf37eb6e739866301deb46eef62778f9bdb8eb
Filip Hlavac <filip.hlavac01@gmail.com>
2023-11-09 18:54:33 +0100

    Reexport Unavailable

--- packages/components/src/Unavailable/index.ts
+++ packages/components/src/Unavailable/index.ts
@@ -1,2 +1,4 @@
+export * from '@patternfly/react-component-groups/dist/dynamic/UnavailableContent';
+export { UnavailableContentProps as UnavailableProps } from '@patternfly/react-component-groups/dist/dynamic/UnavailableContent';
 export { default } from './Unavailable';
 export { default as Unavailable } from './Unavailable';
