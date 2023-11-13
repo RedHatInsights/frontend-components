f89597f583e39edb8bbc83a677cb9cd944251568
Filip Hlavac <filip.hlavac01@gmail.com>
2023-11-13 11:27:53 +0100

    Reexport and deprecate InvalidObject

--- packages/components/src/InvalidObject/InvalidObject.tsx
+++ packages/components/src/InvalidObject/InvalidObject.tsx
@@ -1,28 +1,14 @@
 import React from 'react';
-import Icon404 from './icon-404';
-import { Button, Title } from '@patternfly/react-core';
-import './invalidObject.scss';
+import InvalidObjectPF, { InvalidObjectProps } from '@patternfly/react-component-groups/dist/dynamic/InvalidObject';
 
 // Don't use chrome here because the 404 page on landing does not use chrome
-const isBeta = () => {
-  return window.location.pathname.split('/')[1] === 'beta' ? '/beta' : '';
-};
+const isBeta = () => (window.location.pathname.split('/')[1] === 'beta' ? '/beta' : '');
 
-const InvalidObject: React.FunctionComponent = ({ ...props }) => {
-  return (
-    <section {...props} className="pf-v5-l-page__main-section pf-v5-c-page__main-section ins-c-component_invalid-component">
-      <Title headingLevel="h1" size="3xl">
-        We lost that page
-      </Title>
-      <Icon404 />
-      <Title headingLevel="h1" size="xl" className="ins-c-text__sorry">
-        Let&apos;s find you a new one. Try a new search or return home.
-      </Title>
-      <Button variant="link" component="a" href={`${window.location.origin}${isBeta()}`}>
-        Return to homepage
-      </Button>
-    </section>
-  );
-};
+/**
+ * @deprecated Do not use deprecated InvalidObject import, the component has been moved to @patternfly/react-component-groups
+ */
+const InvalidObject: React.FunctionComponent<InvalidObjectProps> = (props) => (
+  <InvalidObjectPF toLandingPageUrl={`${window.location.origin}${isBeta()}`} {...props} />
+);
 
 export default InvalidObject;
--- packages/components/src/InvalidObject/index.ts
+++ packages/components/src/InvalidObject/index.ts
@@ -1,3 +1,4 @@
+export * from '@patternfly/react-component-groups/dist/dynamic/InvalidObject';
 export { default } from './InvalidObject';
 export { default as InvalidObject } from './InvalidObject';
 export { default as Icon404 } from './icon-404';
