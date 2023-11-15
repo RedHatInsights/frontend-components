9f211e94edd3df4f82a1b91f55055f1fb8f27289
Filip Hlavac <filip.hlavac01@gmail.com>
2023-11-15 13:48:40 +0100

    Remove unused icon404 references

--- packages/components/src/InvalidObject/index.ts
+++ packages/components/src/InvalidObject/index.ts
@@ -1,4 +1,3 @@
 export * from '@patternfly/react-component-groups/dist/dynamic/InvalidObject';
 export { default } from './InvalidObject';
 export { default as InvalidObject } from './InvalidObject';
-export { default as Icon404 } from './icon-404';
