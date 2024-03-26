04a24e5820089ef1eeea4334110249cca133d52d
Zein Sleiman <zsleiman@redhat.com>
2024-03-26 15:09:13 -0500

    add ouiaId to pageheader and title

--- packages/components/src/PageHeader/PageHeader.tsx
+++ packages/components/src/PageHeader/PageHeader.tsx
@@ -3,15 +3,24 @@ import classNames from 'classnames';
 
 import { DarkContext } from '../Dark';
 import './page-header.scss';
+import { useOUIAId } from '@patternfly/react-core/dist/dynamic/helpers/OUIA';
 
 export interface PageHeaderProps {
   className?: string;
+  ouiaId?: string;
+  ouiaSafe?: boolean;
 }
 
 /**
  * This is a page header that mimics the patternfly layout for a header section
  */
-const PageHeader: React.FunctionComponent<React.PropsWithChildren<PageHeaderProps>> = ({ className, children, ...props }) => {
+const PageHeader: React.FunctionComponent<React.PropsWithChildren<PageHeaderProps>> = ({
+  className,
+  children,
+  ouiaId,
+  ouiaSafe = true,
+  ...props
+}) => {
   const pageHeaderClasses = classNames(
     className,
     'pf-v5-l-page-header',
@@ -19,6 +28,8 @@ const PageHeader: React.FunctionComponent<React.PropsWithChildren<PageHeaderProp
     'pf-v5-l-page__main-section',
     'pf-v5-c-page__main-section'
   );
+  const ouiaComponentType = 'RHI/Header';
+  const ouiaFinalId = useOUIAId(ouiaComponentType, ouiaId, ouiaSafe as unknown as string);
 
   return (
     <DarkContext.Consumer>
@@ -26,7 +37,14 @@ const PageHeader: React.FunctionComponent<React.PropsWithChildren<PageHeaderProp
         const themeClasses = classNames({ [`pf-m-${theme}-200`]: theme === 'dark' }, { [`pf-m-light`]: theme === 'light' });
 
         return (
-          <section {...props} className={`${pageHeaderClasses} ${themeClasses}`} widget-type="InsightsPageHeader">
+          <section
+            data-ouia-component-type={ouiaComponentType}
+            data-ouia-component-id={ouiaFinalId}
+            data-ouia-safe={ouiaSafe}
+            {...props}
+            className={`${pageHeaderClasses} ${themeClasses}`}
+            widget-type="InsightsPageHeader"
+          >
             {children}
           </section>
         );
--- packages/components/src/PageHeader/PageHeaderTitle.tsx
+++ packages/components/src/PageHeader/PageHeaderTitle.tsx
@@ -1,19 +1,34 @@
 import React from 'react';
 import classNames from 'classnames';
-import { Title, TitleProps } from '@patternfly/react-core';
+import { Title } from '@patternfly/react-core/dist/dynamic/components/Title';
+import { TitleProps } from '@patternfly/react-core/dist/dynamic/components/Title';
+import { useOUIAId } from '@patternfly/react-core/dist/dynamic/helpers/OUIA';
 
 export interface PageHeaderTitleProps extends Omit<TitleProps, 'title' | 'headingLevel'> {
   title: React.ReactNode;
+  ouiaId?: string;
+  ouiaSafe?: boolean;
 }
 
 /**
  * This is the title section of the pageHeader
  */
-const PageHeaderTitle: React.FunctionComponent<PageHeaderTitleProps> = ({ className, title }) => {
+const PageHeaderTitle: React.FunctionComponent<PageHeaderTitleProps> = ({ className, title, ouiaId, ouiaSafe = true, ...props }) => {
   const pageHeaderTitleClasses = classNames(className);
+  const ouiaComponentType = 'RHI/Header';
+  const ouiaFinalId = useOUIAId(ouiaComponentType, ouiaId, ouiaSafe as unknown as string);
 
   return (
-    <Title headingLevel="h1" size="2xl" className={pageHeaderTitleClasses} widget-type="InsightsPageHeaderTitle">
+    <Title
+      headingLevel="h1"
+      size="2xl"
+      className={pageHeaderTitleClasses}
+      widget-type="InsightsPageHeaderTitle"
+      data-ouia-component-type={ouiaComponentType}
+      data-ouia-component-id={ouiaFinalId}
+      data-ouia-safe={ouiaSafe}
+      {...props}
+    >
       {title}
     </Title>
   );
