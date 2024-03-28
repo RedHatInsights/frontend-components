e17be92c4aeb18cd788e75247179f9bb4da3daed
Muslimjon <mkholjur@redhat.com>
2024-03-28 14:54:07 +0100

    fix shared axios eslint rule lint issues

--- packages/eslint-config/lib/rules/insights-disallow-custom-axios.js
+++ packages/eslint-config/lib/rules/insights-disallow-custom-axios.js
@@ -3,34 +3,30 @@
  * @author Muslimj0n Kholjuraev
  */
 
-  module.exports = {
-    meta: {
-      type: 'suggestion',
-      fixable: 'code',
-      messages: {
-        useSharedAxiosMessage: 'Avoid using axios in your project. Instead, use the shared axios instance with interceptors for better error handling and reporting'
-      },
-      docs: {
-        description: 'Use the shared axios instance',
-        url: 'https://github.com/RedHatInsights/frontend-components/blob/master/packages/utils/src/interceptors/interceptors.ts#L83',
-        recommended: false,
-      },
+module.exports = {
+  meta: {
+    type: 'suggestion',
+    fixable: 'code',
+    messages: {
+      useSharedAxiosMessage:
+        'Avoid using axios in your project. Instead, use the shared axios instance with interceptors for better error handling and reporting',
     },
-    create: function (context) {
-      return {
-        ImportDeclaration: (codePath) => {
-          if (codePath.source.value.includes('axios')) {
-            context.report({
-              node: codePath,
-              messageId: 'useSharedAxiosMessage',
-              data: {
-                package: codePath.source.value,
-                ...data,
-              },
-            });
-          }
-        },
-      };
+    docs: {
+      description: 'Use the shared axios instance',
+      url: 'https://github.com/RedHatInsights/frontend-components/blob/master/packages/utils/src/interceptors/interceptors.ts#L83',
+      recommended: false,
     },
-  };
-  
+  },
+  create: function (context) {
+    return {
+      ImportDeclaration: (codePath) => {
+        if (codePath.source.value.includes('axios')) {
+          context.report({
+            node: codePath,
+            messageId: 'useSharedAxiosMessage',
+          });
+        }
+      },
+    };
+  },
+};
