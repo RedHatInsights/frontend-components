75dd759966886ed7cacebc45919604ea5ebe324f
Muslimjon <mkholjur@redhat.com>
2024-04-11 14:01:26 +0200

    PageHeaderTitle: enable passing actions component

--- packages/components/src/PageHeader/PageHeaderTitle.tsx
+++ packages/components/src/PageHeader/PageHeaderTitle.tsx
@@ -1,21 +1,33 @@
 import React from 'react';
 import classNames from 'classnames';
-import { Title, TitleProps } from '@patternfly/react-core';
+import { 
+  Title, 
+  TitleProps, 
+  Flex, 
+  FlexItem  
+} from '@patternfly/react-core';
+
 
 export interface PageHeaderTitleProps extends Omit<TitleProps, 'title' | 'headingLevel'> {
   title: React.ReactNode;
+  actionsComponent: React.ReactNode;
 }
 
 /**
  * This is the title section of the pageHeader
  */
-const PageHeaderTitle: React.FunctionComponent<PageHeaderTitleProps> = ({ className, title }) => {
+const PageHeaderTitle: React.FunctionComponent<PageHeaderTitleProps> = ({ className, title, actionsComponent }) => {
   const pageHeaderTitleClasses = classNames(className);
 
   return (
-    <Title headingLevel="h1" size="2xl" className={pageHeaderTitleClasses} widget-type="InsightsPageHeaderTitle">
-      {title}
-    </Title>
+   <Flex className="example-border" justifyContent={{ default: 'justifyContentSpaceBetween' }}>
+    <FlexItem>    
+      <Title headingLevel="h1" size="2xl" className={pageHeaderTitleClasses} widget-type="InsightsPageHeaderTitle">
+        {title}
+      </Title>
+    </FlexItem>
+    {actionsComponent && <FlexItem>{actionsComponent}</FlexItem>}
+  </Flex>
   );
 };
 
