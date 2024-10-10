69e992dfeb738697d4b7d7049087502c65caa0d4
Martin Marosi <marvusm.mmi@gmail.com>
2024-10-10 10:51:45 +0200

    fix(executors): scss importer for inner dependencies

--- packages/executors/src/executors/build-styles/executor.ts
+++ packages/executors/src/executors/build-styles/executor.ts
@@ -5,6 +5,7 @@ import { glob } from 'glob'
 import path from 'path'
 import util from 'util';
 import sass, { NodePackageImporter } from 'sass'
+import pki from 'node-sass-package-importer'
 
 const asyncWriteFile = util.promisify(fs.writeFile);
 const asyncCopyFile = util.promisify(fs.copyFile);
@@ -16,7 +17,21 @@ const BuilderExecutorSchema = z.object({
 
 export type BuilderExecutorSchemaType = z.infer<typeof BuilderExecutorSchema>;
 
-async function buildStyle(file: string, outputDir: string, currentProjectRoot: string) {
+function importVariants(url: string) {
+  const parsedUrl = path.parse(url);
+  let variants = [url]
+  if(parsedUrl.dir && !parsedUrl.ext){
+    const moduleName = url.split('/').pop();
+    variants = [
+      `${url.replace(new RegExp(`${moduleName}$`), `_${moduleName}`)}.scss`,
+      `${url}.scss`,
+    ]
+  }
+
+  return variants;
+}
+
+async function buildStyle(file: string, outputDir: string, currentProjectRoot: string, projectRoot: string) {
   const lastFragment = file.split('/src/').pop()
   if (!lastFragment) {
     throw new Error(`Invalid file path: ${file}`);
@@ -48,9 +63,45 @@ async function buildStyle(file: string, outputDir: string, currentProjectRoot: s
 
     const render = sass.compileAsync(file,
       {
-        importers: [new NodePackageImporter()],
+        importers: [{
+          findFileUrl: (url) => {
+            if(url.startsWith('~')){
+              if(url.startsWith('~@redhat-cloud-services')){
+                const repoPackage = url.split('~@redhat-cloud-services/').pop();
+                if(!repoPackage){
+                  throw new Error(`Invalid package: ${url}`);
+                }
+                const r = new URL(`file://${projectRoot}/dist/@redhat-cloud-services/${repoPackage}`)
+                return r
+              }
+              // from node_modules
+              const repoPackage = url.split('~').pop();
+              if(!repoPackage){
+                throw new Error(`Invalid package: ${url}`);
+              }
+              // project node_modules
+              const projectModulesRoot = path.join(projectRoot, '/', currentProjectRoot, '/', 'node_modules', repoPackage);
+              let variants = importVariants(projectModulesRoot);
+              let sassPath = variants.find((v) => fs.existsSync(v));
+              if(sassPath){
+                return new URL(`file://${sassPath}`)
+              }
+              // root node_modules
+              const modulesRoot = path.join(projectRoot, 'node_modules', repoPackage);
+              variants = importVariants(modulesRoot);
+              sassPath = variants.find((v) => fs.existsSync(v));
+              if(sassPath){
+                return new URL(`file://${sassPath}`)
+              }
+              throw new Error("Unable to find valid SCSS package");
+              
+            }
+            return new URL(url)
+          },
+        }],
       }
     ).then(({ css }) => {
+      // console.log({ css })
       return asyncWriteFile(outFile, css, 'utf8');
     });
     const copy = asyncCopyFile(file, outFile.replace(/css$/, 'scss'));
@@ -59,14 +110,14 @@ async function buildStyle(file: string, outputDir: string, currentProjectRoot: s
   return Promise.all(promises);
 }
 
-async function buildStyles(files: string[], outputDir: string, currentProjectRoot: string) {
-  const cmds = files.map(file => buildStyle(file, outputDir, currentProjectRoot));
+async function buildStyles(files: string[], outputDir: string, currentProjectRoot: string, projectRoot: string) {
+  const cmds = files.map(file => buildStyle(file, outputDir, currentProjectRoot, projectRoot));
   return Promise.all(cmds);
 }
 
-async function run(files: string[] = [], outputDir: string, currentProjectRoot: string) {
+async function run(files: string[] = [], outputDir: string, currentProjectRoot: string, projectRoot: string) {
   try {
-    await buildStyles(files, outputDir, currentProjectRoot);
+    await buildStyles(files, outputDir, currentProjectRoot, projectRoot);
   } catch (error) {
     console.error(error);
     process.exit(1);
@@ -93,7 +144,7 @@ export default async function runExecutor(options: BuilderExecutorSchemaType, co
   const outputDir = (projectRoot + "/" + options.outputPath).replace(/\/\//g, '/');
 
 const files = glob.sync(path.resolve(`${currentProjectRoot}/src/**/*.scss`));
-  await run(files, outputDir, currentProjectRoot);
+  await run(files, outputDir, currentProjectRoot, projectRoot);
   return {
     success: true,
   };
