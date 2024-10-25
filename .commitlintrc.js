cb39ef9441d2faff252cac22f288dfaef03d8335
Martin Marosi <marvusm.mmi@gmail.com>
2024-10-25 13:30:29 +0200

    chore: setup husky and commitlint

---
+++ .commitlintrc.js
@@ -0,0 +1,6 @@
+module.exports ={
+    extends: ['@commitlint/config-conventional'],
+    // Ignore rules can be found here
+    // https://github.com/conventional-changelog/commitlint/blob/master/%40commitlint/is-ignored/src/defaults.ts
+    defaultIgnores: true,
+}
