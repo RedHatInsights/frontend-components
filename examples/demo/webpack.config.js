b249a7540e71934d9174241ad6dbc03375dcd836
Martin Maroši <marvusm.mmi@gmail.com>
2024-11-22 10:46:27 +0100

    Revert "chore: downgrade NX to attempt fix release issues"

--- examples/demo/webpack.config.js
+++ examples/demo/webpack.config.js
@@ -1,6 +1,5 @@
-// const { NxAppWebpackPlugin } = require('@nx/webpack/app-plugin');
-// const { NxReactWebpackPlugin } = require('@nx/react/webpack-plugin');
-const { NxWebpackPlugin } = require('@nx/webpack');
+const { NxAppWebpackPlugin } = require('@nx/webpack/app-plugin');
+const { NxReactWebpackPlugin } = require('@nx/react/webpack-plugin');
 const { join } = require('path');
 
 module.exports = {
@@ -16,7 +15,7 @@ module.exports = {
     },
   },
   plugins: [
-    new NxWebpackPlugin({
+    new NxAppWebpackPlugin({
       tsConfig: './tsconfig.app.json',
       compiler: 'babel',
       main: './src/main.tsx',
@@ -26,6 +25,11 @@ module.exports = {
       styles: ['./src/styles.scss'],
       outputHashing: process.env['NODE_ENV'] === 'production' ? 'all' : 'none',
       optimization: process.env['NODE_ENV'] === 'production',
+    }),
+    new NxReactWebpackPlugin({
+      // Uncomment this line if you don't want to use SVGR
+      // See: https://react-svgr.com/
+      // svgr: false
     }),
   ],
 };
