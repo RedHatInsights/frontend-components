fa857b9752816f3a28eea891b5fcdafbbece7920
Martin Marosi <marvusm.mmi@gmail.com>
2024-12-09 10:20:54 +0100

    chore: remove description length limits

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -3,4 +3,6 @@ module.exports ={
     // Ignore rules can be found here
     // https://github.com/conventional-changelog/commitlint/blob/master/%40commitlint/is-ignored/src/defaults.ts
     defaultIgnores: true,
+    'body-max-length': Infinity,
+    'body-max-line-length': Infinity,
 }
