75cce9cfa4e6ff5b6c509380e8604bc56c212efd
NachoBot <crc-nachobot@redhat.com>
2026-05-18 09:46:35 +0000

    feat(components): convert Inventory components to TypeScript

---
+++ packages/components/src/Inventory/InventoryDetail.tsx
@@ -0,0 +1,76 @@
+import React, { Suspense } from 'react';
+import { ScalprumComponent } from '@scalprum/react-core';
+import { useStore } from 'react-redux';
+import { Bullseye } from '@patternfly/react-core/dist/dynamic/layouts/Bullseye';
+import { Spinner } from '@patternfly/react-core/dist/dynamic/components/Spinner';
+import InventoryLoadError from './InventoryLoadError';
+import classNames from 'classnames';
+import WithHistory from './WithHistory';
+
+interface BaseInventoryDetailProps {
+  fallback?: React.ReactNode;
+  innerRef?: React.Ref<any>;
+  component?: string;
+  className?: string;
+  history?: any;
+}
+
+const BaseInventoryDetail: React.FC<BaseInventoryDetailProps> = ({
+  fallback = (
+    <Bullseye className="pf-v6-u-p-lg">
+      <Spinner size="xl" />
+    </Bullseye>
+  ),
+  component = 'section',
+  className,
+  history,
+  innerRef,
+  ...props
+}) => {
+  const store = useStore();
+  const Component = component as keyof JSX.IntrinsicElements;
+  return (
+    <Component className={classNames(className, 'inventory')}>
+      <Suspense fallback={fallback}>
+        <ScalprumComponent
+          history={history}
+          store={store}
+          appName="inventory"
+          module="./InventoryDetail"
+          scope="inventory"
+          ErrorComponent={<InventoryLoadError component="InventoryDetailHead" {...props} />}
+          ref={innerRef}
+          {...props}
+        />
+      </Suspense>
+    </Component>
+  );
+};
+
+export interface InventoryDetailProps {
+  /** React Suspense fallback component. <a href="https://reactjs.org/docs/code-splitting.html#reactlazy" target="_blank">Learn more</a>. */
+  fallback?: React.ReactNode;
+  /** Optional wrapper component */
+  component?: string;
+  /** Optional classname applied to wrapper component */
+  className?: string;
+}
+
+/**
+ * Inventory sub component.
+ *
+ * This component shows complete inventory detail with system info and app's detail in tab(s).
+ */
+const InventoryDetail = React.forwardRef<any, InventoryDetailProps>((props, ref) => (
+  <BaseInventoryDetail innerRef={ref} {...props} />
+));
+
+InventoryDetail.displayName = 'InventoryDetail';
+
+const CompatibilityWrapper = React.forwardRef<any, InventoryDetailProps>((props, ref) => (
+  <WithHistory innerRef={ref} Component={InventoryDetail} {...props} />
+));
+
+CompatibilityWrapper.displayName = 'InventoryDetailCompatibilityWrapper';
+
+export default CompatibilityWrapper;
---
+++ packages/components/src/Inventory/InventoryTable.tsx
@@ -0,0 +1,76 @@
+import React, { Suspense } from 'react';
+import { ScalprumComponent } from '@scalprum/react-core';
+import { useStore } from 'react-redux';
+import { Bullseye } from '@patternfly/react-core/dist/dynamic/layouts/Bullseye';
+import { Spinner } from '@patternfly/react-core/dist/dynamic/components/Spinner';
+import InventoryLoadError from './InventoryLoadError';
+import classNames from 'classnames';
+import WithHistory from './WithHistory';
+
+interface BaseInvTableProps {
+  fallback?: React.ReactNode;
+  innerRef?: React.Ref<HTMLElement>;
+  component?: string;
+  className?: string;
+  history?: any;
+}
+
+const BaseInvTable: React.FC<BaseInvTableProps> = ({
+  fallback = (
+    <Bullseye className="pf-v6-u-p-lg">
+      <Spinner size="xl" aria-label="Loading" />
+    </Bullseye>
+  ),
+  component = 'section',
+  className,
+  history,
+  innerRef,
+  ...props
+}) => {
+  const store = useStore();
+  const Component = component as keyof JSX.IntrinsicElements;
+  return (
+    <Component className={classNames(className, 'inventory')}>
+      <Suspense fallback={fallback}>
+        <ScalprumComponent
+          history={history}
+          store={store}
+          appName="inventory"
+          module="./InventoryTable"
+          scope="inventory"
+          ErrorComponent={<InventoryLoadError component="InventoryTable" {...props} />}
+          ref={innerRef}
+          {...props}
+        />
+      </Suspense>
+    </Component>
+  );
+};
+
+export interface InventoryTableProps {
+  /** React Suspense fallback component. <a href="https://reactjs.org/docs/code-splitting.html#reactlazy" target="_blank">Learn more</a>. */
+  fallback?: React.ReactNode;
+  /** Optional wrapper component */
+  component?: string;
+  /** Optional classname applied to wrapper component */
+  className?: string;
+}
+
+/**
+ * Inventory sub component.
+ *
+ * This component shows systems table connected to redux.
+ */
+const InvTable = React.forwardRef<HTMLElement, InventoryTableProps>((props, ref) => (
+  <BaseInvTable innerRef={ref} {...props} />
+));
+
+InvTable.displayName = 'InvTable';
+
+const CompatibilityWrapper = React.forwardRef<HTMLElement, InventoryTableProps>((props, ref) => (
+  <WithHistory innerRef={ref} Component={InvTable} {...props} />
+));
+
+CompatibilityWrapper.displayName = 'InvTableCompatibilityWrapper';
+
+export default CompatibilityWrapper;
